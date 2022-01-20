93fa11e16786af0fab4207fb56ee56c75a87f5f1
philgei <philipp@geirhos.com>
2022-01-20 11:07:23 +0100

    added Template

---
+++ src/babylon/template/BusinessLogic/API/BusinessLogic.ts
@@ -0,0 +1,19 @@
+import { DataAccess } from './../../DataAccess/API/DataAccess';
+import { IDataAccess } from './IDataAccess';
+
+export class BusinessLogic {
+  // @ts-ignore
+  private _dataAccess: IDataAccess;
+
+  constructor(dataAccess?: IDataAccess) {
+    this._initDataAccess(dataAccess);
+  }
+
+  private _initDataAccess(dataAccess?: IDataAccess): void {
+    if (dataAccess) {
+      this._dataAccess = dataAccess;
+    } else {
+      this._dataAccess = new DataAccess();
+    }
+  }
+}
---
+++ src/babylon/template/PresentationLogic/API/PresentationLogic.ts
@@ -0,0 +1,19 @@
+import { BusinessLogic } from './../../BusinessLogic/API/BusinessLogic';
+import { IBusinessLogic } from './IBusinessLogic';
+import { IPresentationLogic } from './IPresentationLogic';
+export class PresentationLogic implements IPresentationLogic {
+  // @ts-ignore
+  private _businessLogic: IBusinessLogic;
+
+  constructor(businessLogic?: IBusinessLogic) {
+    this._initBusinessLogic(businessLogic);
+  }
+
+  private _initBusinessLogic(businessLogic?: IBusinessLogic): void {
+    if (businessLogic) {
+      this._businessLogic = businessLogic;
+    } else {
+      this._businessLogic = new BusinessLogic();
+    }
+  }
+}
