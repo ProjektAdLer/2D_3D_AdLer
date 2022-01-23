45a43a376ca62f7c3e66ab3f3f168c4f3037fa3e
Philipp Geirhos <philipp@geirhos.com>
2022-01-23 21:45:56 +0100

    updated dependencies for DI-Container

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,7 @@
-const { override, addBabelPlugin } = require('customize-cra');
-const pluginProposalDecorators = require('@babel/plugin-proposal-decorators');
-const test = require('babel-plugin-transform-typescript-metadata');
+// This file is used together with react-scripts-rewire to overwrite the webpack config of your CRA App to add capability for
+// Typescript Decorators
 
-module.exports = override(addBabelPlugin([pluginProposalDecorators, { legacy: true }]), addBabelPlugin(test));
+const { override, addBabelPlugin } = require('customize-cra');
+const babelTsTransformPlugin = require('babel-plugin-transform-typescript-metadata');
+
+module.exports = override(addBabelPlugin(babelTsTransformPlugin));
