9ae4019fa72cc9d8320af5cf906e8a02278c1fcf
Marius Kaczmarek <kaczmarek.m@gmx.de>
2022-02-03 17:43:13 +0100

    removed old test files

--- src/babylon/DI/container.ts
+++
@@ -1,8 +0,0 @@
-import { Container } from "inversify";
-import { DependencyA, DependencyB } from "./dependencies";
-
-var DIContainer = new Container();
-DIContainer.bind<DependencyA>(DependencyA).toSelf();
-DIContainer.bind<DependencyB>(DependencyB).toSelf();
-
-export default DIContainer;
