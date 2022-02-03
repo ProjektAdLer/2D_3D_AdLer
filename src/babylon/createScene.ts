cffb5f6269ac29e9830f64db7b803418c514d39d
Marius Kaczmarek <kaczmarek.m@gmx.de>
2022-02-03 15:17:47 +0100

    resolved merge conflicts

--- src/babylon/createScene.ts
+++ src/babylon/createScene.ts
@@ -12,15 +12,6 @@ export interface CreateSceneModule {
 	default: CreateSceneClass;
 }
 
-<<<<<<< HEAD
-// This gets a Scene by a Name. This serves as an excample and can be ignord for now -PG
-export const getSceneModuleWithName = (
-  name?: string
-): Promise<CreateSceneClass> => {
-  return import("./scenes/Scene1").then((module: CreateSceneModule) => {
-    return module.default;
-  });
-=======
 // This gets a Scene by a Name. This serves as an exsample and can be ignord for now -PG
 export const getSceneModuleWithName = (
 	name?: string
@@ -28,21 +19,14 @@ export const getSceneModuleWithName = (
 	return import('./scenes/Scene1').then((module: CreateSceneModule) => {
 		return module.default;
 	});
->>>>>>> withTemplate
 };
 
 // Sollten wir in zukunft szenen dynamisch rein laden, kann man sie heir auch aus einem
 // als .babylon file importieren
 export const getSceneModule = (): Promise<CreateSceneClass> => {
-<<<<<<< HEAD
-  return import("./scenes/Scene1").then((module: CreateSceneModule) => {
-    return module.default;
-  });
-=======
 	return import(
 		'./components/2D3DCore/Presentation/MainScene/MainSceneView'
 	).then((module: CreateSceneModule) => {
 		return module.default;
 	});
->>>>>>> withTemplate
 };
--- src/react/App.tsx
+++ src/react/App.tsx
@@ -3,19 +3,11 @@ import "./App.css";
 import BabylonCanvas from "./components/BabylonCanvas";
 
 function App() {
-<<<<<<< HEAD:src/App.tsx
-  return (
-    <React.Fragment>
-      <BabylonCanvas style={{ width: "100%", position: "absolute" }} />
-    </React.Fragment>
-  );
-=======
 	return (
 		<React.Fragment>
 			<BabylonCanvas style={{ width: '100%', position: 'absolute' }} />
 		</React.Fragment>
 	);
->>>>>>> withTemplate:src/react/App.tsx
 }
 
 export default App;
