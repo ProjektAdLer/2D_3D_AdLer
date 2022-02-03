bbb33d41f7454fab011ff8506439ae0edef7f52d
philgei <philipp@geirhos.com>
2022-02-03 15:16:07 +0100

    added description to webpack overwrite

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,6 @@
-// This file is used together with react-scripts-rewire to overwrite the webpack config of your CRA App to add capability for
+// This file is used together with react-scripts-rewire to overwrite the webpack config of your CRA App to add capability for:
 // Typescript Decorators
+// TODO: have entry Point of App in src/React/Index.tsx
 
 const { override, addBabelPlugin } = require("customize-cra");
 const babelTsTransformPlugin = require("babel-plugin-transform-typescript-metadata");
