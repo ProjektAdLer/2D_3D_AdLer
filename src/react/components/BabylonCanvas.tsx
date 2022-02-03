9bfc13b3a08c29798ab9f3d195d8a9d4b3c82de1
Marius Kaczmarek <kaczmarek.m@gmx.de>
2022-02-03 17:43:31 +0100

    cleaned up BabylonCanvas

--- src/react/components/BabylonCanvas.tsx
+++ src/react/components/BabylonCanvas.tsx
@@ -1,10 +1,5 @@
 import {
-	Engine,
-	Scene,
-	ArcRotateCamera,
-	Vector3,
-	HemisphericLight,
-	MeshBuilder,
+	Engine
 } from '@babylonjs/core';
 import React, { useEffect, useRef } from 'react';
 import { getSceneModule } from '../../babylon/createScene';
@@ -12,12 +7,6 @@ import { getSceneModule } from '../../babylon/createScene';
 import '@babylonjs/loaders';
 
 import 'reflect-metadata';
-import { Service } from '../../babylon/DI/service';
-import DIContainer from '../../babylon/DI/container';
-
-const service: Service = DIContainer.resolve<Service>(Service);
-
-console.log(service.getAllNames());
 
 export default function BabylonCanvas(props: any) {
 	const canvasRef = useRef<HTMLCanvasElement>(null);
