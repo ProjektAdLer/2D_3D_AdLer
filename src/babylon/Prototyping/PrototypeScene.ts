ffc7b4b2be3e482c6e5a570b7058c073edd92b03
Lukas <lukas.schaab@th-ab.de>
2022-02-08 11:55:22 +0100

    Design 0.1 Video Modell in Prototyp Szene

--- src/babylon/Prototyping/PrototypeScene.ts
+++ src/babylon/Prototyping/PrototypeScene.ts
@@ -1,43 +1,66 @@
 import {
-	Engine,
-	FreeCamera,
-	Scene,
-	Vector3,
-	HemisphericLight,
-	GroundMesh,
-} from '@babylonjs/core';
-import '@babylonjs/inspector';
-import { CreateSceneClass } from '../CreateScene';
+  Engine,
+  FreeCamera,
+  Scene,
+  Vector3,
+  HemisphericLight,
+  GroundMesh,
+  SceneLoader,
+  MeshBuilder,
+  AnimationGroup,
+  Animation,
+} from "@babylonjs/core";
+
+// required imports
+import "@babylonjs/inspector";
+import "@babylonjs/loaders/glTF";
+import { CreateSceneClass } from "../CreateScene";
+
+// digital assets
+import roomModel from "../../Assets/AdLerDesign01.glb";
 
 export class PrototypeScene implements CreateSceneClass {
-	// Mit Pretasks können wir dinge erledigen, welche asyncron ablaufen und vor
-	// derm Rendern der Szene passieren sollen.
-	// In diesem Fall wird das Moodle System initialisiert, es können
-	// aber auch z.B. Modelle geladen werden
-	preTasks = [];
+  // Mit Pretasks können wir dinge erledigen, welche asyncron ablaufen und vor
+  // derm Rendern der Szene passieren sollen.
+  // In diesem Fall wird das Moodle System initialisiert, es können
+  // aber auch z.B. Modelle geladen werden
+  preTasks = [];
 
-	createScene = async (
-		engine: Engine,
-		canvas: HTMLCanvasElement
-	): Promise<Scene> => {
-		// Wichtig: Hier keinen Code reinschreiben, der die Szene erstellt, sondern nur die jeweilige Klasse erstellen
-		// und evtl positionieren oder anderweitig initialisieren / manipulieren - PG
-		const scene = new Scene(engine);
+  createScene = async (
+    engine: Engine,
+    canvas: HTMLCanvasElement
+  ): Promise<Scene> => {
+    // Wichtig: Hier keinen Code reinschreiben, der die Szene erstellt, sondern nur die jeweilige Klasse erstellen
+    // und evtl positionieren oder anderweitig initialisieren / manipulieren - PG
+    const scene = new Scene(engine);
 
-		var camera = new FreeCamera('camera1', new Vector3(0, 5, -10), scene);
+    var camera = new FreeCamera("camera1", new Vector3(-28, 24, -85.5), scene);
+    camera.setTarget(new Vector3(0, 0, -60));
+    camera.rotation = new Vector3(
+      (33 / 180) * Math.PI,
+      (-667 / 180) * Math.PI,
+      0
+    );
+    camera.fov = 0.358;
+    //camera.attachControl(canvas, true);
 
-		camera.setTarget(Vector3.Zero());
+    new HemisphericLight("light", new Vector3(0, 1, 0), scene);
 
-		camera.attachControl(canvas, true);
+    new GroundMesh("Ground", scene);
 
-		new HemisphericLight('light', new Vector3(0, 1, 0), scene);
+    const importResult = await SceneLoader.ImportMeshAsync(
+      "",
+      roomModel,
+      "",
+      scene,
+      undefined,
+      ".glb"
+    );
 
-		new GroundMesh('Ground', scene);
+    scene.debugLayer.show();
 
-		scene.debugLayer.show();
-
-		return scene;
-	};
+    return scene;
+  };
 }
 
 export default new PrototypeScene();
