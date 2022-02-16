34060aa4ed39626e423d434fae037ad5d965a635
philgei <philipp@geirhos.com>
2022-02-16 12:50:30 +0100

    added comment to core facotry

--- src/Babylon/Components/Core/API/CoreFactory.ts
+++ src/Babylon/Components/Core/API/CoreFactory.ts
@@ -3,6 +3,8 @@ import Core from "./Core";
 import ICoreFactory from "./ICoreFactory";
 import CoreDIContainer from "../DependencyInjection/CoreDIContainer";
 
+// This Factory is needed, because React does not allow us to use the DI container.
+
 export default class CoreFactory implements ICoreFactory {
   CreateCore = (): ICore => {
     return CoreDIContainer.get<Core>(Core);
