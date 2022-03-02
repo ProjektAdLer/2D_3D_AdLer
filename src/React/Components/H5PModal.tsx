7d99d3c5682f01da9d5ebe487afb1efe44b3b402
Philipp Geirhos <philipp@geirhos.com>
2022-03-02 18:55:06 +0100

    added Wasserfall h5p with broken css

--- src/React/Components/H5PModal.tsx
+++ src/React/Components/H5PModal.tsx
@@ -13,12 +13,12 @@ export default function H5PModal(props: any) {
         <div className="modal-body">
           {" "}
           <iframe
-            src="https://moodle.cluuub.xyz/h5p/embed.php?url=https%3A%2F%2Fmoodle.cluuub.xyz%2Fpluginfile.php%2F278%2Fmod_h5pactivity%2Fpackage%2F0%2FMetriken%2520Teil%25201.h5p&amp;preventredirect=1&amp;component=mod_h5pactivity"
-            // width=":w"
-            // height=":h"
+            src="http://moodle.cluuub.xyz/h5p/embed.php?url=https%3A%2F%2Fmoodle.cluuub.xyz%2Fpluginfile.php%2F278%2Fmod_h5pactivity%2Fpackage%2F0%2FMetriken%2520Teil%25201.h5p"
+            width=":w"
+            height="100%"
             // allowfullscreen="allowfullscreen"
             // class="h5p-player w-100 border-0"
-            style={{ height: "25px", width: "100%", border: "0" }}
+            style={{ height: "400px", width: "100%" }}
             // id="621fa7f7f1cfb621fa7f7dd88914-h5player"
           ></iframe>
         </div>
