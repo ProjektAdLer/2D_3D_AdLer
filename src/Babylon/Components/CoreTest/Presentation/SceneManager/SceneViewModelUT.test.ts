f346e4c69cfb1cd7b0556dcf9a9b26bb20393223
Marius Kaczmarek <kaczmarek.m@gmx.de>
2022-03-03 17:24:07 +0100

    added SceneViewModel test implementation

--- src/Babylon/Components/CoreTest/Presentation/SceneManager/SceneViewModelUT.test.ts
+++ src/Babylon/Components/CoreTest/Presentation/SceneManager/SceneViewModelUT.test.ts
@@ -1,8 +1,14 @@
-jest.mock("@babylon/core");
-
 import { Scene } from "@babylonjs/core/scene";
 import CoreDIContainer from "../../../Core/DependencyInjection/CoreDIContainer";
 import SceneViewModel from "../../../Core/Presentation/SceneManagment/SceneViewModel";
+
+jest.mock("@babylonjs/core/scene", () => {
+  return {
+    Scene: jest.fn().mockImplementation(() => {
+      return {};
+    }),
+  };
+});
 
 describe("SceneViewModel", () => {
   let sceneViewModel: SceneViewModel;
@@ -11,5 +17,9 @@ describe("SceneViewModel", () => {
     sceneViewModel = CoreDIContainer.get(SceneViewModel);
   });
 
-  test.todo("Scene setter sets a scene, getter retruns the scene");
+  test("Scene setter sets a scene, getter returns the scene", () => {
+    let testScene = new Scene();
+    sceneViewModel.Scene = testScene;
+    expect(sceneViewModel.Scene).toStrictEqual(testScene);
+  });
 });
