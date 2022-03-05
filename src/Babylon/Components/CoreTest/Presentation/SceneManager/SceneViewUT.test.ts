07e499bb3ec800e1eea09a0586a41c779472f120
Marius Kaczmarek <kaczmarek.m@gmx.de>
2022-03-05 13:22:41 +0100

    added tests for SceneView class

---
+++ src/Babylon/Components/CoreTest/Presentation/SceneManager/SceneViewUT.test.ts
@@ -0,0 +1,26 @@
+import { Engine } from "@babylonjs/core";
+import CoreDIContainer from "../../../Core/DependencyInjection/CoreDIContainer";
+import CORE_TYPES from "../../../Core/DependencyInjection/types";
+import SceneView from "../../../Core/Presentation/SceneManagment/SceneView";
+
+jest.mock("@babylonjs/core");
+
+const runRenderLoopMock = jest.spyOn(Engine.prototype, "runRenderLoop");
+
+describe("SceneView", () => {
+  let sceneView: SceneView;
+
+  beforeEach(() => {
+    sceneView = CoreDIContainer.get(CORE_TYPES.ISceneView);
+  });
+
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+
+  test("createRenderLoop call the babylon engine", () => {
+    sceneView.createRenderLoop(new Engine(document.createElement("canvas")));
+    expect(runRenderLoopMock).toHaveBeenCalledTimes(1);
+    // todo: maybe extend this to expect to be called with another function as argument -MK
+  });
+});
