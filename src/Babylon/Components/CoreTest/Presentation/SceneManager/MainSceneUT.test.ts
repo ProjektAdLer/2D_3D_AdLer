6701c126a0458e4a999fdfa013a85cce56295c7a
Marius Kaczmarek <kaczmarek.m@gmx.de>
2022-03-05 13:22:22 +0100

    added tests for MainScene class

---
+++ src/Babylon/Components/CoreTest/Presentation/SceneManager/MainSceneUT.test.ts
@@ -0,0 +1,26 @@
+import { DebugLayer, Engine, Scene } from "@babylonjs/core";
+import MainScene from "../../../Core/Presentation/SceneManagment/MainScene";
+
+jest.mock("@babylonjs/core");
+
+describe("MainScene", () => {
+  let mainScene: MainScene;
+
+  beforeEach(() => {
+    mainScene = new MainScene();
+  });
+
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+
+  test("createScene returns an object of type Scene", () => {
+    let canvas = document.createElement("canvas");
+    let engine = new Engine(canvas);
+
+    // this will break when the debugLayer is called in createScene
+    mainScene.createScene(engine, canvas).then((data) => {
+      expect(data).toBeInstanceOf(Scene);
+    });
+  });
+});
