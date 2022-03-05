3503bae5d1feb5ace5589e182d8854ef528427ce
Marius Kaczmarek <kaczmarek.m@gmx.de>
2022-03-05 13:34:11 +0100

    added tests for EngineManager class

---
+++ src/Babylon/Components/CoreTest/Presentation/EngineManager/EngineManagerUT.test.ts
@@ -0,0 +1,29 @@
+import { Engine } from "@babylonjs/core";
+import EngineManager from "../../../Core/Presentation/EngineManager/EngineManager";
+
+jest.mock("@babylonjs/core");
+
+describe("EngineManager", () => {
+  let engineManager: EngineManager;
+
+  beforeEach(() => {
+    engineManager = new EngineManager();
+  });
+
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+
+  test.todo("test createEngine");
+
+  test("Engine getter returns an object of type Engine after Engine is created", () => {
+    engineManager.createEngine(document.createElement("canvas"));
+    expect(engineManager.Engine).toBeInstanceOf(Engine);
+  });
+
+  test("Engine getter throws error when Engine wasn't created", () => {
+    expect(() => {
+      engineManager.Engine;
+    }).toThrowError();
+  });
+});
