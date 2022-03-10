1563323220431b87cd238c43b3ea8b427ef57c37
Philipp Geirhos <philipp@geirhos.com>
2022-03-10 12:33:30 +0100

    Added: Lorem Imsum hor H5P Footer

--- src/React/Components/H5PModal.tsx
+++ src/React/Components/H5PModal.tsx
@@ -80,6 +80,12 @@ export default function H5PModal(props: {
           <button onClick={props.onClose} className="button">
             close
           </button>
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum.
+          </p>
         </div>
       </div>
     </div>
