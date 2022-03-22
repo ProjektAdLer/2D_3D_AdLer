1d5627e9771d096dec9ed2b60e04c0d5eb0ede8b
splican <daniel.glaab@th-ab.de>
2022-03-22 14:11:24 +0100

    started to merge stylesheet css to inline tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blacktrans50: "rgba(0,0,0,0.5)", //50% schwarz für Modal-Hintergrund
+      },
+    },
   },
   plugins: [],
 };
