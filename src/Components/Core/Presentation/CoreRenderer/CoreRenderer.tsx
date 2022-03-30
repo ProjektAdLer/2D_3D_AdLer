fffae57eb1a0b1bf2c466df8ce645c9d706c4d5d
Marius Kaczmarek <kaczmarek.m@gmx.de>
2022-03-30 10:21:02 +0200

    added class name to CoreRenderer

--- src/Components/Core/Presentation/CoreRenderer/CoreRenderer.tsx
+++ src/Components/Core/Presentation/CoreRenderer/CoreRenderer.tsx
@@ -7,7 +7,7 @@ import { Provider } from "inversify-react";
 import CoreDIContainer from "../../DependencyInjection/CoreDIContainer";
 
 @injectable()
-export default class implements ICoreRenderer {
+export default class CoreRenderer implements ICoreRenderer {
   setupReact(): void {
     ReactDOM.render(
       <React.StrictMode>
