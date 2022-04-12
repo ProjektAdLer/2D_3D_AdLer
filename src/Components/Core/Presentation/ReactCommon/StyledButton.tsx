4b54902acded13c75c71093a8f990de90c11249b
philgei <philipp@geirhos.com>
2022-04-12 14:10:15 +0200

    fixed spreading of pros in Styled Components

--- src/Components/Core/Presentation/ReactCommon/StyledButton.tsx
+++ src/Components/Core/Presentation/ReactCommon/StyledButton.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 
-export default function StyledButton(
-  props: React.HTMLProps<HTMLButtonElement>
-) {
+export default function StyledButton({
+  children,
+  className,
+  ...rest
+}: React.HTMLAttributes<HTMLButtonElement>) {
   return (
     <button
       className={
-        props.className +
+        className +
         "container-button w-3/4 hover:cursor-pointer text-sm lg:text-xl border-b-4 border-r-4 border-adlerdarkblue active:border-0 m-auto py-1 px-2 bg-adlerblue rounded-lg font-black text-white"
       }
+      {...rest}
     >
-      {props.children}
+      {children}
     </button>
   );
 }
