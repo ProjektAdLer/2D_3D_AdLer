98d1fc72f18ec733141a5b7c612bb435696924bf
Marius Kaczmarek <kaczmarek.m@gmx.de>
2022-04-21 14:33:13 +0200

    fixed broken references to removed interface

--- src/Components/Core/Presentation/LearningElementPanel/LearningElementPanelViewModel.ts
+++ src/Components/Core/Presentation/LearningElementPanel/LearningElementPanelViewModel.ts
@@ -1,9 +1,8 @@
 import { injectable } from "inversify";
 import ObservablePrimitive from "../../BusinessLogic/EntityManager/Observables/ObservablePrimitive";
-import IViewModel from "../ViewModelProvider/IViewModel";
 
 @injectable()
-export default class LearningElementPanelViewModel implements IViewModel {
+export default class LearningElementPanelViewModel {
   public text: ObservablePrimitive<string> = new ObservablePrimitive<string>(
     "LE Panel"
   );
--- src/Components/Core/Presentation/ViewModelProvider/IViewModelProvider.ts
+++ src/Components/Core/Presentation/ViewModelProvider/IViewModelProvider.ts
@@ -1,12 +1,10 @@
-import IViewModel from "./IViewModel";
-
 export default interface IViewModelProvider {
   /**
    * Registers a callback for all view models of the given type. The callback is called every time a new view model of the given type is registered.
    * @param callback The callback to be called when a new view model is registered.
    * @param viewModelClass The type of view model to register for.
    */
-  registerRequest<T extends IViewModel>(
+  registerRequest<T>(
     callback: (viewModels: T[]) => void,
     viewModelClass: { new (): T }
   ): void;
@@ -16,7 +14,7 @@ export default interface IViewModelProvider {
    * @param callback The callback to be cancelled.
    * @param viewModelClass The type of view model to cancel the registration for.
    */
-  cancelRequest<T extends IViewModel>(
+  cancelRequest<T>(
     callback: (viewModels: T[]) => void,
     viewModelClass: { new (): T }
   ): void;
@@ -26,18 +24,12 @@ export default interface IViewModelProvider {
    * @param viewModel The view model to register.
    * @param viewModelClass The type of the registered view model.
    */
-  registerViewModel<T extends IViewModel>(
-    viewModel: T,
-    viewModelClass: { new (): T }
-  ): void;
+  registerViewModel<T>(viewModel: T, viewModelClass: { new (): T }): void;
 
   /**
    * Removes a view model from the provider.
    * @param viewModel The view model to remove.
    * @param viewModelClass The type of the removed view model.
    */
-  removeViewModel<T extends IViewModel>(
-    viewModel: T,
-    viewModelClass: { new (): T }
-  ): void;
+  removeViewModel<T>(viewModel: T, viewModelClass: { new (): T }): void;
 }
