59181cfaefdf53000eece83f6bb9b44e0e39d7f9
philgei <philipp@geirhos.com>
2022-05-02 16:30:37 +0200

    added: Unit Tests for Entity Manager

---
+++ src/Components/CoreTest/Domain/EntityContainer/EntityContainerUT.test.ts
@@ -0,0 +1,84 @@
+import { TestEntity, TestEntity2 } from "./TestEntities";
+import CoreDIContainer from "../../../Core/DependencyInjection/CoreDIContainer";
+import CORE_TYPES from "../../../Core/DependencyInjection/CoreTypes";
+import IEntityContainer from "../../../Core/Domain/EntityContainer/IEntityContainer";
+
+describe("EntityManager", () => {
+  beforeEach(() => {
+    CoreDIContainer.snapshot();
+  });
+
+  afterEach(() => {
+    CoreDIContainer.restore();
+  });
+
+  test("The Root Entity of the Entity Manager is always defined", () => {
+    const container = getContainer();
+    expect(container.getRootEntity()).toBeDefined();
+  });
+
+  test("getEntitiesofType returns the propper Entities as an Array in correct order", () => {
+    const container = getContainer();
+
+    const entity1 = container.createEntity<TestEntity>(
+      {
+        test1: "entity1",
+      },
+      TestEntity
+    );
+    const entity2 = container.createEntity<TestEntity>(
+      {
+        test1: "entity2",
+      },
+      TestEntity
+    );
+
+    expect(container.getEntitiesOfType(TestEntity)).toEqual([entity1, entity2]);
+  });
+
+  test("getEntityOfType returns Empty array if no Entity is found", () => {
+    const container = getContainer();
+
+    const entity1 = container.createEntity<TestEntity>(
+      {
+        test1: "entity1",
+      },
+      TestEntity
+    );
+    const entity2 = container.createEntity<TestEntity>(
+      {
+        test1: "entity2",
+      },
+      TestEntity
+    );
+
+    expect(container.getEntitiesOfType(TestEntity2)).toEqual([]);
+  });
+
+  test("New Entities are Stored Correctly", () => {
+    const container = getContainer();
+
+    container.createEntity<TestEntity>({}, TestEntity);
+    container.createEntity<TestEntity>({}, TestEntity);
+
+    container.createEntity<TestEntity2>({}, TestEntity2);
+
+    expect(container.getEntitiesOfType(TestEntity).length).toBe(2);
+    expect(container.getEntitiesOfType(TestEntity2).length).toBe(1);
+  });
+
+  test("DeleteEntity deletes the right Entity", () => {
+    const container = getContainer();
+
+    const entity1 = container.createEntity<TestEntity>({}, TestEntity);
+    const entity2 = container.createEntity<TestEntity>({}, TestEntity);
+
+    container.deleteEntity(entity1);
+
+    expect(container.getEntitiesOfType(TestEntity).length).toBe(1);
+    expect(container.getEntitiesOfType(TestEntity)[0]).toBe(entity2);
+  });
+});
+function getContainer() {
+  return CoreDIContainer.get<IEntityContainer>(CORE_TYPES.IEntityContainer);
+}
---
+++ src/Components/CoreTest/Domain/EntityContainer/TestEntities.ts
@@ -0,0 +1,10 @@
+import AbstractEntity from "../../../Core/Domain/Entities/AbstractEntity";
+
+export class TestEntity extends AbstractEntity {
+  test1: string;
+  test2: boolean;
+}
+
+export class TestEntity2 extends AbstractEntity {
+  test1: string;
+}
