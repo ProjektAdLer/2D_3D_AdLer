e9a1e697e555227358f63c4cfe79803e3f274130
Marius Kaczmarek <kaczmarek.m@gmx.de>
2022-05-05 17:50:10 +0200

    fixed room tests

--- src/Components/CoreTest/Presentation/Room/RoomControllerUT.test.ts
+++ src/Components/CoreTest/Presentation/Room/RoomControllerUT.test.ts
@@ -1,39 +1,11 @@
-import CoreDIContainer from "../../../Core/DependencyInjection/CoreDIContainer";
-import CORE_TYPES from "../../../Core/DependencyInjection/CoreTypes";
 import LearningRoomController from "../../../Core/Presentation/LearningRoom/LearningRoomController";
-import LearningRoomView from "../../../Core/Presentation/LearningRoom/LearningRoomView";
-import SceneController from "../../../Core/Presentation/SceneManagment/SceneController";
-
-const SceneGetterMock = jest.spyOn(SceneController.prototype, "Scene", "get");
-const DisplayRoomViewMock = jest.spyOn(
-  LearningRoomView.prototype,
-  "displayRoom"
-);
-// const CreateWallsViewMock = jest.spyOn(RoomView.prototype, "createWalls");
 
 describe("RoomController", () => {
   let learningRoomController: LearningRoomController;
 
   beforeEach(() => {
-    learningRoomController = CoreDIContainer.get(
-      CORE_TYPES.ILearningRoomController
-    );
+    learningRoomController = new LearningRoomController();
   });
 
-  afterAll(() => {
-    jest.restoreAllMocks();
-  });
-  //TODO: Update Room tests when Rooms are reimplemented
-
-  test("displayRoom calls createFloor.", () => {
-    learningRoomController.view.displayRoom();
-    expect(SceneGetterMock).toBeCalledTimes(1);
-    expect(DisplayRoomViewMock).toBeCalledTimes(1);
-  });
-
-  // test("createWalls calls createWalls.", () => {
-  //   roomController.createWalls();
-  //   expect(SceneGetterMock).toBeCalledTimes(1);
-  //   expect(CreateWallsViewMock).toBeCalledTimes(1);
-  // });
+  test.todo("write tests when controller has functionality");
 });
