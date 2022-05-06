7d2e4fa707a6190f6e995e1e8b9701db76f6dcc2
Marius Kaczmarek <kaczmarek.m@gmx.de>
2022-05-06 13:21:02 +0200

    added comments to presentation director interface

--- src/Components/Core/Presentation/PresentationBuilder/IPresentationDirector.ts
+++ src/Components/Core/Presentation/PresentationBuilder/IPresentationDirector.ts
@@ -1,6 +1,19 @@
 import IPresentationBuilder from "./IPresentationBuilder";
 
+/**
+ * @interface IPresentationDirector
+ * @description The Presentation Director directs the build process of View, Controller, Presenter and ViewModel with a specific builder.
+ */
 export default interface IPresentationDirector {
+  /**
+   * @method build
+   * @description Executes the build process with the set builder. Results can be accessed via the builder.
+   */
   build(): void;
+
+  /**
+   * @method setBuilder
+   * @description Sets the builder to be used for the build process.
+   */
   set Builder(newPresentationBuilder: IPresentationBuilder);
 }
