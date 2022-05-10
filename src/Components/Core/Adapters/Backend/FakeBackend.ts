3b07b724c228ee7b996dab4350d827c1c5aa3cba
philgei <philipp@geirhos.com>
2022-05-10 16:15:36 +0200

    added learningelements and learningrooms to backend

--- src/Components/Core/Adapters/Backend/FakeBackend.ts
+++ src/Components/Core/Adapters/Backend/FakeBackend.ts
@@ -2,16 +2,83 @@ import { APIWorldTo } from "./APIWorldTO";
 import { injectable } from "inversify";
 import { type IBackend } from "./IBackend";
 
+import { APILearningRoomTO } from "./APILearningRoomTO";
+import { APILearningElementTO } from "./APILearningElementTO";
+
 @injectable()
 export class FakeBackend implements IBackend {
-  getWorld(): Promise<APIWorldTo> {
+  getWorld(): Promise<Partial<APIWorldTo>> {
     return new Promise((resolve) => {
       setTimeout(() => {
-        resolve({
-          name: "Fake World from FakeApi",
-          description: "This is a fake world",
-        });
+        resolve(learningWorld);
       }, 1000);
     });
   }
+
+  getLearningRooms(): Promise<APILearningRoomTO[]> {
+    return Promise.resolve(learningRooms);
+  }
+
+  getLearningElements(): Promise<(APILearningElementTO | undefined)[]> {
+    return Promise.resolve(learningElements);
+  }
 }
+
+const learningWorld = {
+  name: "Fake World from FakeApi",
+  description: "This is a fake world",
+  learningRoommIds: [1],
+};
+
+const learningRooms = [
+  {
+    id: 1,
+    name: "Room 1",
+    learningElementIds: [2, 3],
+  },
+];
+
+const learningElements = [
+  {
+    id: 2,
+    identifier: {
+      type: "name",
+      value: "LOC complete",
+    },
+    elementType: "H5P",
+    value: [
+      {
+        type: "points",
+        value: 10,
+      },
+    ],
+    requirements: [
+      {
+        type: "points",
+        value: 5,
+      },
+    ],
+    name: "H5P Lernelement",
+  },
+  {
+    id: 3,
+    identifier: {
+      type: "name",
+      value: "LOC complete",
+    },
+    elementType: "Text",
+    value: [
+      {
+        type: "points",
+        value: 10,
+      },
+    ],
+    requirements: [
+      {
+        type: "points",
+        value: 5,
+      },
+    ],
+    name: "Text Lernelement",
+  },
+];
--- src/Components/Core/Adapters/Backend/IBackend.ts
+++ src/Components/Core/Adapters/Backend/IBackend.ts
@@ -1,5 +1,12 @@
+import { APILearningRoomTO } from "./APILearningRoomTO";
+
 import { APIWorldTo } from "./APIWorldTO";
+import { APILearningElementTO } from "./APILearningElementTO";
 
 export interface IBackend {
-  getWorld(): Promise<APIWorldTo>; // Promise<IWorld>
+  getWorld(): Promise<Partial<APIWorldTo>>; // Promise<IWorld>
+
+  getLearningRooms(): Promise<Partial<APILearningRoomTO[]>>;
+
+  getLearningElements(): Promise<Partial<APILearningElementTO[]>>;
 }
