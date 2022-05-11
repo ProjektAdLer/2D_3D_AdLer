b15cab0fe9613636b1a0988284b4ed3d34c940fa
splican <daniel2105@gmx.net>
2022-05-11 14:28:08 +0200

    styled videocomponent

--- src/Components/Core/Presentation/React/LearningElementModal/VideoComponent.tsx
+++ src/Components/Core/Presentation/React/LearningElementModal/VideoComponent.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 
 const VideoComponent = (props: { embedId: string }) => (
-  <div>
+  <div className="lg:w-full w-5/6 aspect-video">
     <iframe
-      width="853"
-      height="480"
       src={`https://www.youtube.com/embed/${props.embedId}`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
