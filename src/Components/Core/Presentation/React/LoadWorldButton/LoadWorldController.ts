615d1d9746bd614f495030c29a969a0511ee930f
Philipp Geirhos <philipp@geirhos.com>
2022-05-12 13:11:12 +0200

    removed old ugly code

--- src/Components/Core/Presentation/React/LoadWorldButton/LoadWorldController.ts
+++ src/Components/Core/Presentation/React/LoadWorldButton/LoadWorldController.ts
@@ -3,7 +3,6 @@ import { injectable } from "inversify";
 import CoreDIContainer from "../../../DependencyInjection/CoreDIContainer";
 import ILoadWorldController from "./ILoadWorldController";
 import CORE_TYPES from "../../../DependencyInjection/CoreTypes";
-import LearningWorldViewModel from "../../Ports/LearningWorldPort/LearningWorldViewModel";
 import IViewModelProvider from "../../ViewModelProvider/IViewModelProvider";
 import USECASE_TYPES from "../../../DependencyInjection/UseCases/USECASE_SYMBOLS";
 
@@ -13,20 +12,6 @@ export class LoadWorldController implements ILoadWorldController {
     const useCase = CoreDIContainer.get<ILoadWorldUseCase>(
       USECASE_TYPES.ILoadWorldUseCase
     );
-
-    const provider = CoreDIContainer.get<IViewModelProvider>(
-      CORE_TYPES.IViewModelProvider
-    );
-
-    let viewModel: LearningWorldViewModel;
-
-    // This is ugly AF, but it is the only way to access the VM thorugh the VMProvider. Marius please fix :)
-    provider.registerRequest((value) => {
-      viewModel = value[0];
-    }, LearningWorldViewModel);
-
-    //@ts-ignore
-    // viewModel.worldNameLoading.setValue(true);
 
     await useCase.executeAsync();
   }
