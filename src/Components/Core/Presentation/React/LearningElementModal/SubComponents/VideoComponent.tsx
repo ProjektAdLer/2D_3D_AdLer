96cdd6b7a2dc5039a01e22426dfffe89acdb544c
splican <daniel2105@gmx.net>
2022-05-19 08:39:34 +0200

    changed component order in app.tsx

--- src/Components/Core/Presentation/React/LearningElementModal/SubComponents/VideoComponent.tsx
+++ src/Components/Core/Presentation/React/LearningElementModal/SubComponents/VideoComponent.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 const VideoComponent = (props: { embedId: string }) => (
-  <div className="w-3/5  aspect-video">
+  <div className="w-3/5 aspect-video">
     <iframe
-      className="rounded-lg  w-full h-full"
+      className="rounded-lg w-full h-full"
       src={`https://www.youtube.com/embed/${props.embedId}`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
