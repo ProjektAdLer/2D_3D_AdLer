9f052724d9dc26787008cf12eea5ec9777dd515e
Marius Kaczmarek <kaczmarek.m@gmx.de>
2022-05-23 12:35:31 +0200

    refactored door presenter

--- src/Components/Core/Presentation/Babylon/Door/DoorPresenter.ts
+++ src/Components/Core/Presentation/Babylon/Door/DoorPresenter.ts
@@ -3,16 +3,17 @@ import DoorViewModel from "./DoorViewModel";
 import IDoorPresenter from "./IDoorPresenter";
 
 export default class DoorPresenter implements IDoorPresenter {
-  constructor(private viewModel: DoorViewModel) {}
+  constructor(private viewModel: DoorViewModel) {
+    if (!this.viewModel) {
+      throw new Error("ViewModel was passed as undefined");
+    }
+  }
+
   openDoor(): void {
     this.viewModel.isOpen.Value = true;
   }
 
   presentDoor(position: [Vector3, number]): void {
-    if (!this.viewModel) {
-      throw new Error("ViewModel not set");
-    }
-
     this.viewModel.position.Value = position[0];
     this.viewModel.rotation.Value = position[1];
     this.viewModel.isVisible.Value = true;
