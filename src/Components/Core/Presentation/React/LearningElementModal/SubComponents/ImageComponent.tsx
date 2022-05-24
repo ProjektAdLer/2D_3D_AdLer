d466043584eb8ecd80fdeb51909c621bc16c305c
Lizard Guard <felixkleis@yahoo.de>
2022-05-24 10:29:04 +0200

    Fixed picture learning component styling

--- src/Components/Core/Presentation/React/LearningElementModal/SubComponents/ImageComponent.tsx
+++ src/Components/Core/Presentation/React/LearningElementModal/SubComponents/ImageComponent.tsx
@@ -1,6 +1,6 @@
 export default function ImageComponent(props: { imagesrc: string }) {
   return (
-    <div className="flex justify-center items-top max-h-90pro sm:w-[300px] md:w-[315px] lg:w-[700px] xl:w-[900px] sm:w-[1100px]">
+    <div className="flex justify-center items-top max-h-90pro sm:w-[300px] md:w-[315px] lg:w-[700px] xl:w-[900px] 2xl:w-[1100px]">
       <img
         className="sm:max-w-[300px] md:max-w-[315px] lg:max-w-[700px] xl:max-w-[900px] 2xl:max-w-[1100px]"
         alt="LearningImage!"
