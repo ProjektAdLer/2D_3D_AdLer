0198517e4bc4b88e6c13fb52b00aeb61382c1e1c
Lizard Guard <felixkleis@yahoo.de>
2022-06-15 17:27:04 +0200

    Exchanged door model again, defnitely the last time...

--- src/Components/Core/Presentation/Babylon/LearningRoom/LearningRoomViewModel.ts
+++ src/Components/Core/Presentation/Babylon/LearningRoom/LearningRoomViewModel.ts
@@ -12,7 +12,7 @@ export default class LearningRoomViewModel {
   public roomLength: Observable<number> = new Observable(10);
   public baseHeight: Observable<number> = new Observable(0.5);
   public roomHeight: Observable<number> = new Observable(1.5);
-  public doorWidth: Observable<number> = new Observable(1.1);
+  public doorWidth: Observable<number> = new Observable(1.0);
   public doorHeight: Observable<number> = new Observable(2.19);
   public wallThickness: Observable<number> = new Observable(0.3);
 
