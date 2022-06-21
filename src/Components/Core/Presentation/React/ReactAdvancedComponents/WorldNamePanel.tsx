2f38f10bf7c38336bb338ee78ea053a66d5a724e
splican <daniel2105@gmx.net>
2022-06-21 08:42:59 +0200

    changed StyledContainer

--- src/Components/Core/Presentation/React/ReactAdvancedComponents/WorldNamePanel.tsx
+++ src/Components/Core/Presentation/React/ReactAdvancedComponents/WorldNamePanel.tsx
@@ -16,7 +16,7 @@ export default function WorldNamePanel() {
   return (
     <div className="flex justify-center">
       {(worldNameLoading || worldName) && (
-        <StyledContainer className="p-13 text-4xl text-white whitespace-nowrap bg-adlergold">
+        <StyledContainer className="fixed p-13 text-4xl text-white whitespace-nowrap bg-adlergold">
           <LearningWorldComponent
             worldName={worldName}
             worldNameLoading={worldNameLoading}
