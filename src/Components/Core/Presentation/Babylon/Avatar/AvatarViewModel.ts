dd0835127e039ee054a54c04f0a470a958cdfdc3
Marius Kaczmarek <kaczmarek.m@gmx.de>
2022-06-21 11:23:15 +0200

    updated navigation parameters

--- src/Components/Core/Presentation/Babylon/Avatar/AvatarViewModel.ts
+++ src/Components/Core/Presentation/Babylon/Avatar/AvatarViewModel.ts
@@ -6,8 +6,8 @@ export default class AvatarViewModel {
 
   public agentIndex: number;
   public agentParams: IAgentParameters = {
-    radius: 0.1,
-    height: 0.2,
+    radius: 0.5,
+    height: 1,
     maxAcceleration: 4.0,
     maxSpeed: 1.0,
     collisionQueryRange: 0.5,
