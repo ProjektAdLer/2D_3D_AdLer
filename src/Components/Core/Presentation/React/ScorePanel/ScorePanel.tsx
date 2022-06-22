bb30207475e87111271ef6ff9d7523f450ee6f61
splican <daniel2105@gmx.net>
2022-06-22 09:36:08 +0200

    fixed ScorePanel placement

--- src/Components/Core/Presentation/React/ScorePanel/ScorePanel.tsx
+++ src/Components/Core/Presentation/React/ScorePanel/ScorePanel.tsx
@@ -11,12 +11,14 @@ export default function ScorePanel() {
   const [score] = useObservable<number>(viewModels[0]?.score);
 
   return (
-    <StyledContainer className="text-4xl text-white text-shadow font-extrabold bg-transparent">
-      {score ? score : 0}
-      <img
-        src="icons/coin_icon.svg"
-        className="xl:w-16 lg:w-10 md:w-8 sm:w-8"
-      ></img>
-    </StyledContainer>
+    <div className="grid justify-end">
+      <StyledContainer className="text-lg lg:text-4xl text-white text-shadow font-extrabold bg-transparent">
+        {score ? score : 0}
+        <img
+          src="icons/coin_icon.svg"
+          className="xl:w-16 lg:w-10 md:w-8 sm:w-8"
+        ></img>
+      </StyledContainer>
+    </div>
   );
 }
