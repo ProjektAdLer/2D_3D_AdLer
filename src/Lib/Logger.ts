f309aa7f4017d349d017eb2d492e9a7b7343937a
Philipp Geirhos <philipp@geirhos.com>
2022-06-29 18:04:17 +0200

    added logger

---
+++ src/Lib/Logger.ts
@@ -0,0 +1,48 @@
+interface LogFn {
+  (message?: any, ...optionalParams: any[]): void;
+}
+
+interface Logger {
+  log: LogFn;
+  warn: LogFn;
+  error: LogFn;
+}
+
+type LogLevel = "log" | "warn" | "error";
+
+const defaultLogLevel = "warn";
+
+const NO_OP: LogFn = (message?: any, ...optionalParams: any[]) => {};
+
+export class ConsoleLogger implements Logger {
+  readonly log: LogFn;
+  readonly warn: LogFn;
+  readonly error: LogFn;
+
+  constructor(options?: { level?: LogLevel }) {
+    const { level } = options || {};
+
+    this.error = console.error.bind(console, "[ERROR] ");
+
+    if (level === "error") {
+      this.warn = NO_OP;
+      this.log = NO_OP;
+
+      return;
+    }
+
+    this.warn = console.warn.bind(console, "[WARNING] ");
+
+    if (level === "warn") {
+      this.log = NO_OP;
+
+      return;
+    }
+
+    this.log = console.trace.bind(console, "[LOG] ");
+  }
+}
+
+export const logger = new ConsoleLogger({
+  level: (process.env.REACT_APP_LOGLEVEL || defaultLogLevel) as LogLevel,
+});
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,12 @@ import CoreDIContainer from "./Components/Core/DependencyInjection/CoreDIContain
 import CORE_TYPES from "./Components/Core/DependencyInjection/CoreTypes";
 import IReactEntry from "./Components/Core/Presentation/React/ReactEntryPoint/IReactEntry";
 import "./Components/Core/Types/array.extensions";
+import { logger } from "./Lib/Logger";
 
 const reactEntry = CoreDIContainer.get<IReactEntry>(CORE_TYPES.ICoreRenderer);
 
 reactEntry.setupReact();
+
+logger.warn("This is a warning");
+logger.error("This is an error");
+logger.log("This is a log");
