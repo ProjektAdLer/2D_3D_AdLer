48176fb3867639471aaaaf8ae5df915225e5825e
philgei <philipp@geirhos.com>
2022-07-02 00:39:15 +0200

    fixed log user in usecase

--- src/Components/CoreTest/Application/LogUserIntoMoodle/LogUserIntoMoodleUseCase.test.ts
+++ src/Components/CoreTest/Application/LogUserIntoMoodle/LogUserIntoMoodleUseCase.test.ts
@@ -2,9 +2,12 @@ import { injectable } from "inversify";
 import LogUserIntoMoodleUseCase from "../../../Core/Application/LogUserIntoMoodle/LogUserIntoMoodleUseCase";
 import CoreDIContainer from "../../../Core/DependencyInjection/CoreDIContainer";
 import CORE_TYPES from "../../../Core/DependencyInjection/CoreTypes";
+import PORT_TYPES from "../../../Core/DependencyInjection/Ports/PORT_TYPES";
 import USECASE_TYPES from "../../../Core/DependencyInjection/UseCases/USECASE_SYMBOLS";
 import UserDataEntity from "../../../Core/Domain/Entities/UserData";
 import EntityContainer from "../../../Core/Domain/EntityContainer/EntityContainer";
+import DebugPort from "../../../Core/Ports/DebugPort/DebugPort";
+import IMoodlePort from "../../../Core/Ports/MoodlePort/IMoodlePort";
 import MoodlePort from "../../../Core/Ports/MoodlePort/MoodlePort";
 
 @injectable()
@@ -16,13 +19,42 @@ class BackendMock implements IBackend {
 }
 
 const createEntityMock = jest.spyOn(EntityContainer.prototype, "createEntity");
-const debug_DisplayUserTokenMock = jest.spyOn(
-  MoodlePort.prototype,
-  "debug_DisplayUserToken"
-);
+
+@injectable()
+//@ts-ignore
+class MoodlePortMock implements IMoodlePort {
+  registerMoodleLoginPresenter = jest.fn();
+  registerMoodleLoginButtonPresenter = jest.fn();
+  displayLoginForm = jest.fn();
+  loginSuccessful = jest.fn();
+  debug_DisplayUserToken = jest.fn();
+}
+
+@injectable()
+//@ts-ignore
+class DebugPortMock implements IDebugPort {
+  registerDebugPanelPresenter = jest.fn();
+  setUserToken = jest.fn();
+}
 
 describe("LogUserIntoMoodleUseCase", () => {
   let useCase: LogUserIntoMoodleUseCase;
+
+  beforeAll(() => {
+    CoreDIContainer.unbind(PORT_TYPES.IMoodlePort);
+    CoreDIContainer.bind(PORT_TYPES.IMoodlePort).to(MoodlePortMock);
+
+    CoreDIContainer.unbind(PORT_TYPES.IDebugPort);
+    CoreDIContainer.bind(PORT_TYPES.IDebugPort).to(DebugPortMock);
+  });
+
+  afterAll(() => {
+    CoreDIContainer.unbind(PORT_TYPES.IMoodlePort);
+    CoreDIContainer.bind(PORT_TYPES.IMoodlePort).to(MoodlePort);
+
+    CoreDIContainer.unbind(PORT_TYPES.IDebugPort);
+    CoreDIContainer.bind(PORT_TYPES.IDebugPort).to(DebugPort);
+  });
 
   beforeEach(() => {
     CoreDIContainer.snapshot();
@@ -39,7 +71,7 @@ describe("LogUserIntoMoodleUseCase", () => {
     CoreDIContainer.restore();
   });
 
-  test("executeAsync calls the backend", async () => {
+  test("executeAsync calls the backend and stores correct user data in Enity", async () => {
     BackendMock.prototype.logInUser = jest
       .fn()
       .mockImplementation((username: string, password: string) =>
@@ -51,15 +83,15 @@ describe("LogUserIntoMoodleUseCase", () => {
       password: "password",
     });
 
-    expect(BackendMock.prototype.logInUser).toHaveBeenCalledWith(
-      "username",
-      "password"
-    );
+    expect(BackendMock.prototype.logInUser).toHaveBeenCalledWith({
+      username: "username",
+      password: "password",
+    });
+
     expect(createEntityMock).toHaveBeenCalledWith(
       {
         userToken: "token",
         username: "username",
-        password: "password",
         isLoggedIn: true,
       },
       UserDataEntity
