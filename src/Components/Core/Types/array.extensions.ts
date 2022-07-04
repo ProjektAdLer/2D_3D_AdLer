b7dd5a9e75666510cdbadb349b4f78ab1eb14bc2
Philipp Geirhos <philipp@geirhos.com>
2022-07-04 11:11:36 +0200

    added documentation for array extentions

--- src/Components/Core/Types/array.extensions.ts
+++ src/Components/Core/Types/array.extensions.ts
@@ -4,10 +4,15 @@ interface Array {
   first(): any;
 }
 
+/**
+ * gets the last element of an array
+ */
 Array.prototype.last = function () {
   return this[this.length - 1];
 };
-
+/**
+ * gets the first element of an array
+ */
 Array.prototype.first = function () {
   return this[0];
 };
