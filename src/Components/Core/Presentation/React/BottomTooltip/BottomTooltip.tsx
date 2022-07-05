a042456ae0e9c5de377abd56d0ed26fa34d64ec9
Philipp Geirhos <philipp@geirhos.com>
2022-07-05 12:23:44 +0200

    changed icons

--- src/Components/Core/Presentation/React/BottomTooltip/BottomTooltip.tsx
+++ src/Components/Core/Presentation/React/BottomTooltip/BottomTooltip.tsx
@@ -3,6 +3,11 @@ import useViewModelControllerProvider from "../CustomHooks/useViewModelControlle
 import StyledContainer from "../ReactBaseComponents/StyledContainer";
 import BottomTooltipViewModel from "./BottomTooltipViewModel";
 
+import textIcon from "../../../../../Assets/icons/Zettel_darkblue_text-icon.svg";
+import h5pIcon from "../../../../../Assets/icons/h5p_icon.svg";
+import videoIcon from "../../../../../Assets/icons/video_icon_screen_button.svg";
+import imageIcon from "../../../../../Assets/icons/bild_icon.svg";
+
 export default function BottomTooltip() {
   const [viewModels] = useViewModelControllerProvider<BottomTooltipViewModel>(
     BottomTooltipViewModel
@@ -16,17 +21,13 @@ export default function BottomTooltip() {
   const getIcon = (type: string) => {
     switch (type) {
       case "text":
-        return (
-          <img className="w-10" src="icons/Zettel_darkblue_text-icon.svg"></img>
-        );
+        return <img className="w-10" src={textIcon}></img>;
       case "video":
-        return (
-          <img className="w-10" src="icons/video_icon_screen_button.svg"></img>
-        );
+        return <img className="w-10" src={videoIcon}></img>;
       case "image":
-        return <img className="w-10" src="icons/bild_icon.svg"></img>;
+        return <img className="w-10" src={imageIcon}></img>;
       case "h5p":
-        return <img className="w-10" src="icons/h5p_icon.svg"></img>;
+        return <img className="w-10" src={h5pIcon}></img>;
       default:
         return null;
     }
