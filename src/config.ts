8609d8c228e0070490f1e879a44c3f5ab2da413e
Philipp Geirhos <philipp@geirhos.com>
2022-07-06 15:34:47 +0200

    added config provider with default values

---
+++ src/config.ts
@@ -0,0 +1,12 @@
+const getBool = (input: string | boolean): boolean => {
+  if (typeof input === "boolean") return input;
+  return input === "true";
+};
+export const config = {
+  isDebug: getBool(process.env.DEBUG || false),
+  useAutoLogin: getBool(process.env.REACT_APP_USE_AUTOLOGIN || false),
+  userName: process.env.REACT_APP_DEBUG_USERNAME || "",
+  password: process.env.REACT_APP_DEBUG_PASSWORD || "",
+  logLevel: process.env.REACT_APP_LOGLEVEL || "warn",
+  serverURL: process.env.REACT_APP_API_SERVER_URL || "https://api.cluuub.xyz",
+};
