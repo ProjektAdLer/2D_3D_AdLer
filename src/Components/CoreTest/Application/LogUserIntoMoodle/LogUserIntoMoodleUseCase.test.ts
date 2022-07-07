842f428d27938dcd358d7ba85931d15fdde038a6
philgei <philipp@geirhos.com>
2022-07-07 11:35:59 +0200

    refactored test LoginUserToMoodle

--- src/Components/CoreTest/Application/LogUserIntoMoodle/LogUserIntoMoodleUseCase.test.ts
+++ src/Components/CoreTest/Application/LogUserIntoMoodle/LogUserIntoMoodleUseCase.test.ts
@@ -3,92 +3,64 @@ import LogUserIntoMoodleUseCase from "../../../Core/Application/LogUserIntoMoodl
 import CoreDIContainer from "../../../Core/DependencyInjection/CoreDIContainer";
 import CORE_TYPES from "../../../Core/DependencyInjection/CoreTypes";
 import PORT_TYPES from "../../../Core/DependencyInjection/Ports/PORT_TYPES";
-import USECASE_TYPES from "../../../Core/DependencyInjection/UseCases/USECASE_SYMBOLS";
 import UserDataEntity from "../../../Core/Domain/Entities/UserData";
 import EntityContainer from "../../../Core/Domain/EntityContainer/EntityContainer";
-import DebugPort from "../../../Core/Ports/DebugPort/DebugPort";
 import IMoodlePort from "../../../Core/Ports/MoodlePort/IMoodlePort";
-import MoodlePort from "../../../Core/Ports/MoodlePort/MoodlePort";
 
-@injectable()
-//@ts-ignore
-class BackendMock implements IBackend {
-  async logInUser(username: string, password: string): Promise<string> {
-    return "token";
-  }
-}
+import { mock } from "jest-mock-extended";
+import IBackend from "../../../Core/Adapters/Backend/IBackend";
+import IDebugPort from "../../../Core/Ports/DebugPort/IDebugPort";
+import IEntityContainer from "../../../Core/Domain/EntityContainer/IEntityContainer";
 
-const createEntityMock = jest.spyOn(EntityContainer.prototype, "createEntity");
-
-@injectable()
-//@ts-ignore
-class MoodlePortMock implements IMoodlePort {
-  registerMoodleLoginPresenter = jest.fn();
-  registerMoodleLoginButtonPresenter = jest.fn();
-  displayLoginForm = jest.fn();
-  loginSuccessful = jest.fn();
-  debug_DisplayUserToken = jest.fn();
-}
-
-@injectable()
-//@ts-ignore
-class DebugPortMock implements IDebugPort {
-  registerDebugPanelPresenter = jest.fn();
-  setUserToken = jest.fn();
-}
+const entityContainerMock = mock<IEntityContainer>();
+const backendMock = mock<IBackend>();
+const debugMock = mock<IDebugPort>();
+const moodleMock = mock<IMoodlePort>();
 
 describe("LogUserIntoMoodleUseCase", () => {
   let useCase: LogUserIntoMoodleUseCase;
 
   beforeAll(() => {
-    CoreDIContainer.unbind(PORT_TYPES.IMoodlePort);
-    CoreDIContainer.bind(PORT_TYPES.IMoodlePort).to(MoodlePortMock);
-
-    CoreDIContainer.unbind(PORT_TYPES.IDebugPort);
-    CoreDIContainer.bind(PORT_TYPES.IDebugPort).to(DebugPortMock);
-  });
-
-  afterAll(() => {
-    CoreDIContainer.unbind(PORT_TYPES.IMoodlePort);
-    CoreDIContainer.bind(PORT_TYPES.IMoodlePort).to(MoodlePort);
-
-    CoreDIContainer.unbind(PORT_TYPES.IDebugPort);
-    CoreDIContainer.bind(PORT_TYPES.IDebugPort).to(DebugPort);
-  });
-
-  beforeEach(() => {
     CoreDIContainer.snapshot();
 
-    CoreDIContainer.unbind(CORE_TYPES.IBackend);
-    CoreDIContainer.bind(CORE_TYPES.IBackend).to(BackendMock);
+    CoreDIContainer.unbind(PORT_TYPES.IMoodlePort);
+    CoreDIContainer.bind(PORT_TYPES.IMoodlePort).toConstantValue(moodleMock);
 
-    useCase = CoreDIContainer.get<LogUserIntoMoodleUseCase>(
-      USECASE_TYPES.ILogUserIntoMoodleUseCase
+    CoreDIContainer.unbind(PORT_TYPES.IDebugPort);
+    CoreDIContainer.bind(PORT_TYPES.IDebugPort).toConstantValue(debugMock);
+
+    CoreDIContainer.unbind(CORE_TYPES.IBackend);
+    CoreDIContainer.bind(CORE_TYPES.IBackend).toConstantValue(backendMock);
+
+    CoreDIContainer.unbind(CORE_TYPES.IEntityContainer);
+    CoreDIContainer.bind(CORE_TYPES.IEntityContainer).toConstantValue(
+      entityContainerMock
     );
   });
 
-  afterEach(() => {
+  afterAll(() => {
     CoreDIContainer.restore();
   });
 
+  beforeEach(() => {
+    useCase = CoreDIContainer.resolve(LogUserIntoMoodleUseCase);
+  });
+
   test("executeAsync calls the backend and stores correct user data in Enity", async () => {
-    BackendMock.prototype.logInUser = jest
-      .fn()
-      .mockImplementation((username: string, password: string) =>
-        Promise.resolve("token")
-      );
+    backendMock.logInUser.mockResolvedValue("token");
+    entityContainerMock.getEntitiesOfType.mockReturnValue([]);
 
     await useCase.executeAsync({
       username: "username",
       password: "password",
     });
 
-    expect(BackendMock.prototype.logInUser).toHaveBeenCalledWith({
+    expect(backendMock.logInUser).toHaveBeenCalledWith({
       username: "username",
       password: "password",
     });
 
-    expect(createEntityMock).toHaveBeenCalledWith(
+    expect(entityContainerMock.useSingletonEntity).toHaveBeenCalledWith(
       {
         userToken: "token",
         username: "username",
@@ -96,5 +68,7 @@ describe("LogUserIntoMoodleUseCase", () => {
       },
       UserDataEntity
     );
+
+    expect(moodleMock.loginSuccessful).toHaveBeenCalled();
   });
 });
