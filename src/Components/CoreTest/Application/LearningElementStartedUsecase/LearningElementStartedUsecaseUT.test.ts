ee59a09579b9030049009fd1cafb411c2048c1b0
philgei <philipp@geirhos.com>
2022-07-11 17:26:18 +0200

    finished tests of Learning element started UseCase

--- src/Components/CoreTest/Application/LearningElementStartedUsecase/LearningElementStartedUsecaseUT.test.ts
+++ src/Components/CoreTest/Application/LearningElementStartedUsecase/LearningElementStartedUsecaseUT.test.ts
@@ -7,61 +7,85 @@ import IEntityContainer from "../../../Core/Domain/EntityContainer/IEntityContai
 import LearningElementPort from "../../../Core/Ports/LearningElementPort/LearningElementPort";
 import { ConstructorReference } from "../../../Core/Types/EntityManagerTypes";
 
-const portMock = jest.spyOn(
-  LearningElementPort.prototype,
-  "startLearningElementEditing"
-);
+import { mock } from "jest-mock-extended";
+import ILearningElementPort from "../../../Core/Application/LearningElementStarted/ILearningElementPort";
+import PORT_TYPES from "../../../Core/DependencyInjection/Ports/PORT_TYPES";
+import LearningElementStartedUseCase from "../../../Core/Application/LearningElementStarted/LearningElementStartedUseCase";
 
-@injectable()
-//@ts-ignore
-class EntityContainerMock implements IEntityContainer {
-  filterEntitiesOfType<T extends object>(
-    entityType: ConstructorReference<T>,
-    filter: (entity: T) => boolean
-  ): T[] {
-    return [
-      {
-        id: 1,
-      },
-    ] as T[];
-  }
-}
+const learningElementPortMock = mock<ILearningElementPort>();
+const entityContainerMock = mock<IEntityContainer>();
 
 describe("Calculate Total Room Score UseCase", () => {
-  beforeEach(() => {
+  let useCaseToTest;
+  beforeAll(() => {
     CoreDIContainer.snapshot();
+
+    CoreDIContainer.unbind(CORE_TYPES.IEntityContainer);
+    CoreDIContainer.bind<IEntityContainer>(
+      CORE_TYPES.IEntityContainer
+    ).toConstantValue(entityContainerMock);
+
+    CoreDIContainer.unbind(PORT_TYPES.ILearningElementPort);
+    CoreDIContainer.bind(PORT_TYPES.ILearningElementPort).toConstantValue(
+      learningElementPortMock
+    );
+  });
+  beforeEach(() => {
+    useCaseToTest = CoreDIContainer.resolve(LearningElementStartedUseCase);
   });
 
-  afterEach(() => {
+  afterAll(() => {
     CoreDIContainer.restore();
   });
 
   it("should call the presenter of the Room with the LE ", () => {
-    CoreDIContainer.unbind(CORE_TYPES.IEntityContainer);
-    CoreDIContainer.bind(CORE_TYPES.IEntityContainer).to(EntityContainerMock);
+    entityContainerMock.filterEntitiesOfType.mockReturnValue([
+      {
+        id: 1,
+      },
+    ]);
 
-    const useCaseToTest = CoreDIContainer.get<ILearningElementStartedUseCase>(
-      USECASE_TYPES.ILearningElementStartedUseCase
+    useCaseToTest.execute({
+      learningElementId: 1,
+    });
+
+    expect(
+      learningElementPortMock.startLearningElementEditing
+    ).toHaveBeenCalledWith({
+      id: 1,
+    });
+  });
+
+  it("should throw, if the learning Element is not found", () => {
+    entityContainerMock.filterEntitiesOfType.mockReturnValue([]);
+    expect(() => {
+      useCaseToTest.execute({
+        learningElementId: 2,
+      });
+    }).toThrow();
+  });
+
+  test("filter Callback should return a boolean", () => {
+    let filterReturn: boolean;
+    entityContainerMock.filterEntitiesOfType.mockImplementation(
+      <T>(
+        entityType: ConstructorReference<T>,
+        filter: (entity: T) => boolean
+      ) => {
+        filterReturn = filter(new entityType());
+        return [
+          {
+            learningElements: [],
+          },
+        ];
+      }
     );
 
     useCaseToTest.execute({
       learningElementId: 1,
     });
 
-    expect(portMock).toHaveBeenCalledWith({
-      id: 1,
-    });
-  });
-
-  it("should throw, if the learning Element is not found", () => {
-    const useCaseToTest = CoreDIContainer.get<ILearningElementStartedUseCase>(
-      USECASE_TYPES.ILearningElementStartedUseCase
-    );
-
-    expect(() => {
-      useCaseToTest.execute({
-        learningElementId: 2,
-      });
-    }).toThrow();
+    // @ts-ignore TS does not know about the mock
+    expect(filterReturn).toBe(false);
   });
 });
