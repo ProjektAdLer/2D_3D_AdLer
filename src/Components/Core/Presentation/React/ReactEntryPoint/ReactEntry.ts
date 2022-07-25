3bd5d574ccacb49b62931d212f1bdf10dcbc1045
Marius Kaczmarek <kaczmarek.m@gmx.de>
2022-07-25 13:20:59 +0200

    fixed typo

--- src/Components/Core/Presentation/React/ReactEntryPoint/ReactEntry.ts
+++ src/Components/Core/Presentation/React/ReactEntryPoint/ReactEntry.ts
@@ -40,7 +40,7 @@ export default class ReactEntry implements IReactEntry {
     );
 
     if (
-      config.autoLoginWithoutShortct &&
+      config.autoLoginWithoutShortcut &&
       process.env.NODE_ENV === "development" &&
       config.isDebug &&
       !isInDebug
