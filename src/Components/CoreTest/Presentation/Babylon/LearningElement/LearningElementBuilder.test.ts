ed325540c8c4e34ff01211afaee4c1e4231b320f
Marius Kaczmarek <kaczmarek.m@gmx.de>
2022-07-27 11:17:29 +0200

    fixed broken injection in test

--- src/Components/CoreTest/Presentation/Babylon/LearningElement/LearningElementBuilder.test.ts
+++ src/Components/CoreTest/Presentation/Babylon/LearningElement/LearningElementBuilder.test.ts
@@ -1,27 +1,47 @@
-import LearningElementPort from "../../../../Core/Ports/LearningElementPort/LearningElementPort";
+import mock from "jest-mock-extended/lib/Mock";
+import CoreDIContainer from "../../../../Core/DependencyInjection/CoreDIContainer";
+import PORT_TYPES from "../../../../Core/DependencyInjection/Ports/PORT_TYPES";
+import ILearningElementPort from "../../../../Core/Ports/LearningElementPort/ILearningElementPort";
 import LearningElementBuilder from "../../../../Core/Presentation/Babylon/LearningElement/LearningElementBuilder";
 import LearningElementPresenter from "../../../../Core/Presentation/Babylon/LearningElement/LearningElementPresenter";
 
-const addLearningElementPresenterMock = jest.spyOn(
-  LearningElementPort.prototype,
-  "addLearningElementPresenter"
-);
+const learningElementPortMock = mock<ILearningElementPort>();
 
 describe("LearningElementViewModel", () => {
   let systemUnderTest: LearningElementBuilder;
+
+  beforeAll(() => {
+    CoreDIContainer.snapshot();
+    CoreDIContainer.rebind(PORT_TYPES.ILearningElementPort).toConstantValue(
+      learningElementPortMock
+    );
+  });
 
   beforeEach(() => {
     systemUnderTest = new LearningElementBuilder();
   });
 
+  afterAll(() => {
+    CoreDIContainer.restore();
+  });
+
   test("buildPresenter concludes the build step successfully and registers the presneter with the port", () => {
     systemUnderTest.buildViewModel();
     systemUnderTest.buildPresenter();
+
+    // build results
     expect(systemUnderTest["presenter"]).toBeDefined();
     expect(systemUnderTest.getPresenter()).toBeDefined();
     expect(systemUnderTest.getPresenter()).toBeInstanceOf(
       LearningElementPresenter
     );
-    expect(addLearningElementPresenterMock).toHaveBeenCalledTimes(1);
+
+    // call to the learning element port
+    expect(
+      learningElementPortMock.addLearningElementPresenter
+    ).toHaveBeenCalledTimes(1);
+    expect(
+      learningElementPortMock.addLearningElementPresenter
+    ).toHaveBeenCalledWith(systemUnderTest["presenter"]);
   });
 });
