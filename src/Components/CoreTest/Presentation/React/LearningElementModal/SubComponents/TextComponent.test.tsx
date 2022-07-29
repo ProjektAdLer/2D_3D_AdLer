0765326eef34e6ad73d23b01bbf6bd1cb729b393
Philipp Geirhos <philipp@geirhos.com>
2022-07-29 14:58:06 +0200

    added tests for text component

---
+++ src/Components/CoreTest/Presentation/React/LearningElementModal/SubComponents/TextComponent.test.tsx
@@ -0,0 +1,11 @@
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import TextComponent from "../../../../../Core/Presentation/React/LearningElementModal/SubComponents/TextComponent";
+
+describe("TextLearningElementSubComponent", () => {
+  test("should render its Text", () => {
+    const text = "Hello World";
+    const { getByText } = render(<TextComponent textContent={text} />);
+    expect(getByText(text)).toBeInTheDocument();
+  });
+});
