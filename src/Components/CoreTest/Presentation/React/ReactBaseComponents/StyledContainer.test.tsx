cb8929dbfbb51fe05d465a43d3e4b4d358d226c2
Philipp Geirhos <philipp@geirhos.com>
2022-07-29 14:10:17 +0200

    added all test for base components

---
+++ src/Components/CoreTest/Presentation/React/ReactBaseComponents/StyledContainer.test.tsx
@@ -0,0 +1,20 @@
+import { render } from "@testing-library/react";
+import StyledContainer from "../../../../Core/Presentation/React/ReactBaseComponents/StyledContainer";
+import "@testing-library/jest-dom";
+
+describe("Styled Container", () => {
+  test("should render", () => {
+    const componentUnderTest = render(<StyledContainer data-testid="test" />);
+    //expect(componentUnderTest.getByTestId("test")).toBeInTheDocument();
+    expect(componentUnderTest.baseElement).toBeInTheDocument();
+  });
+
+  test("should render its children", () => {
+    const componentUnderTest = render(
+      <StyledContainer data-testid="test">
+        <div>Test</div>
+      </StyledContainer>
+    );
+    expect(componentUnderTest.getByText("Test")).toBeInTheDocument();
+  });
+});
