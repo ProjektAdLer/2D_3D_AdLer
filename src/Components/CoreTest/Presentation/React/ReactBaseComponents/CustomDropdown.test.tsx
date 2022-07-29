6635e41e1b17385573b4d8cfdece800d2f90bd6a
Philipp Geirhos <philipp@geirhos.com>
2022-07-29 13:53:19 +0200

    added test for dropdown

---
+++ src/Components/CoreTest/Presentation/React/ReactBaseComponents/CustomDropdown.test.tsx
@@ -0,0 +1,29 @@
+import { fireEvent, render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import CustomDropdown from "../../../../Core/Presentation/React/ReactBaseComponents/CustomDropdown";
+
+describe("Custom Dropdown", () => {
+  it("should render", () => {
+    const componentUnderTest = render(<CustomDropdown />);
+    expect(componentUnderTest.baseElement).toBeInTheDocument();
+  });
+
+  it("Should render its children", () => {
+    const componentUnderTest = render(
+      <CustomDropdown isOpen={true} initialOpen={true}>
+        <div>Test</div>
+      </CustomDropdown>
+    );
+    expect(componentUnderTest.getByText("Test")).toBeInTheDocument();
+  });
+
+  it("should open when clicked", () => {
+    const componentUnderTest = render(
+      <CustomDropdown headerElement={"OpenElement"} useAsTriggerOnly={true}>
+        <div>Test</div>
+      </CustomDropdown>
+    );
+    fireEvent.click(componentUnderTest.getByText("OpenElement"));
+    expect(componentUnderTest.getByText("Test")).toBeInTheDocument();
+  });
+});
