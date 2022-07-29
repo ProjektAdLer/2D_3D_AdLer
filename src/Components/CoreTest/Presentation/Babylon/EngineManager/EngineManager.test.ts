bdccacb9594922223f491b067011d20fdecdfe46
Marius Kaczmarek <kaczmarek.m@gmx.de>
2022-07-29 11:56:50 +0200

    completed EngineManager tests

--- src/Components/CoreTest/Presentation/Babylon/EngineManager/EngineManager.test.ts
+++ src/Components/CoreTest/Presentation/Babylon/EngineManager/EngineManager.test.ts
@@ -1,5 +1,6 @@
 import { Engine } from "@babylonjs/core";
 import EngineManager from "../../../../Core/Presentation/Babylon/EngineManager/EngineManager";
+import { fireEvent } from "@testing-library/dom";
 
 jest.mock("@babylonjs/core");
 
@@ -14,11 +15,16 @@ describe("EngineManager", () => {
     jest.restoreAllMocks();
   });
 
-  test.todo("test createEngine");
-
   test("Engine getter returns an object of type Engine after engine is created", () => {
     systemUnderTest.createEngine(document.createElement("canvas"));
     expect(systemUnderTest.Engine).toBeInstanceOf(Engine);
+  });
+
+  test("window.resize calls the engine resize callback", () => {
+    systemUnderTest.createEngine(document.createElement("canvas"));
+    fireEvent.resize(window);
+
+    expect(systemUnderTest.Engine.resize).toHaveBeenCalledTimes(1);
   });
 
   test("Engine getter throws error when engine wasn't created", () => {
