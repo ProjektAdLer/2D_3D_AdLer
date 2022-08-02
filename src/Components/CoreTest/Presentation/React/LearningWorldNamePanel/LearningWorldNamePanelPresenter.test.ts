63dff219bad56c6e7b7e3524a47492776e9434df
Philipp Geirhos <philipp@geirhos.com>
2022-08-02 12:45:15 +0200

    added test for tooltip, dropdown, namepanel presenter

---
+++ src/Components/CoreTest/Presentation/React/LearningWorldNamePanel/LearningWorldNamePanelPresenter.test.ts
@@ -0,0 +1,17 @@
+import type ILearningWorldNamePanelPresenter from "../../../../../../src/Components/Core/Presentation/React/LearningWorldNamePanel/ILearningWorldNamePanelPresenter";
+import LearningWorldNamePanelViewModel from "../../../../../../src/Components/Core/Presentation/React/LearningWorldNamePanel/LearningWorldNamePanelViewModel";
+import LearningWorldNamePanelPresenter from "../../../../../../src/Components/Core/Presentation/React/LearningWorldNamePanel/LearningWorldNamePanelPresenter";
+describe("LearningWorldNamePanel", () => {
+  let systemUnderTest: ILearningWorldNamePanelPresenter;
+  let vm: LearningWorldNamePanelViewModel;
+
+  beforeEach(() => {
+    vm = new LearningWorldNamePanelViewModel();
+    systemUnderTest = new LearningWorldNamePanelPresenter(vm);
+  });
+
+  test("should set the name of the world in the vm ", () => {
+    systemUnderTest.displayWorldName("Test World");
+    expect(vm.worldName.Value).toBe("Test World");
+  });
+});
