5a39d6785aa76f8869d2270e71b72f61f89597f2
Marius Kaczmarek <kaczmarek.m@gmx.de>
2022-08-02 19:23:20 +0200

    refactored LearningElementController and completed tests

--- src/Components/CoreTest/Presentation/Babylon/LearningElement/LearningElementController.test.ts
+++ src/Components/CoreTest/Presentation/Babylon/LearningElement/LearningElementController.test.ts
@@ -1,22 +1,61 @@
-import LearningElementStartedUseCase from "../../../../Core/Application/LearningElementStarted/LearningElementStartedUseCase";
+import { mock } from "jest-mock-extended";
+import ILearningElementStartedUseCase from "../../../../Core/Application/LearningElementStarted/ILearningElementStartedUseCase";
+import CoreDIContainer from "../../../../Core/DependencyInjection/CoreDIContainer";
+import PORT_TYPES from "../../../../Core/DependencyInjection/Ports/PORT_TYPES";
+import USECASE_TYPES from "../../../../Core/DependencyInjection/UseCases/USECASE_TYPES";
+import IUIPort from "../../../../Core/Ports/UIPort/IUIPort";
 import LearningElementController from "../../../../Core/Presentation/Babylon/LearningElement/LearningElementController";
 import LearningElementViewModel from "../../../../Core/Presentation/Babylon/LearningElement/LearningElementViewModel";
 
-const executeMock = jest.spyOn(
-  LearningElementStartedUseCase.prototype,
-  "execute"
-);
+const learningElementStartedUseCaseMock =
+  mock<ILearningElementStartedUseCase>();
+const uiPortMock = mock<IUIPort>();
 
 describe("LearningElementController", () => {
-  const viewModel = new LearningElementViewModel();
-  const systemUnderTest = new LearningElementController(viewModel);
-  test("clicked calls useCase", () => {
+  let viewModel: LearningElementViewModel;
+  let systemUnderTest: LearningElementController;
+
+  beforeAll(() => {
+    CoreDIContainer.snapshot();
+    CoreDIContainer.rebind(
+      USECASE_TYPES.ILearningElementStartedUseCase
+    ).toConstantValue(learningElementStartedUseCaseMock);
+    CoreDIContainer.rebind(PORT_TYPES.IUIPort).toConstantValue(uiPortMock);
+  });
+
+  beforeEach(() => {
+    viewModel = new LearningElementViewModel();
+    systemUnderTest = new LearningElementController(viewModel);
+  });
+
+  afterAll(() => {
+    CoreDIContainer.restore();
+  });
+
+  test("pointerOver calls IUIPort.displayLearningElementTooltip", () => {
+    systemUnderTest.pointerOver();
+
+    expect(uiPortMock.displayLearningElementTooltip).toHaveBeenCalledTimes(1);
+    expect(uiPortMock.displayLearningElementTooltip).toHaveBeenCalledWith({
+      name: viewModel.name.Value,
+      learningElementData: viewModel.learningElementData.Value,
+      id: viewModel.id,
+    });
+  });
+
+  test("pointerOut calls IUIPort.hideBottomTooltip", () => {
+    systemUnderTest.pointerOut();
+
+    expect(uiPortMock.hideBottomTooltip).toHaveBeenCalledTimes(1);
+  });
+
+  test("clicked calls ILearningElementStartedUseCase.execute", () => {
     viewModel.id = 42;
 
     systemUnderTest.clicked();
 
-    expect(executeMock).toHaveBeenCalledTimes(1);
-    expect(executeMock).toHaveBeenCalledWith({
+    expect(learningElementStartedUseCaseMock.execute).toHaveBeenCalledTimes(1);
+    expect(learningElementStartedUseCaseMock.execute).toHaveBeenCalledWith({
       learningElementId: 42,
     });
   });
