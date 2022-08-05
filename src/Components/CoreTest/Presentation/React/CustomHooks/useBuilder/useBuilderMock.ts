567fefb5696bfaf02aa394e76487c4319287ed5b
Philipp Geirhos <philipp@geirhos.com>
2022-08-05 14:15:42 +0200

    added tests for usebuilder

---
+++ src/Components/CoreTest/Presentation/React/CustomHooks/useBuilder/useBuilderMock.ts
@@ -0,0 +1,12 @@
+import * as useBuilder from "~ReactComponents/CustomHooks/useBuilder";
+
+/**
+ * Mocks the useViewModelControllerProvider hook.
+ * @param mockedTupels The return value, which the mocked hook will return.
+ *
+ */
+export default function useBuilderMock(
+  mockedTupels: [viewModel: any, controller: any] = [undefined, undefined]
+) {
+  jest.spyOn(useBuilder, "default").mockReturnValue(mockedTupels);
+}
