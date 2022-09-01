e39e41fd1dca414e3b479cab37953bcc1e940bbc
Marius Kaczmarek <kaczmarek.m@gmx.de>
2022-09-01 15:10:58 +0200

    changed logger errors back to actual exeptions

--- src/Components/Core/Ports/DebugPort/DebugPort.ts
+++ src/Components/Core/Ports/DebugPort/DebugPort.ts
@@ -7,17 +7,19 @@ import IDebugPort from "./IDebugPort";
 @injectable()
 export default class DebugPort implements IDebugPort {
   private debugPanelPresenter: IDebugPanelPresenter;
+
   addToMisc(key: string, value: string): void {
     if (!this.debugPanelPresenter) {
-      logger.error("DebugPanelPresenter is not registered");
+      throw new Error("DebugPanelPresenter is not registered");
     }
 
     this.debugPanelPresenter.setMisc({ key, value });
   }
+
   @bind
   setUserToken(userToken: string): void {
     if (!this.debugPanelPresenter) {
-      logger.error("DebugPanelPresenter is not registered");
+      throw new Error("DebugPanelPresenter is not registered");
     }
 
     this.debugPanelPresenter.setMoodleToken(userToken);
