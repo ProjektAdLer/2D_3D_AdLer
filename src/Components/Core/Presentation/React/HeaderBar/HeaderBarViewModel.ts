9f8737c18a7cefaf7808c5b07b796264f8e7b3f5
Marius Kaczmarek <kaczmarek.m@gmx.de>
2022-09-06 14:33:10 +0200

    added comment

--- src/Components/Core/Presentation/React/HeaderBar/HeaderBarViewModel.ts
+++ src/Components/Core/Presentation/React/HeaderBar/HeaderBarViewModel.ts
@@ -1,5 +1,6 @@
 import Observable from "src/Lib/Observable";
 
 export default class HeaderBarViewModel {
+  // TODO: replace default value when the title is set correctly
   public title: Observable<string> = new Observable<string>("Lernwelt");
 }
