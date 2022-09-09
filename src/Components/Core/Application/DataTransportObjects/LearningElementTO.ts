6a28b2109ffab41732df3152f0f5d914ffa4497a
Marius Kaczmarek <kaczmarek.m@gmx.de>
2022-09-09 13:45:35 +0200

    hotfixed error

--- src/Components/Core/Application/DataTransportObjects/LearningElementTO.ts
+++ src/Components/Core/Application/DataTransportObjects/LearningElementTO.ts
@@ -4,7 +4,7 @@ import { LearningComponentID } from "../../Domain/Types/EntityTypes";
 export default class LearningElementTO {
   id: LearningComponentID;
   name: string;
-  value: number;
+  value?: number;
   // TODO: make this not optional sometime
   requirements?: { type: string; value: number }[];
   learningElementData: AbstractLearningElement;
