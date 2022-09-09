b8e6e58e23146effa822f5eea022920bc70d35e6
Lizard Guard <felixkleis@yahoo.de>
2022-09-09 09:15:57 +0200

    Added rough outline for REACT ROUTER

--- src/Components/Core/Presentation/React/ReactEntryPoint/App.tsx
+++ src/Components/Core/Presentation/React/ReactEntryPoint/App.tsx
@@ -1,12 +1,24 @@
 import "./App.css";
 import LearningRoom from "./LearningRoom";
 import LearningWorldMenu from "./LearningWorldMenu";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import WelcomePage from "./WelcomePage";
 
-function App() {
+export interface IAppProps {}
+
+const App: React.FunctionComponent<IAppProps> = (props) => {
   return (
-    //<LearningRoom />
-    <LearningWorldMenu />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="worldmenu">
+          <Route index element={<LearningWorldMenu />} />
+        </Route>
+        <Route path="room" element={<LearningRoom />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/Components/Core/Presentation/React/ReactEntryPoint/WelcomePage.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import StyledButton from "~ReactComponents/ReactBaseComponents/StyledButton";
+
+import logo from "../../../../../Assets/icons/adLerEngine_flat_logo_simple.svg";
+
+export interface IWelcomePageProps {}
+
+// THIS IS A BIG TODO FOR ANOTHER TIME
+const WelcomePage: React.FunctionComponent<IWelcomePageProps> = (props) => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <img className="absolute w-full h-full" src={logo} alt="Adler Logo" />
+      <p>This is the welcome page.</p>
+
+      <p>
+        <StyledButton
+          shape="freefloatleft"
+          onClick={() => navigate("/worldmenu")}
+        >
+          Go to Menu by onclick button
+        </StyledButton>
+        <StyledButton shape="freefloatleft" onClick={() => navigate("/room")}>
+          Go to room by onclick button
+        </StyledButton>
+      </p>
+    </div>
+  );
+};
+
+export default WelcomePage;
