c1d375b32c745dd0636574cbae8226a800773ca0
Marius Kaczmarek <kaczmarek.m@gmx.de>
2022-09-12 15:10:22 +0200

    refactored useBuilder hook

--- src/Components/Core/Presentation/React/CustomHooks/useBuilder.ts
+++ src/Components/Core/Presentation/React/CustomHooks/useBuilder.ts
@@ -1,27 +1,22 @@
 import { interfaces } from "inversify";
 import { useInjection } from "inversify-react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import BUILDER_TYPES from "~DependencyInjection/Builders/BUILDER_TYPES";
 import IPresentationBuilder from "../../PresentationBuilder/IPresentationBuilder";
 import IPresentationDirector from "../../PresentationBuilder/IPresentationDirector";
 
 export default function useBuilder<ViewModel, Controller>(
   builderType: interfaces.ServiceIdentifier<IPresentationBuilder>
-): [ViewModel | undefined, Controller | undefined] {
+): [ViewModel, Controller] {
   const director = useInjection<IPresentationDirector>(
     BUILDER_TYPES.IPresentationDirector
   );
   const builder = useInjection<IPresentationBuilder>(builderType);
 
-  const [controller, setController] = useState<Controller>();
-  const [viewModel, setViewModel] = useState<ViewModel>();
-
-  useEffect(() => {
+  const [[viewModel, controller]] = useState<[ViewModel, Controller]>(() => {
     director.build(builder);
-
-    setController(builder.getController());
-    setViewModel(builder.getViewModel());
-  }, []);
+    return [builder.getViewModel(), builder.getController()];
+  });
 
   return [viewModel, controller];
 }
