99df3c134fb6bb5069b437f6e88b34494fd189cc
Marius Kaczmarek <kaczmarek.m@gmx.de>
2022-09-13 12:27:49 +0200

    updated blueprint template

--- blueprint-templates/reactComponent/__name__/__name__.tsx
+++ blueprint-templates/reactComponent/__name__/__name__.tsx
@@ -1,11 +1,11 @@
 import useBuilder from "../CustomHooks/useBuilder";
-import {{name}}Controller from "./{{name}}Controller";
+import I{{name}}Controller from "./I{{name}}Controller";
 import {{name}}ViewModel from "./{{name}}ViewModel";
 
 export default function {{name}}() {
   const [viewModel, controller] = useBuilder<
   {{name}}ViewModel,
-  {{name}}Controller
+  I{{name}}Controller
 >(BUILDER_TYPES.I{{name}}Builder);
 
   if (!viewModel || !controller) return null;
