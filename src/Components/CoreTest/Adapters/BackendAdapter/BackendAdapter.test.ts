61f5697e519fa22dea5d8aa9c1fa7f14ac6d5635
philgei <philipp@geirhos.com>
2022-10-02 19:41:01 +0200

    added all tests for the backend adapter

--- src/Components/CoreTest/Adapters/BackendAdapter/BackendAdapter.test.ts
+++ src/Components/CoreTest/Adapters/BackendAdapter/BackendAdapter.test.ts
@@ -1,3 +1,5 @@
+import { mock } from "jest-mock-extended";
+import { XAPiEvent } from "./../../../Core/Application/UseCases/ScoreH5PElement/IScoreH5PElement";
 import {
   expectedElementTO,
   expectedSpaceTO,
@@ -161,5 +163,167 @@ describe("BackendAdapter", () => {
     expect(returnedVal).toEqual([
       { courses: { courseId: 1, courseName: "string" } },
     ]);
+  });
+
+  test("should score a H5P Element", async () => {
+    mockedAxios.patch.mockResolvedValue({
+      data: {
+        isSuceess: true,
+      },
+    });
+    const h5pMock = mock<XAPiEvent>();
+    const returnedVal = await systemUnderTest.scoreH5PElement({
+      courseId: 1,
+      h5pId: 1,
+      userToken: "token",
+      rawH5PEvent: h5pMock,
+    });
+
+    expect(mockedAxios.patch).toHaveBeenCalledTimes(1);
+    expect(mockedAxios.patch).toHaveBeenCalledWith(
+      config.serverURL + "/LearningElements/Course/" + 1 + "/Element/" + 1,
+      {
+        serializedXAPIEvent: JSON.stringify(h5pMock),
+      },
+      {
+        headers: {
+          token: "token",
+        },
+      }
+    );
+    expect(returnedVal).toEqual(true);
+  });
+  test("should get Element Source", async () => {
+    mockedAxios.get.mockResolvedValue({
+      data: { filePath: "string" },
+    });
+    const returnedVal = await systemUnderTest.getElementSource("token", 1, 1);
+
+    expect(mockedAxios.get).toHaveBeenCalledTimes(1);
+    expect(mockedAxios.get).toHaveBeenCalledWith(
+      config.serverURL +
+        "/LearningElements/FilePath/Course/" +
+        1 +
+        "/Element/" +
+        1,
+      {
+        headers: {
+          token: "token",
+        },
+      }
+    );
+    expect(returnedVal).toEqual("string");
+  });
+  test("should get World Status", async () => {
+    mockedAxios.get.mockResolvedValue({
+      data: {
+        courseId: 1,
+        learningElements: [
+          {
+            elementId: 1,
+            successss: true,
+          },
+        ],
+      },
+    });
+    const returnedVal = await systemUnderTest.getWorldStatus("token", 1);
+
+    expect(mockedAxios.get).toHaveBeenCalledTimes(1);
+    expect(mockedAxios.get).toHaveBeenCalledWith(
+      config.serverURL + "/Courses/" + 1 + "/status",
+      {
+        headers: {
+          token: "token",
+        },
+      }
+    );
+    expect(returnedVal).toEqual({
+      courseId: 1,
+      learningElements: [
+        {
+          elementId: 1,
+          successss: true,
+        },
+      ],
+    });
+  });
+  test("should get Element Score", async () => {
+    mockedAxios.get.mockResolvedValue({
+      data: {
+        elementId: 1,
+        successss: true,
+      },
+    });
+    const returnedVal = await systemUnderTest.getElementScore("token", 1, 1);
+
+    expect(mockedAxios.get).toHaveBeenCalledTimes(1);
+    expect(mockedAxios.get).toHaveBeenCalledWith(
+      config.serverURL +
+        "/LearningElements/Course/" +
+        1 +
+        "/Element/" +
+        1 +
+        "/Score",
+      {
+        headers: {
+          token: "token",
+        },
+      }
+    );
+    expect(returnedVal).toEqual({
+      elementId: 1,
+      successss: true,
+    });
+  });
+  test("should get Player Data", async () => {
+    mockedAxios.get.mockResolvedValue({
+      data: {
+        playerGender: "string",
+        playerWorldColor: "string",
+      },
+    });
+    const returnedVal = await systemUnderTest.getPlayerData("token");
+
+    expect(mockedAxios.get).toHaveBeenCalledTimes(1);
+    expect(mockedAxios.get).toHaveBeenCalledWith(
+      config.serverURL + "/PlayerData",
+      {
+        headers: {
+          token: "token",
+        },
+      }
+    );
+    expect(returnedVal).toEqual({
+      playerGender: "string",
+      playerWorldColor: "string",
+    });
+  });
+  test("should update Player Data", async () => {
+    mockedAxios.patch.mockResolvedValue({
+      data: {
+        playerGender: "string",
+        playerWorldColor: "string",
+      },
+    });
+    const returnedVal = await systemUnderTest.updatePlayerData("token", {
+      playerGender: "string1",
+    });
+
+    expect(mockedAxios.patch).toHaveBeenCalledTimes(1);
+
+    expect(returnedVal).toEqual({
+      playerGender: "string",
+      playerWorldColor: "string",
+    });
+  });
+  test("should delete Player Data", async () => {
+    mockedAxios.delete.mockResolvedValue({
+      data: true,
+    });
+    const returnedVal = await systemUnderTest.deletePlayerData("token");
+
+    expect(mockedAxios.delete).toHaveBeenCalledTimes(1);
+
+    expect(returnedVal).toEqual(true);
   });
 });
