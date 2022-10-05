f5b89ab46a12e5798c3eddc80eeeaf6c7212c799
Kleis <kleisfee@hs-kempten.local>
2022-10-05 15:13:37 +0200

    Improved App tsx test slightly

--- src/Components/CoreTest/Presentation/React/ReactRelated/ReactEntryPoint/App.test.tsx
+++ src/Components/CoreTest/Presentation/React/ReactRelated/ReactEntryPoint/App.test.tsx
@@ -1,6 +1,7 @@
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
 import App from "../../../../../Core/Presentation/React/ReactRelated/ReactEntryPoint/App";
+import history from "history/browser";
 
 jest.mock(
   "../../../../../Core/Presentation/React/ReactRelated/ReactEntryPoint/Space.tsx",
@@ -18,5 +19,16 @@ jest.mock(
 describe("App", () => {
   test("should render", () => {
     render(<App />);
+  });
+  //TODO: Tests are not entering the switch case constructs yet.
+  test("App works if pathname is /space", () => {
+    render(<App />);
+    history.push("/space");
+    expect(history.location.pathname).toBe("/space");
+  });
+  test("App works if pathname is /worldmenu", () => {
+    render(<App />);
+    history.push("/worldmenu");
+    expect(history.location.pathname).toBe("/worldmenu");
   });
 });
