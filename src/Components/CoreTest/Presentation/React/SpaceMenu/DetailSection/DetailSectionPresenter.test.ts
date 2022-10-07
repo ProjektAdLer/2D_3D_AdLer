e013c5b285313021e3428bdd0dfea4a9a757bbba
Kleis <kleisfee@hs-kempten.local>
2022-10-07 14:26:36 +0200

    Updated DetailsectionPresenter Tests

--- src/Components/CoreTest/Presentation/React/SpaceMenu/DetailSection/DetailSectionPresenter.test.ts
+++ src/Components/CoreTest/Presentation/React/SpaceMenu/DetailSection/DetailSectionPresenter.test.ts
@@ -12,12 +12,16 @@ describe("DetailSectionPresenter", () => {
   test("onSpaceLoaded sets data in the view model", () => {
     const spaceTO: SpaceTO = {
       id: 42,
+      description: "description",
+      goals: "goals",
+      requiredPoints: 42,
+      requirements: [42, 20, 1],
       name: "Test Space",
       elements: [
         {
           id: 1,
           name: "Test Element 1",
-
+          parentSpaceId: 42,
           type: "h5p",
           description: "Test Description 1",
           goals: "TestGoal",
@@ -26,6 +30,7 @@ describe("DetailSectionPresenter", () => {
         {
           id: 2,
           name: "Test Element 2",
+          parentSpaceId: 42,
           type: "text",
           description: "Test Description 1",
           goals: "TestGoal",
@@ -40,6 +45,12 @@ describe("DetailSectionPresenter", () => {
     expect(systemUnderTest["viewModel"].elements.Value).toEqual([
       ["h5p", "Test Element 1"],
       ["text", "Test Element 2"],
+    ]);
+
+    expect(systemUnderTest["viewModel"].requirements.Value).toEqual([
+      [false, "TBD 42"],
+      [false, "TBD 20"],
+      [false, "TBD 1"],
     ]);
   });
 });
