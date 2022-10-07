2fe75a942b38b3dd83b1ce88aadd8531bcb099a9
Kleis <kleisfee@hs-kempten.local>
2022-10-07 13:43:05 +0200

    Fixed Detailsection View tests (100%)

--- src/Components/CoreTest/Presentation/React/SpaceMenu/DetailSection/DetailSectionView.test.tsx
+++ src/Components/CoreTest/Presentation/React/SpaceMenu/DetailSection/DetailSectionView.test.tsx
@@ -1,16 +1,29 @@
 import { render } from "@testing-library/react";
 import { ElementTypeStrings } from "src/Components/Core/Domain/Types/ElementTypes";
+import Observable from "src/Lib/Observable";
 import DetailSection from "~ReactComponents/SpaceMenu/DetailSection/DetailSection";
+import DetailSectionController from "~ReactComponents/SpaceMenu/DetailSection/DetailSectionController";
 import DetailSectionViewModel from "~ReactComponents/SpaceMenu/DetailSection/DetailSectionViewModel";
+import IDetailSectionController from "~ReactComponents/SpaceMenu/DetailSection/IDetailSectionController";
 import useBuilderMock from "../../ReactRelated/CustomHooks/useBuilder/useBuilderMock";
 
+let mockViewmodel = new DetailSectionViewModel();
+const mockElements: Observable<[ElementTypeStrings, string][]> = new Observable(
+  [
+    ["text" as ElementTypeStrings, "test"],
+    ["image" as ElementTypeStrings, "test"],
+  ]
+);
+const mockRequirements: Observable<[boolean, string][]> = new Observable([
+  [true, "test"],
+  [false, "test"],
+]);
 describe("DetailSection in Space Menu", () => {
   test("should render", () => {
-    useBuilderMock([new DetailSectionViewModel(), undefined]);
-    const elements = [
-      ["text" as ElementTypeStrings, "test"],
-      ["image" as ElementTypeStrings, "test"],
-    ];
+    useBuilderMock([mockViewmodel, new DetailSectionController()]);
+    mockViewmodel.elements = mockElements;
+    mockViewmodel.requirements = mockRequirements;
+
     //TODO: elements has to be included in the detailsection
     render(<DetailSection />);
   });
