2ad24d612ce37335a6554fe28eea2961a60490be
philgei <philipp@geirhos.com>
2022-10-14 14:07:06 +0200

    fixed dropdown and detailSectionController

--- src/Components/CoreTest/Presentation/React/SpaceMenu/DetailSection/DetailSectionController.test.ts
+++ src/Components/CoreTest/Presentation/React/SpaceMenu/DetailSection/DetailSectionController.test.ts
@@ -1,20 +1,22 @@
 import { logger } from "../../../../../../Lib/Logger";
 import history from "history/browser";
 import DetailSectionController from "../../../../../Core/Presentation/React/SpaceMenu/DetailSection/DetailSectionController";
+import DetailSectionViewModel from "../../../../../Core/Presentation/React/SpaceMenu/DetailSection/DetailSectionViewModel";
 
 jest.mock("../../../../../../Lib/Logger.ts");
 const mockHistoryPush = jest.spyOn(history, "push");
+
+const vm = new DetailSectionViewModel();
 
 describe("DetailSectionController", () => {
   let systemUnderTest: DetailSectionController;
 
   beforeEach(() => {
-    systemUnderTest = new DetailSectionController();
+    systemUnderTest = new DetailSectionController(vm);
   });
 
   test("onSpaceButtonClicked calls logger.warn and history.push", () => {
     systemUnderTest.onSpaceButtonClicked();
-    expect(logger.warn).toBeCalled();
     expect(mockHistoryPush).toBeCalled();
   });
 });
