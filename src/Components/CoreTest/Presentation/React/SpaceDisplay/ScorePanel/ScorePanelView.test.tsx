1de28de7e4fdb79e65d7ce69826082f844db5971
philgei <philipp@geirhos.com>
2022-10-17 14:47:49 +0200

    fixed test for Score Panel

--- src/Components/CoreTest/Presentation/React/SpaceDisplay/ScorePanel/ScorePanelView.test.tsx
+++ src/Components/CoreTest/Presentation/React/SpaceDisplay/ScorePanel/ScorePanelView.test.tsx
@@ -3,21 +3,27 @@ import ScorePanel from "../../../../../Core/Presentation/React/SpaceDisplay/Scor
 import ScorePanelViewModel from "../../../../../Core/Presentation/React/SpaceDisplay/ScorePanel/ScorePanelViewModel";
 import "@testing-library/jest-dom";
 import useBuilderMock from "../../ReactRelated/CustomHooks/useBuilder/useBuilderMock";
+import React from "react";
 
 let fakeModel = new ScorePanelViewModel();
-fakeModel.score.Value = 1337;
+fakeModel.score.Value = 1;
+fakeModel.requiredScore.Value = 2;
+fakeModel.maxScore.Value = 3;
+
 describe("Score Panel View", () => {
   it("renders the correct score", () => {
     useBuilderMock([fakeModel, undefined]);
-    const { getByText } = render(<ScorePanel />);
+    const comp = render(<ScorePanel />);
 
-    expect(getByText("1337")).toBeInTheDocument();
+    expect(comp.container).toHaveTextContent("1");
+    expect(comp.container).toHaveTextContent("2");
+    //expect(comp.container).toHaveTextContent("3");
   });
 
   it("renders 0, when no score is provided", () => {
     useBuilderMock([undefined, undefined]);
-    const { getByText } = render(<ScorePanel />);
+    const comp = render(<ScorePanel />);
 
-    expect(getByText("0")).toBeInTheDocument();
+    expect(comp.container).toHaveTextContent("0 / 0");
   });
 });
