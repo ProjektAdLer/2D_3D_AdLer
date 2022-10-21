8799aa00229f7a6b8fdeeefb9ae629562393d95c
DanielGlaab <daniel.glaab@th-ab.de>
2022-10-21 16:03:27 +0200

    changed font on credits

--- src/Components/Core/Presentation/React/ReactRelated/ReactEntryPoint/WelcomePage.tsx
+++ src/Components/Core/Presentation/React/ReactRelated/ReactEntryPoint/WelcomePage.tsx
@@ -16,7 +16,7 @@ const WelcomePage: React.FunctionComponent<IWelcomePageProps> = (props) => {
         <div className="col-start-1 row-start-1">
           <MoodleLoginButton />
         </div>
-        <p className="self-center col-span-2 col-start-4 row-start-1 p-2 text-xl font-extrabold text-white rounded-lg lg:col-span-3 lg:col-start-2 lg:row-start-1 lg:p-5 justify-self-center bg-adlergold text-shadow lg:text-4xl">
+        <p className="self-center col-span-2 col-start-4 row-start-1 p-2 text-lg font-extrabold text-white rounded-lg lg:col-span-3 lg:col-start-2 lg:row-start-1 lg:p-5 justify-self-center bg-adlergold text-shadow lg:text-4xl">
           Willkommen bei AdLer!
         </p>
 
@@ -34,7 +34,7 @@ const WelcomePage: React.FunctionComponent<IWelcomePageProps> = (props) => {
           src={logo}
           alt="Adler Logo"
         />
-        <p className="self-end col-span-5 col-start-1 row-start-5 p-2 text-xs text-center text-white rounded-lg lg:text-lg lg:font-semibold lg:text-shadow-sm font-regular justify-self-center bg-adlergold">
+        <p className="self-end col-span-5 col-start-1 row-start-5 p-2 text-xs text-center text-black rounded-lg lg:text-md lg:font-semibold font-regular justify-self-center bg-adlergold">
           © Projekt AdLer, Technische Hochschule Aschaffenburg, Hochschule für
           angewandte Wissenschaften Kempten, ZFH - Zentrum für Fernstudien im
           Hochschulverbund
