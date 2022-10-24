5a99f684fdede5605ad4c47f3e7747e5e71f7225
DanielGlaab <daniel.glaab@th-ab.de>
2022-10-24 10:00:13 +0200

    added <img> alt="" in SpaceCompletionModal

--- src/Components/Core/Presentation/React/SpaceDisplay/SpaceCompletionModal/SpaceCompletionModal.tsx
+++ src/Components/Core/Presentation/React/SpaceDisplay/SpaceCompletionModal/SpaceCompletionModal.tsx
@@ -33,7 +33,13 @@ export default function SpaceCompletionModal() {
       }}
     >
       <div className="flex flex-col items-center justify-center">
-        {<img className="w-20 mb-4 lg:w-32" src={RubicsCube} alt=""></img>}
+        {
+          <img
+            className="w-20 mb-4 lg:w-32"
+            src={RubicsCube}
+            alt="Lösung-Icon"
+          ></img>
+        }
         <div className="p-4 mb-4 w-96">
           <p className="p-1 text-sm lg:text:xl">
             Du hast {score} von {requiredScore} erforderlichen Punkten erreicht!{" "}
