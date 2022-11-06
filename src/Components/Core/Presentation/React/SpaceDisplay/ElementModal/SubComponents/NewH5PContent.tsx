1c0dd8f3aa91613a0b4d2322c407a485a723071b
philgei <philipp@geirhos.com>
2022-11-06 17:55:38 +0100

    Fixed first 2 h5p files

--- src/Components/Core/Presentation/React/SpaceDisplay/ElementModal/SubComponents/NewH5PContent.tsx
+++ src/Components/Core/Presentation/React/SpaceDisplay/ElementModal/SubComponents/NewH5PContent.tsx
@@ -7,22 +7,42 @@ import IScoreH5PElement from "src/Components/Core/Application/UseCases/ScoreH5PE
 import USECASE_TYPES from "~DependencyInjection/UseCases/USECASE_TYPES";
 import { useInjection } from "inversify-react";
 import IGetElementSourceUseCase from "src/Components/Core/Application/UseCases/GetElementSourceUseCase/IGetElementSourceUseCase";
-
 export const h5pEventCalled = (
   event: any,
   viewModel: ElementModalViewModel
 ) => {
-  //do something useful with the event
-  //logger.log("xAPI event: ", event);
-  const xapiData = event.data.statement;
-  if (
-    xapiData.verb.id === "http://adlnet.gov/expapi/verbs/completed" ||
-    (xapiData.verb.id === "http://adlnet.gov/expapi/verbs/answered" &&
-      typeof xapiData.result.success !== "undefined")
-  ) {
+  // Skip malformed events.
+  let hasStatement = event && event.data && event.data.statement;
+  if (!hasStatement) {
+    return;
+  }
+
+  let statement = event.data.statement;
+  let validVerb = statement.verb && statement.verb.id;
+  if (!validVerb) {
+    return;
+  }
+
+  let isCompleted =
+    statement.verb.id === "http://adlnet.gov/expapi/verbs/answered" ||
+    statement.verb.id === "http://adlnet.gov/expapi/verbs/completed";
+
+  let isChild =
+    statement.context &&
+    statement.context.contextActivities &&
+    statement.context.contextActivities.parent &&
+    statement.context.contextActivities.parent[0] &&
+    statement.context.contextActivities.parent[0].id;
+
+  if (isCompleted && !isChild) {
+    const xapiData = event.data.statement as XAPIData;
+
+    statement.result.success = statement.result.score.scaled == 1 || false;
+
     CoreDIContainer.get<IScoreH5PElement>(
       USECASE_TYPES.IScoreH5PElement
     ).executeAsync({
+      //@ts-ignore
       xapiData: xapiData,
       elementId: viewModel.id.Value,
       courseId: viewModel.parentCourseId.Value,
@@ -78,6 +98,28 @@ export default function NewH5PContent({
           /* istanbul ignore next */
           h5pEventCalled(event, viewModel);
         });
+        //@ts-ignore
+        H5P.xAPICompletedListener = function (t) {
+          if (
+            ("completed" === t.getVerb() || "answered" === t.getVerb()) &&
+            !t.getVerifiedStatementValue([
+              "context",
+              "contextActivities",
+              "parent",
+            ])
+          ) {
+            var n = t.getScore(),
+              r = t.getMaxScore(),
+              i = t.getVerifiedStatementValue([
+                "object",
+                "definition",
+                "extensions",
+                "http://h5p.org/x-api/h5p-local-content-id",
+              ]);
+            //@ts-ignore
+            e.setFinished(i, n, r);
+          }
+        };
       }
     };
     debug();
@@ -100,4 +142,108 @@ export default function NewH5PContent({
       ></div>
     </div>
   );
+}
+
+export interface Account {
+  name: string;
+  homePage: string;
+}
+
+export interface Actor {
+  name: string;
+  objectType: string;
+  account: Account;
+}
+
+export interface Display {
+  "en-US": string;
+}
+
+export interface Verb {
+  id: string;
+  display: Display;
+}
+
+export interface Name {
+  "en-US": string;
+}
+
+export interface Description {
+  "en-US": string;
+}
+
+export interface Description2 {
+  "en-US": string;
+}
+
+export interface Source {
+  id: string;
+  description: Description2;
+}
+
+export interface Description3 {
+  "en-US": string;
+}
+
+export interface Target {
+  id: string;
+  description: Description3;
+}
+
+export interface Definition {
+  name: Name;
+  description: Description;
+  type: string;
+  interactionType: string;
+  source: Source[];
+  correctResponsesPattern: string[];
+  target: Target[];
+}
+
+export interface Object {
+  id: string;
+  objectType: string;
+  definition: Definition;
+}
+
+export interface Parent {
+  id: string;
+  objectType: string;
+}
+
+export interface Category {
+  id: string;
+  objectType: string;
+}
+
+export interface ContextActivities {
+  parent: Parent[];
+  category: Category[];
+}
+
+export interface Context {
+  contextActivities: ContextActivities;
+}
+
+export interface Score {
+  min: number;
+  max: number;
+  raw: number;
+  scaled: number;
+}
+
+export interface Result {
+  score: Score;
+  completion: boolean;
+  success: boolean;
+  duration: string;
+  response: string;
+}
+
+export interface XAPIData {
+  actor: Actor;
+  verb: Verb;
+  object: Object;
+  context: Context;
+  result: Result;
 }
