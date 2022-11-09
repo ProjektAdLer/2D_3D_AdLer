1ea3948eea59f4ed2ea43d91d12a0b0564317206
LuckyLuke28 <37455486+LuckyLuke28@users.noreply.github.com>
2022-11-09 11:27:45 +0100

    styled MenuTutorial

--- src/Components/Core/Presentation/React/SpaceMenu/MenuTutorial/MenuTutorial.tsx
+++ src/Components/Core/Presentation/React/SpaceMenu/MenuTutorial/MenuTutorial.tsx
@@ -3,23 +3,25 @@ import { useState } from "react";
 export default function MenuTutorial() {
   const [isOpen, setIsOpen] = useState(true);
   return (
-    <div className="absolute left-5 bottom-5">
+    <div className="fixed right-2 top-2">
       <div
-        className="flex flex-col p-5 bg-white border-4 rounded border-adlerblue"
+        className="flex flex-col text-shadow text-white p-5 bg-adlerblue-700 rounded active:border-transparent border-adlerdarkblue border-b-4 border-r-4"
         onClick={() => {
           setIsOpen(!isOpen);
         }}
       >
         {isOpen ? (
-          <div>
-            <div className="pb-1 pl-1 font-bold">Menu Tutorial</div>
-            <div>Top Left Button gets you back to Title Screen</div>
-            <div>Leftclick on a Room Button to see Details</div>
-            <div>Enter unlocked Room through Button in Details</div>
-            <div>Leftclick this text to minimize</div>
+          <div className="text-shadow-none">
+            <div className="text-shadow pb-1 font-bold">Use Guide</div>
+            <ul className="pl-3 list-disc">
+              <li>Top Left Button gets you back to Title Screen </li>
+              <li> Leftclick on a Room Button to see Details </li>
+              <li>Enter unlocked Room through Button in Details</li>
+              <li>Leftclick this text to minimize</li>
+            </ul>
           </div>
         ) : (
-          <div className="font-bold">Menu Tutorial</div>
+          <div className="font-bold">Use Guide</div>
         )}
       </div>
     </div>
