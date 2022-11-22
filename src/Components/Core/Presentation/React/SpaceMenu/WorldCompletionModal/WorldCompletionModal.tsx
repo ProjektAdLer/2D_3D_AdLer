52c0571b4c0b1e04a5d8bdcef9da9b3d7452b9ed
Marius Kaczmarek <kaczmarek.m@gmx.de>
2022-11-22 12:35:09 +0100

    fixed still broken link

--- src/Components/Core/Presentation/React/SpaceMenu/WorldCompletionModal/WorldCompletionModal.tsx
+++ src/Components/Core/Presentation/React/SpaceMenu/WorldCompletionModal/WorldCompletionModal.tsx
@@ -35,7 +35,7 @@ export default function WorldCompletionModal() {
             Bitte fülle jetzt hier den{" "}
             <b className="text-adlerblue-900">
               {" "}
-              <a href="ww3.unipark.de/uc/softwareengineeringkempten/">
+              <a href="https://ww3.unipark.de/uc/softwareengineeringkempten/">
                 Evaluationsfragebogen{" "}
               </a>{" "}
             </b>{" "}
