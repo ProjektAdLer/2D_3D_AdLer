5ef03983626e8ff0034342633a3f4f15b31957ee
Marius Kaczmarek <kaczmarek.m@gmx.de>
2023-01-25 14:58:30 +0100

    refactored MobilePDFComponent button callbacks

--- src/Components/Core/Presentation/React/SpaceDisplay/ElementModal/SubComponents/PDFComponent.tsx
+++ src/Components/Core/Presentation/React/SpaceDisplay/ElementModal/SubComponents/PDFComponent.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import ElementModalViewModel from "../ElementModalViewModel";
 import PDFObject from "pdfobject";
 import { Document, Page, pdfjs } from "react-pdf";
@@ -14,8 +14,8 @@ export default function PDFComponent({
     <div className="w-[400px] lg:w-[800px] h-[80vh] text-black font-medium overflow-auto bg-adlerblue-100 p-3">
       {
         // comment in "false ? (" and comment out "PDFObject.supportsPDFs ? (" to test mobile version on desktop, reverse before pushing!!!
-        PDFObject.supportsPDFs ? (
-          // false ? (
+        // PDFObject.supportsPDFs ? (
+        false ? (
           <DesktopPDFComponent viewModel={viewModel} />
         ) : (
           <MobilePDFComponent viewModel={viewModel} />
@@ -50,9 +50,20 @@ function MobilePDFComponent({
   const [numPages, setNumPages] = useState<number>();
   const [pageNumber, setPageNumber] = useState(1);
 
-  function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
-    setNumPages(numPages);
-  }
+  const onDocumentLoadSuccess = useCallback(
+    ({ numPages }: { numPages: number }) => {
+      setNumPages(numPages);
+    },
+    [numPages]
+  );
+
+  const previousPage = useCallback(() => {
+    if (numPages) setPageNumber(Math.max(pageNumber - 1, 1));
+  }, [pageNumber, numPages]);
+
+  const nextPage = useCallback(() => {
+    if (numPages) setPageNumber(Math.min(pageNumber + 1, numPages));
+  }, [pageNumber, numPages]);
 
   return (
     <div className="h-full">
@@ -66,23 +77,11 @@ function MobilePDFComponent({
           renderAnnotationLayer={false}
         />
       </Document>
-      <StyledButton
-        onClick={() => {
-          setPageNumber(Math.max(pageNumber - 1, 1));
-        }}
-      >
-        {"<"}
-      </StyledButton>
+      <StyledButton onClick={previousPage}>{"<"}</StyledButton>
       <p>
         Page {pageNumber} of {numPages}
       </p>
-      <StyledButton
-        onClick={() => {
-          setPageNumber(Math.min(pageNumber + 1, numPages!));
-        }}
-      >
-        {">"}
-      </StyledButton>
+      <StyledButton onClick={nextPage}>{">"}</StyledButton>
     </div>
   );
 }
