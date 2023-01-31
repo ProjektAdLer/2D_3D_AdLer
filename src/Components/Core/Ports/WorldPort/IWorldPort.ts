05c9b3e7332656a73c962cc3799362776be71780
Marius Kaczmarek <kaczmarek.m@gmx.de>
2023-01-31 16:39:59 +0100

    added basis for WorldPort extension

--- src/Components/Core/Ports/WorldPort/IWorldPort.ts
+++ src/Components/Core/Ports/WorldPort/IWorldPort.ts
@@ -1,7 +1,20 @@
+import ElementTO from "../../Application/DataTransferObjects/ElementTO";
+import SpaceScoreTO from "../../Application/DataTransferObjects/SpaceScoreTO";
+import SpaceTO from "../../Application/DataTransferObjects/SpaceTO";
 import WorldTO from "../../Application/DataTransferObjects/WorldTO";
+import { ElementID } from "../../Domain/Types/EntityTypes";
 import { IAbstractPort } from "../AbstractPort/IAbstractPort";
 import IWorldAdapter from "./IWorldAdapter";
 
 export default interface IWorldPort extends IAbstractPort<IWorldAdapter> {
+  // world
   onWorldLoaded(worldTO: WorldTO): void;
+
+  // space
+  onSpaceLoaded(spaceTO: SpaceTO): void;
+  onSpaceScored(spaceScoreTO: SpaceScoreTO): void;
+
+  // element
+  onElementLoaded(elementStartedTO: ElementTO): void;
+  onElementScored(hasScored: boolean, elementID: ElementID): void;
 }
