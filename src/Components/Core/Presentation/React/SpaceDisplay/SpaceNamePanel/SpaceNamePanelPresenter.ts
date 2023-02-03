8dc1b70353d35f3a046991f7c370ab4692ff8bf0
Marius Kaczmarek <kaczmarek.m@gmx.de>
2023-02-03 15:07:57 +0100

    removed unused import

--- src/Components/Core/Presentation/React/SpaceDisplay/SpaceNamePanel/SpaceNamePanelPresenter.ts
+++ src/Components/Core/Presentation/React/SpaceDisplay/SpaceNamePanel/SpaceNamePanelPresenter.ts
@@ -1,5 +1,4 @@
 import SpaceTO from "src/Components/Core/Application/DataTransferObjects/SpaceTO";
-import { ElementID } from "src/Components/Core/Domain/Types/EntityTypes";
 import ISpaceNamePanelPresenter from "./ISpaceNamePanelPresenter";
 import SpaceNamePanelViewModel from "./SpaceNamePanelViewModel";
 
