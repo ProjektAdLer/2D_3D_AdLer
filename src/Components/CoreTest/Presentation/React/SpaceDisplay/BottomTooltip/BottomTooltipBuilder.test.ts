a9fd4903bc8eb60065aa52194977af0f72e00e07
Marius Kaczmarek <kaczmarek.m@gmx.de>
2023-02-08 15:14:08 +0100

    added additional test to BottomTooltipBuilder

--- src/Components/CoreTest/Presentation/React/SpaceDisplay/BottomTooltip/BottomTooltipBuilder.test.ts
+++ src/Components/CoreTest/Presentation/React/SpaceDisplay/BottomTooltip/BottomTooltipBuilder.test.ts
@@ -1,6 +1,8 @@
 import CoreDIContainer from "../../../../../Core/DependencyInjection/CoreDIContainer";
 import BottomTooltipBuilder from "../../../../../Core/Presentation/React/SpaceDisplay/BottomTooltip/BottomTooltipBuilder";
 import PRESENTATION_TYPES from "../../../../../Core/DependencyInjection/Presentation/PRESENTATION_TYPES";
+import { mock } from "jest-mock-extended";
+import IBottomTooltipPresenter from "../../../../../Core/Presentation/React/SpaceDisplay/BottomTooltip/IBottomTooltipPresenter";
 
 describe("BottomTooltipBuilder", () => {
   let systemUnderTest: BottomTooltipBuilder;
@@ -23,5 +25,18 @@ describe("BottomTooltipBuilder", () => {
     expect(
       CoreDIContainer.get(PRESENTATION_TYPES.IBottomTooltipPresenter)
     ).toBe(systemUnderTest.getPresenter()!);
+  });
+
+  test("buildPresenter unbinds the presenter if it is already bound", () => {
+    CoreDIContainer.bind(
+      PRESENTATION_TYPES.IBottomTooltipPresenter
+    ).toConstantValue(mock<IBottomTooltipPresenter>);
+
+    const unbindSpy = jest.spyOn(CoreDIContainer, "unbind");
+
+    systemUnderTest.buildViewModel();
+    systemUnderTest.buildPresenter();
+
+    expect(unbindSpy).toHaveBeenCalledTimes(1);
   });
 });
