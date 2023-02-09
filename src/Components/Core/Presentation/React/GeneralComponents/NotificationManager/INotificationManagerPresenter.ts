975b77af7aed7e3a828b340260b502f1b58a8887
Marius Kaczmarek <kaczmarek.m@gmx.de>
2023-02-09 15:20:09 +0100

    removed unused import

--- src/Components/Core/Presentation/React/GeneralComponents/NotificationManager/INotificationManagerPresenter.ts
+++ src/Components/Core/Presentation/React/GeneralComponents/NotificationManager/INotificationManagerPresenter.ts
@@ -1,5 +1,3 @@
-import IUIAdapter, {
-  NotificationType,
-} from "src/Components/Core/Ports/UIPort/IUIAdapter";
+import IUIAdapter from "src/Components/Core/Ports/UIPort/IUIAdapter";
 
 export default interface INotificationManagerPresenter extends IUIAdapter {}
