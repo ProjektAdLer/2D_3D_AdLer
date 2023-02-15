5ca03a112bcae6deadfd2db092a7dfd9b863fb8b
Felix Kleis <felixkleis@yahoo.de>
2023-02-15 15:51:47 +0100

    Completed WorldDetailBuilder Tests

--- src/Components/CoreTest/Presentation/React/WorldMenu/WorldDetail/WorldDetailBuilder.test.ts
+++ src/Components/CoreTest/Presentation/React/WorldMenu/WorldDetail/WorldDetailBuilder.test.ts
@@ -1,13 +1,35 @@
+import { mock } from "jest-mock-extended";
+import CoreDIContainer from "../../../../../Core/DependencyInjection/CoreDIContainer";
+import PORT_TYPES from "../../../../../Core/DependencyInjection/Ports/PORT_TYPES";
+import IWorldPort from "../../../../../Core/Ports/WorldPort/IWorldPort";
 import WorldDetailBuilder from "../../../../../Core/Presentation/React/WorldMenu/WorldDetail/WorldDetailBuilder";
 
+const worldPortMock = mock<IWorldPort>();
 describe("WorldDetailBuilder", () => {
   let systemUnderTest: WorldDetailBuilder;
 
+  beforeAll(() => {
+    CoreDIContainer.snapshot();
+
+    CoreDIContainer.rebind(PORT_TYPES.IWorldPort).toConstantValue(
+      worldPortMock
+    );
+  });
   beforeEach(() => {
     systemUnderTest = new WorldDetailBuilder();
   });
 
   test("constructor doesn't throw", () => {
     expect(systemUnderTest).toBeDefined();
+  });
+
+  test("buildPresenter builds the presenter and register it with the WorldPort", () => {
+    systemUnderTest.buildViewModel();
+    systemUnderTest.buildPresenter();
+
+    expect(systemUnderTest["presenter"]).toBeDefined();
+    expect(worldPortMock.registerAdapter).toHaveBeenCalledWith(
+      systemUnderTest["presenter"]
+    );
   });
 });
