1f98132c27d5d8538c7e0470d382271b7783d8a3
Marius Kaczmarek <kaczmarek.m@gmx.de>
2023-02-20 15:37:32 +0100

    LoginComponent is visible by default

--- src/Components/Core/Presentation/React/WelcomePage/LoginComponent/LoginComponentViewModel.ts
+++ src/Components/Core/Presentation/React/WelcomePage/LoginComponent/LoginComponentViewModel.ts
@@ -2,5 +2,5 @@ import Observable from "../../../../../../Lib/Observable";
 
 export default class LoginComponentViewModel {
   userLoggedIn = new Observable<boolean>(false);
-  modalVisible = new Observable<boolean>(false);
+  modalVisible = new Observable<boolean>(true);
 }
