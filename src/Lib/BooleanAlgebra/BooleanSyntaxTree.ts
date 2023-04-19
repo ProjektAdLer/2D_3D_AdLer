951bf33105e4f36869dcf0affd23ee8cad25f55b
Marius Kaczmarek <kaczmarek.m@gmx.de>
2023-04-19 16:25:52 +0200

    removed unused binary node

--- src/Lib/BooleanAlgebra/BooleanSyntaxTree.ts
+++ src/Lib/BooleanAlgebra/BooleanSyntaxTree.ts
@@ -4,22 +4,6 @@ export abstract class BooleanNode {
   abstract evaluate(evaluationMap: EvaluationMap): boolean;
 }
 
-export abstract class BooleanBinaryNode extends BooleanNode {
-  get LeftExpression(): BooleanNode {
-    return this.leftExpression;
-  }
-  get RightExpression(): BooleanNode {
-    return this.rightExpression;
-  }
-
-  constructor(
-    private readonly leftExpression: BooleanNode,
-    private readonly rightExpression: BooleanNode
-  ) {
-    super();
-  }
-}
-
 export abstract class BooleanNaryNode extends BooleanNode {
   get Expressions(): BooleanNode[] {
     return this.expressions;
@@ -34,6 +18,7 @@ export class BooleanValueNode extends BooleanNode {
   get Value(): string {
     return this.value;
   }
+
   constructor(private readonly value: string) {
     super();
   }
