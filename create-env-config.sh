affda11faee502368291b561d7da979e78cdbcd4
Markus Heck <markus.heck@hs-kempten.de>
2023-04-28 20:33:06 +0200

    fix create-env-config.sh

--- create-env-config.sh
+++ create-env-config.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+!/bin/bash
 rm -f /usr/share/nginx/html/env-config.js
 echo "window._env_ = {" >> /usr/share/nginx/html/env-config.js
 echo "    API_URL: '$API_URL'," >> /usr/share/nginx/html/env-config.js
 echo "};" >> /usr/share/nginx/html/env-config.js
+
