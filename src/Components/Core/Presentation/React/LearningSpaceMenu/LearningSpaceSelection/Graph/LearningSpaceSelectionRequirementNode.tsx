02da2d5b57bb0f52fc9a1b3e8560c51d413cb10a
Marius Kaczmarek <kaczmarek.m@gmx.de>
2023-05-02 12:36:12 +0200

    improved variable name

--- src/Components/Core/Presentation/React/LearningSpaceMenu/LearningSpaceSelection/Graph/LearningSpaceSelectionRequirementNode.tsx
+++ src/Components/Core/Presentation/React/LearningSpaceMenu/LearningSpaceSelection/Graph/LearningSpaceSelectionRequirementNode.tsx
@@ -4,7 +4,7 @@ import { Handle, Node, Position } from "reactflow";
 export type BooleanOperatorType = "and" | "or";
 
 type LearningSpaceSelectionRequirementNodeData = {
-  type: BooleanOperatorType;
+  operatorType: BooleanOperatorType;
 };
 export type LearningSpaceSelectionRequirementNodeType =
   Node<LearningSpaceSelectionRequirementNodeData>;
@@ -17,8 +17,8 @@ function LearningSpaceSelectionRequirementNode(
       <Handle type={"target"} position={Position.Top}></Handle>
       <div className="flex items-center justify-center w-12 h-6 border-b-2 border-r-2 rounded-md border-1 bg-nodehandlecolor -top-5 border-adlerdarkblue">
         <p className="font-[roboto] uppercase text-adlerdarkblue">
-          {props.data?.type === "and" && "und"}
-          {props.data?.type === "or" && "oder"}
+          {props.data?.operatorType === "and" && "und"}
+          {props.data?.operatorType === "or" && "oder"}
         </p>
       </div>
       <Handle type={"source"} position={Position.Bottom}></Handle>
