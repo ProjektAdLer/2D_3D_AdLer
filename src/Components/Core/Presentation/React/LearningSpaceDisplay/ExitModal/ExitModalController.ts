86b5f02ae086f74032cff968989360c8c8fb6c9e
Felix Kleis <felixkleis@yahoo.de>
2023-05-19 18:32:52 +0200

    Added functionality to exitmodal buttons to move between rooms

--- src/Components/Core/Presentation/React/LearningSpaceDisplay/ExitModal/ExitModalController.ts
+++ src/Components/Core/Presentation/React/LearningSpaceDisplay/ExitModal/ExitModalController.ts
@@ -1,10 +1,36 @@
-import { ComponentID } from "src/Components/Core/Domain/Types/EntityTypes";
+import type { ComponentID } from "src/Components/Core/Domain/Types/EntityTypes";
 import IExitModalController from "./IExitModalController";
 import history from "history/browser";
+import bind from "bind-decorator";
+import CoreDIContainer from "~DependencyInjection/CoreDIContainer";
+import ILoadLearningSpaceUseCase from "src/Components/Core/Application/UseCases/LoadLearningSpace/ILoadLearningSpaceUseCase";
+import USECASE_TYPES from "~DependencyInjection/UseCases/USECASE_TYPES";
+import IGetUserLocationUseCase from "src/Components/Core/Application/UseCases/GetUserLocation/IGetUserLocationUseCase";
+import ExitModalViewModel from "./ExitModalViewModel";
 
 export default class ExitModalController implements IExitModalController {
+  private loadLearningSpaceUseCase: ILoadLearningSpaceUseCase;
+  private getUserLocation: IGetUserLocationUseCase;
+  constructor(private viewModel: ExitModalViewModel) {
+    this.loadLearningSpaceUseCase = CoreDIContainer.get(
+      USECASE_TYPES.ILoadLearningSpaceUseCase
+    );
+    this.getUserLocation = CoreDIContainer.get(
+      USECASE_TYPES.IGetUserLocationUseCase
+    );
+  }
   onExitButtonClicked(): void {
     history.back();
   }
-  onSuccessorSpaceClicked(id: ComponentID): void {}
+
+  @bind
+  onSuccessorSpaceClicked(id: ComponentID): void {
+    const userLocation = this.getUserLocation.execute();
+    this.loadLearningSpaceUseCase.executeAsync({
+      spaceID: id,
+      worldID: userLocation.worldID!,
+    });
+    history.push("/spacedisplay/" + id);
+    this.viewModel.isOpen.Value = false;
+  }
 }
--- src/Components/CoreTest/Presentation/React/LearningSpaceDisplay/ExitModal/ExitModalController.test.ts
+++ src/Components/CoreTest/Presentation/React/LearningSpaceDisplay/ExitModal/ExitModalController.test.ts
@@ -1,17 +1,49 @@
 import history from "history/browser";
 import ExitModalController from "../../../../../Core/Presentation/React/LearningSpaceDisplay/ExitModal/ExitModalController";
+import ExitModalViewModel from "../../../../../Core/Presentation/React/LearningSpaceDisplay/ExitModal/ExitModalViewModel";
+import ILoadLearningSpaceUseCase from "../../../../../Core/Application/UseCases/LoadLearningSpace/ILoadLearningSpaceUseCase";
+import { mock } from "jest-mock-extended";
+import CoreDIContainer from "../../../../../Core/DependencyInjection/CoreDIContainer";
+import USECASE_TYPES from "../../../../../Core/DependencyInjection/UseCases/USECASE_TYPES";
+import IGetUserLocationUseCase from "../../../../../Core/Application/UseCases/GetUserLocation/IGetUserLocationUseCase";
+import { get } from "http";
 
 const mockHistoryBack = jest.spyOn(history, "back");
-
+const mockHistoryPush = jest.spyOn(history, "push");
+const viewModel = new ExitModalViewModel();
+const loadLearningSpaceUseCaseMock = mock<ILoadLearningSpaceUseCase>();
+const getUserLocationMock = mock<IGetUserLocationUseCase>();
 describe("ExitModalController", () => {
   let systemUnderTest: ExitModalController;
+  beforeAll(() => {
+    CoreDIContainer.snapshot();
+    CoreDIContainer.rebind(
+      USECASE_TYPES.ILoadLearningSpaceUseCase
+    ).toConstantValue(loadLearningSpaceUseCaseMock);
+    CoreDIContainer.rebind(
+      USECASE_TYPES.IGetUserLocationUseCase
+    ).toConstantValue(getUserLocationMock);
+  });
 
   beforeEach(() => {
-    systemUnderTest = new ExitModalController();
+    systemUnderTest = new ExitModalController(viewModel);
+  });
+  afterAll(() => {
+    CoreDIContainer.restore();
   });
 
   test("onExitButtonClicked calls history.back", () => {
     systemUnderTest.onExitButtonClicked();
     expect(mockHistoryBack).toBeCalled();
+  });
+  test("onSuccessorSpaceClicked calls LoadSpaceUseCase", () => {
+    getUserLocationMock.execute.mockReturnValue({ spaceID: 2, worldID: 1 });
+    systemUnderTest.onSuccessorSpaceClicked(42);
+
+    expect(loadLearningSpaceUseCaseMock.executeAsync).toBeCalledWith({
+      spaceID: 42,
+      worldID: 1,
+    });
+    expect(mockHistoryPush).toBeCalledWith("/spacedisplay/42");
   });
 });
