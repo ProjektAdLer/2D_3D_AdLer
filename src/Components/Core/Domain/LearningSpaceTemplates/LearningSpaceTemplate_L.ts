b420c82dde363cbbf80de8122c7c1d045abe39d3
Marius Kaczmarek <kaczmarek.m@gmx.de>
2023-05-24 16:05:15 +0200

    refactored L template config

--- src/Components/Core/Domain/LearningSpaceTemplates/LearningSpaceTemplate_L.ts
+++ src/Components/Core/Domain/LearningSpaceTemplates/LearningSpaceTemplate_L.ts
@@ -7,7 +7,6 @@ export const LearningSpaceTemplate_L: ILearningSpaceTemplate = {
     { x: 0, y: -7.6 }, // A
     { x: -6.8, y: -7.6 }, // B
     { x: -6.8, y: 5.2 }, // C
-    { x: 0, y: 5.2 }, // D
     { x: 5.6, y: 5.2 }, // E
     { x: 5.6, y: 0 }, // F
     { x: 0, y: 0 }, // G
@@ -15,80 +14,79 @@ export const LearningSpaceTemplate_L: ILearningSpaceTemplate = {
   walls: [
     { start: 1, end: 2 },
     { start: 2, end: 3 },
-    { start: 3, end: 4 },
   ],
   elementSlots: [
     {
       // 1
       position: { x: -6, y: -5.2 },
-      orientation: { rotation: Math.PI / 2 },
+      orientation: { rotation: 180 },
     },
     {
       // 2
       position: { x: -6, y: -2 },
-      orientation: { rotation: Math.PI / 2 },
+      orientation: { rotation: 180 },
     },
     {
       // 3
       position: { x: -6, y: 1.2 },
-      orientation: { rotation: Math.PI / 2 },
+      orientation: { rotation: 180 },
     },
     {
       // 4
       position: { x: -2.4, y: 4.4 },
-      orientation: { rotation: 0 },
+      orientation: { rotation: 90 },
     },
     {
       // 5
       position: { x: 0.8, y: 4.4 },
-      orientation: { rotation: 0 },
+      orientation: { rotation: 90 },
     },
     {
       // 6
       position: { x: 4, y: 4.4 },
-      orientation: { rotation: 0 },
+      orientation: { rotation: 90 },
     },
     {
       // 7
       position: { x: 4, y: 0.8 },
-      orientation: { rotation: Math.PI },
+      orientation: { rotation: -90 },
     },
     {
       // 8
       position: { x: 0.8, y: 0.8 },
-      orientation: { rotation: Math.PI },
+      orientation: { rotation: -90 },
     },
     {
       // 9
       position: { x: -0.8, y: -2 },
-      orientation: { rotation: -(Math.PI / 2) },
+      orientation: { rotation: 0 },
     },
     {
       // 10
       position: { x: -0.8, y: -5.2 },
-      orientation: { rotation: -(Math.PI / 2) },
+      orientation: { rotation: 0 },
     },
   ],
   entranceDoor: {
     position: { x: -3.2, y: -7.6 },
-    orientation: { rotation: Math.PI },
+    orientation: { rotation: -90 },
   },
   exitDoor: {
     position: { x: 5.6, y: 2.4 },
-    orientation: { rotation: -(Math.PI / 2) },
+    orientation: { rotation: 0 },
   },
   windows: [
     {
       position: { x: -6.8, y: -3.6 },
-      orientation: { rotation: Math.PI / 2 },
+      orientation: { rotation: 180 },
     },
     {
       position: { x: -0.8, y: 5.2 },
-      orientation: { rotation: 0 },
+      orientation: { rotation: 90 },
     },
     {
       position: { x: 2.4, y: 5.2 },
-      orientation: { rotation: 0 },
+      orientation: { rotation: 90 },
     },
   ],
 };
