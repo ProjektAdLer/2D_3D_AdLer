2c1f4329a37beea0c4134deeb6cdc1d6d7c8d9d8
Marius Kaczmarek <kaczmarek.m@gmx.de>
2023-05-30 19:52:55 +0200

    implemented randomizer on an array

---
+++ src/Components/CoreTest/Presentation/Utils/ArrayItemRandomizer.test.ts
@@ -0,0 +1,27 @@
+import ArrayItemRandomizer from "../../../Core/Presentation/Utils/ArrayItemRandomizer/ArrayItemRandomizer";
+
+let mockItems: string[] = [];
+for (let i = 0; i < 100; i++) {
+  mockItems.push(`item${i}`);
+}
+
+describe("ArrayItemRandomizer", () => {
+  let systemUnderTest: ArrayItemRandomizer<string>;
+
+  beforeEach(() => {
+    systemUnderTest = new ArrayItemRandomizer(mockItems);
+  });
+
+  test("getImport should return an import from the given list without a seed", () => {
+    expect(mockItems).toContain(systemUnderTest.getImport());
+  });
+
+  test("getImport should return the same import with the same seed", () => {
+    const result1 = systemUnderTest.getImport("mockSeed");
+    const result2 = systemUnderTest.getImport("mockSeed");
+
+    expect(mockItems).toContain(result1);
+    expect(mockItems).toContain(result2);
+    expect(result1).toEqual(result2);
+  });
+});
