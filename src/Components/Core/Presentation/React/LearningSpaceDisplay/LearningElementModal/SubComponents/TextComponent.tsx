dd874aebba91b0ae3a05a72cb10938bf91ea2940
LuckyLuke28 <37455486+LuckyLuke28@users.noreply.github.com>
2023-05-31 10:50:53 +0200

    added formatting to text-elements

--- src/Components/Core/Presentation/React/LearningSpaceDisplay/LearningElementModal/SubComponents/TextComponent.tsx
+++ src/Components/Core/Presentation/React/LearningSpaceDisplay/LearningElementModal/SubComponents/TextComponent.tsx
@@ -18,9 +18,11 @@ export default function TextComponent({
 
   return (
     <div className="flex justify-center max-h-[75vh] lg:max-h-[85vh] xl:max-h-[85vh] w-fit h-fit max-w-[99vw] font-medium text-black">
-      <p className="w-[80vw] md:w-[60vw] lg:w-[50vw] xl:w-[40vw] 2xl:[30vw] p-8 bg-white rounded-lg overflow-auto">
-        {text}
-      </p>
+      <pre>
+        <p className="w-[80vw] md:w-[60vw] lg:w-[50vw] xl:w-[40vw] 2xl:[30vw] p-8 bg-white rounded-lg overflow-auto">
+          {text}
+        </p>
+      </pre>
     </div>
   );
 }
