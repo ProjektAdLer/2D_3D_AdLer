2fea4651885bb15d311b33a2904d644a676bbfae
Marius Kaczmarek <kaczmarek.m@gmx.de>
2023-05-31 13:54:13 +0200

    removed case sensitivity from templates

--- src/Components/Core/Domain/Types/LearningSpaceTemplateType.ts
+++ src/Components/Core/Domain/Types/LearningSpaceTemplateType.ts
@@ -1,7 +1,7 @@
 export enum LearningSpaceTemplateType {
-  L = "L_32x31_10L",
-  R6 = "R_20x20_6L",
-  R8 = "R_20x30_8L",
+  L = "L_32X31_10L",
+  R6 = "R_20X20_6L",
+  R8 = "R_20X30_8L",
   None = "",
 }
 
