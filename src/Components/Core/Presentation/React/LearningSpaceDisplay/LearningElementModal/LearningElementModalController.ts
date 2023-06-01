97c0a2f44dcec31dda06531bb2f400c7653bb546
philgei <philipp@geirhos.com>
2023-06-01 16:27:00 +0200

    updated xAPI handling to be compliant with Moodle

--- src/Components/Core/Presentation/React/LearningSpaceDisplay/LearningElementModal/LearningElementModalController.ts
+++ src/Components/Core/Presentation/React/LearningSpaceDisplay/LearningElementModal/LearningElementModalController.ts
@@ -20,24 +20,30 @@ export default class LearningElementModalController
   }
 
   async h5pEventCalled(event: any): Promise<void> {
-    // Skip malformed events
-    if (!event?.data?.statement) return;
+    // Skip malformed events.
+    var hasStatement = event && event.data && event.data.statement;
+    if (!hasStatement) {
+      return;
+    }
 
-    const statement = event.data.statement;
-    if (!statement.verb?.id) return;
+    var statement = event.data.statement;
+    var validVerb = statement.verb && statement.verb.id;
+    if (!validVerb) {
+      return;
+    }
 
-    const isCompleted =
+    var isCompleted =
       statement.verb.id === "http://adlnet.gov/expapi/verbs/answered" ||
       statement.verb.id === "http://adlnet.gov/expapi/verbs/completed";
 
-    // const parentArray = statement.context?.contextActivities?.parent;
-    // const isChild =
-    //   parentArray && parentArray.length > 0 ? parentArray[0]?.id : undefined;
+    var isChild =
+      statement.context &&
+      statement.context.contextActivities &&
+      statement.context.contextActivities.parent &&
+      statement.context.contextActivities.parent[0] &&
+      statement.context.contextActivities.parent[0].id;
 
-    //if (isCompleted && !isChild) {
-
-    // Why is the isChild check here? - PG
-    if (isCompleted) {
+    if (isCompleted && !isChild) {
       const xapiData = event.data.statement as XAPIData;
 
       statement.result.success =
