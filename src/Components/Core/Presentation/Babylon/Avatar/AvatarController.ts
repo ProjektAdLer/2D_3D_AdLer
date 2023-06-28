c41d29c6e34b26733c7e0e5a836861c4a0ebc489
Felix Kleis <felixkleis@yahoo.de>
2023-06-28 14:30:01 +0200

    Added Prototype Movement Indicator

--- src/Components/Core/Presentation/Babylon/Avatar/AvatarController.ts
+++ src/Components/Core/Presentation/Babylon/Avatar/AvatarController.ts
@@ -7,7 +7,11 @@ import {
   MeshBuilder,
   PointerEventTypes,
   PointerInfo,
+  StandardMaterial,
   Vector3,
+  Animation,
+  Color3,
+  Mesh,
 } from "@babylonjs/core";
 import bind from "bind-decorator";
 import SCENE_TYPES, {
@@ -29,6 +33,12 @@ export default class AvatarController implements IAvatarController {
   private navigation: INavigation;
   private pathLine: LinesMesh;
 
+  //movement indicator
+  private movementIndicator: Mesh;
+  private movementIndicatorMaterial: StandardMaterial;
+  private movementIndicatorAnimationX: Animation;
+  private movementIndicatorAnimationZ: Animation;
+
   constructor(private viewModel: AvatarViewModel) {
     this.navigation = CoreDIContainer.get<INavigation>(CORE_TYPES.INavigation);
     let scenePresenterFactory = CoreDIContainer.get<ScenePresenterFactory>(
@@ -41,6 +51,46 @@ export default class AvatarController implements IAvatarController {
       this.processKeyboardEvent
     );
     this.scenePresenter.Scene.onBeforeRenderObservable.add(this.applyInputs);
+
+    // create a movement indicator
+    this.movementIndicator = MeshBuilder.CreateTorus(
+      "movementIndicator",
+      { diameter: 0.5, thickness: 0.1 },
+      this.scenePresenter.Scene
+    );
+    this.movementIndicatorMaterial = new StandardMaterial(
+      "movementIndicatorMaterial",
+      this.scenePresenter.Scene
+    );
+    this.movementIndicatorAnimationX = new Animation(
+      "movementIndicatorAnimation",
+      "scaling.x",
+      100,
+      Animation.ANIMATIONTYPE_FLOAT
+    );
+    this.movementIndicatorAnimationZ = new Animation(
+      "movementIndicatorAnimation",
+      "scaling.z",
+      100,
+      Animation.ANIMATIONTYPE_FLOAT
+    );
+    this.movementIndicator.isVisible = false;
+
+    this.movementIndicatorAnimationX.setKeys([
+      { frame: 0, value: 1 },
+      { frame: 50, value: 2 },
+      { frame: 100, value: 1 },
+    ]);
+    this.movementIndicatorAnimationZ.setKeys([
+      { frame: 0, value: 1 },
+      { frame: 50, value: 2 },
+      { frame: 100, value: 1 },
+    ]);
+    this.movementIndicator.animations = [
+      this.movementIndicatorAnimationX,
+      this.movementIndicatorAnimationZ,
+    ];
+    this.movementIndicatorMaterial.diffuseColor = new Color3(0.66, 0.83, 0.98);
   }
 
   @bind
@@ -49,20 +99,15 @@ export default class AvatarController implements IAvatarController {
       if (this.viewModel.pointerMovementThreshold)
         this.navigation.Crowd.agentGoto(
           this.viewModel.agentIndex,
-          this.navigation.Plugin.getClosestPoint(
-            this.viewModel.parentNode.position.add(
-              this.viewModel.keyMovementTarget
-            )
-          )
+          this.viewModel.keyMovementTarget
         );
 
       this.debug_drawPath(this.viewModel.keyMovementTarget);
 
       this.viewModel.keyMovementTarget = Vector3.Zero();
     } else if (!this.viewModel.pointerMovementTarget.equals(Vector3.Zero())) {
-      const targetOnNavMesh = this.navigation.Plugin.getClosestPoint(
-        this.viewModel.pointerMovementTarget
-      );
+      const targetOnNavMesh = this.viewModel.pointerMovementTarget;
+
       const movementDistance = targetOnNavMesh
         .subtract(this.viewModel.parentNode.position)
         .length();
@@ -98,6 +143,9 @@ export default class AvatarController implements IAvatarController {
           .normalize()
       )
       .normalize();
+    this.viewModel.keyMovementTarget = this.navigation.Plugin.getClosestPoint(
+      this.viewModel.keyMovementTarget
+    );
   }
 
   private getReferenceAxisByKey(key: string): Vector3 {
@@ -124,6 +172,24 @@ export default class AvatarController implements IAvatarController {
       return;
 
     this.viewModel.pointerMovementTarget = pointerInfo.pickInfo.pickedPoint;
+    this.viewModel.pointerMovementTarget =
+      this.navigation.Plugin.getClosestPoint(
+        this.viewModel.pointerMovementTarget
+      );
+    this.movementIndicator.position = this.viewModel.pointerMovementTarget;
+    this.movementIndicator.isVisible = true;
+    this.scenePresenter.Scene.beginAnimation(
+      this.movementIndicator,
+      0,
+      100,
+      false,
+      1,
+      () => (
+        (this.movementIndicator.isVisible = false),
+        this.scenePresenter.Scene.stopAnimation(this.movementIndicator),
+        console.log("done", this.movementIndicatorAnimationX)
+      )
+    );
   }
 
   private debug_drawPath(target: Vector3): void {
