4af1d2826ee178d0d87708c725e1272a67c1dafc
Felix Kleis <felixkleis@yahoo.de>
2023-07-08 08:24:26 +0200

    Added test to vimeo testing suite, added another skipped test for later fixing

--- src/Components/CoreTest/Presentation/React/LearningSpaceDisplay/LearningElementModal/SubComponents/VideoHosters/VimeoVideoHost.test.tsx
+++ src/Components/CoreTest/Presentation/React/LearningSpaceDisplay/LearningElementModal/SubComponents/VideoHosters/VimeoVideoHost.test.tsx
@@ -1,14 +1,39 @@
 import { render } from "@testing-library/react";
 import React from "react";
 import VimeoVideoHost from "./../../../../../../../Core/Presentation/React/LearningSpaceDisplay/LearningElementModal/SubComponents/VideoHosters/VimeoVideoHost";
+import mock, { mockDeep } from "jest-mock-extended/lib/Mock";
+import axios from "axios";
+import { logger } from "../../../../../../../../Lib/Logger";
+
+const url = "https://vimeo.com/236357509";
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe("VimeoVideoHost", () => {
   it.skip("renders an iframe with the provided URL", () => {
-    const url = "https://vimeo.com/236357509";
     const { container } = render(<VimeoVideoHost url={url} />);
     const iframe = container.getElementsByClassName(
       "w-full rounded-lg aspect-video"
     )[0];
     expect(iframe).toHaveAttribute("src", url);
+  });
+  it("returns null if no URL is provided", () => {
+    // @ts-ignore
+    const { container } = render(<VimeoVideoHost />);
+    expect(container.firstChild).toBeNull();
+  });
+  test.skip("logs error if srcArray is not of length 2", async () => {
+    mockedAxios.get.mockResolvedValue({
+      data: {
+        html: "URL",
+        title: "Title",
+      },
+    });
+
+    render(<VimeoVideoHost url={url} />);
+
+    expect(logger.error).toBeCalledWith(
+      "Could not extract video url from vimeo oembed api"
+    );
   });
 });
