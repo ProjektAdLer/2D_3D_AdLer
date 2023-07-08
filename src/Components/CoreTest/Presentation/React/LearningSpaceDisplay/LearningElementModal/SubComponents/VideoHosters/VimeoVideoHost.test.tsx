726c2b163ae3c9f006ab8b1dd4219dae6dd95b24
Felix Kleis <felixkleis@yahoo.de>
2023-07-08 09:27:03 +0200

    tinkered a bit more with vimeo tests

--- src/Components/CoreTest/Presentation/React/LearningSpaceDisplay/LearningElementModal/SubComponents/VideoHosters/VimeoVideoHost.test.tsx
+++ src/Components/CoreTest/Presentation/React/LearningSpaceDisplay/LearningElementModal/SubComponents/VideoHosters/VimeoVideoHost.test.tsx
@@ -1,23 +1,35 @@
 import { render } from "@testing-library/react";
 import React from "react";
 import VimeoVideoHost from "./../../../../../../../Core/Presentation/React/LearningSpaceDisplay/LearningElementModal/SubComponents/VideoHosters/VimeoVideoHost";
-import mock, { mockDeep } from "jest-mock-extended/lib/Mock";
 import axios from "axios";
 import { logger } from "../../../../../../../../Lib/Logger";
 
 const url = "https://vimeo.com/236357509";
+jest.mock("src/Lib/Logger");
 jest.mock("axios");
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe("VimeoVideoHost", () => {
-  it.skip("renders an iframe with the provided URL", () => {
+  test.skip("renders an iframe with the provided URL", () => {
+    mockedAxios.get.mockResolvedValue({
+      data: {
+        html: "https://vimeo.com/236357509",
+        title: "title",
+      },
+    });
     const { container } = render(<VimeoVideoHost url={url} />);
     const iframe = container.getElementsByClassName(
       "w-full rounded-lg aspect-video"
     )[0];
     expect(iframe).toHaveAttribute("src", url);
   });
-  it("returns null if no URL is provided", () => {
+  test("returns null if no URL is provided", () => {
+    mockedAxios.get.mockResolvedValue({
+      data: {
+        html: "URL",
+        title: "Title",
+      },
+    });
     // @ts-ignore
     const { container } = render(<VimeoVideoHost />);
     expect(container.firstChild).toBeNull();
@@ -25,15 +37,16 @@ describe("VimeoVideoHost", () => {
   test.skip("logs error if srcArray is not of length 2", async () => {
     mockedAxios.get.mockResolvedValue({
       data: {
-        html: "URL",
-        title: "Title",
+        html: "url",
+        title: "title",
+        test: "test",
       },
     });
 
     render(<VimeoVideoHost url={url} />);
 
-    expect(logger.error).toBeCalledWith(
-      "Could not extract video url from vimeo oembed api"
-    );
+    //test should work as is, coverage shows it goes through error messages, yet nothing gets logged ~fk
+
+    expect(logger.error).toBeCalledTimes(1);
   });
 });
