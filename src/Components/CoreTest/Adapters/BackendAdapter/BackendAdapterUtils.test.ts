4e7141bad3660aa6a1bfcc126836c053643766dd
Marius Kaczmarek <kaczmarek.m@gmx.de>
2023-08-04 13:25:01 +0200

    extended BackendAdapterUtils tests

--- src/Components/CoreTest/Adapters/BackendAdapter/BackendAdapterUtils.test.ts
+++ src/Components/CoreTest/Adapters/BackendAdapter/BackendAdapterUtils.test.ts
@@ -16,4 +16,41 @@ describe("BackendAdapterUtils", () => {
     ]);
     expect(result).toHaveLength(0);
   });
+
+  test("mapElements return an element with given model if the modelname is valid", () => {
+    const result = BackendAdapterUtils["mapElements"]([
+      {
+        elementId: 1,
+        elementName: "test",
+        elementDescription: "test",
+        elementGoals: ["test"],
+        elementCategory: "h5p",
+        elementFileType: "test",
+        elementMaxScore: 1,
+        elementModel:
+          LearningElementModelTypeEnums.H5pElementModelTypes.Blackboard,
+      },
+    ]);
+    expect(result[0].model).toBe(
+      LearningElementModelTypeEnums.H5pElementModelTypes.Blackboard
+    );
+  });
+
+  test("mapElements return an element with NoElementModelTypes.None if the modelname is invalid", () => {
+    const result = BackendAdapterUtils["mapElements"]([
+      {
+        elementId: 1,
+        elementName: "test",
+        elementDescription: "test",
+        elementGoals: ["test"],
+        elementCategory: "h5p",
+        elementFileType: "test",
+        elementMaxScore: 1,
+        elementModel: "invalid",
+      },
+    ]);
+    expect(result[0].model).toBe(
+      LearningElementModelTypeEnums.NoElementModelTypes.None
+    );
+  });
 });
