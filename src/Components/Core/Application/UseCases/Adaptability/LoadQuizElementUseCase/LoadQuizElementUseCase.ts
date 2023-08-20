8c99ab201815b8bff92f86f73de445fd1dff600d
Felix Kleis <felixkleis@yahoo.de>
2023-08-20 18:43:19 +0200

    Added Logger to LoadQuizElementUseCase

--- src/Components/Core/Application/UseCases/Adaptability/LoadQuizElementUseCase/LoadQuizElementUseCase.ts
+++ src/Components/Core/Application/UseCases/Adaptability/LoadQuizElementUseCase/LoadQuizElementUseCase.ts
@@ -3,10 +3,15 @@ import ILoadQuizElementUseCase from "./ILoadQuizElementUseCase";
 import QuizElementTO from "../../../DataTransferObjects/QuizElementTO";
 import PORT_TYPES from "~DependencyInjection/Ports/PORT_TYPES";
 import type ILearningWorldPort from "../../../Ports/Interfaces/ILearningWorldPort";
+import CORE_TYPES from "~DependencyInjection/CoreTypes";
+import type ILoggerPort from "../../../Ports/Interfaces/ILoggerPort";
+import { LogLevelTypes } from "src/Components/Core/Domain/Types/LogLevelTypes";
 
 @injectable()
 export default class LoadQuizElementUseCase implements ILoadQuizElementUseCase {
   constructor(
+    @inject(CORE_TYPES.ILogger)
+    private logger: ILoggerPort,
     @inject(PORT_TYPES.ILearningWorldPort)
     private worldPort: ILearningWorldPort
   ) {}
@@ -21,6 +26,10 @@ export default class LoadQuizElementUseCase implements ILoadQuizElementUseCase {
           this.worldPort.onAdaptabilityElementLoaded(quizElement)
         )
       );
+    this.logger.log(
+      LogLevelTypes.TRACE,
+      `LoadQuizElementUseCase: Loaded QuizElement from ${filepath}.`
+    );
 
     return Promise.resolve();
   }
