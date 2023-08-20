a70990be8a422c1d068e7f11022a7cc1ff5f4e80
Felix Kleis <felixkleis@yahoo.de>
2023-08-20 18:21:46 +0200

    Added Logger to GetLearningElementSourceUseCase

--- src/Components/Core/Application/UseCases/GetLearningElementSource/GetLearningElementSourceUseCase.ts
+++ src/Components/Core/Application/UseCases/GetLearningElementSource/GetLearningElementSourceUseCase.ts
@@ -4,12 +4,15 @@ import UserDataEntity from "src/Components/Core/Domain/Entities/UserDataEntity";
 import CORE_TYPES from "~DependencyInjection/CoreTypes";
 import IGetLearningElementSourceUseCase from "./IGetLearningElementSourceUseCase";
 import { inject, injectable } from "inversify";
-
+import type ILoggerPort from "../../Ports/Interfaces/ILoggerPort";
+import { LogLevelTypes } from "src/Components/Core/Domain/Types/LogLevelTypes";
 @injectable()
 export default class GetLearningElementSourceUseCase
   implements IGetLearningElementSourceUseCase
 {
   constructor(
+    @inject(CORE_TYPES.ILogger)
+    private logger: ILoggerPort,
     @inject(CORE_TYPES.IEntityContainer)
     private entityContainer: IEntityContainer,
     @inject(CORE_TYPES.IBackendAdapter)
@@ -29,6 +32,11 @@ export default class GetLearningElementSourceUseCase
       elementID: data.elementID,
       worldID: data.worldID,
     });
+
+    this.logger.log(
+      LogLevelTypes.TRACE,
+      `GetLearningElementSourceUseCase: Got source for element ${data.elementID} in world ${data.worldID}`
+    );
 
     return resp;
   }
