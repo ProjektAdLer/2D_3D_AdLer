0c28042d4174ac430cb645fd5f099d7e8ae4b913
Felix Kleis <felixkleis@yahoo.de>
2023-08-20 16:13:33 +0200

    Removed unused imports in a test.

--- src/Components/CoreTest/Application/UseCases/LoadAvatar/LoadAvatarUseCase.test.ts
+++ src/Components/CoreTest/Application/UseCases/LoadAvatar/LoadAvatarUseCase.test.ts
@@ -1,11 +1,8 @@
 import mock from "jest-mock-extended/lib/Mock";
 import Logger from "../../../../Core/Adapters/Logger/Logger";
-import IAvatarPort from "../../../../Core/Application/UseCases/LoadAvatar/IAvatarPort";
 import LoadAvatarUseCase from "../../../../Core/Application/UseCases/LoadAvatar/LoadAvatarUseCase";
 import CoreDIContainer from "../../../../Core/DependencyInjection/CoreDIContainer";
 import CORE_TYPES from "../../../../Core/DependencyInjection/CoreTypes";
-import PORT_TYPES from "../../../../Core/DependencyInjection/Ports/PORT_TYPES";
-import PresentationDirector from "../../../../Core/Presentation/PresentationBuilder/PresentationDirector";
 import USECASE_TYPES from "../../../../Core/DependencyInjection/UseCases/USECASE_TYPES";
 
 const mockLogger = mock<Logger>();
