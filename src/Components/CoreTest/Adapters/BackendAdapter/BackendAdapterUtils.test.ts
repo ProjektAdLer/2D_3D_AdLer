ceb290f6d60a524df6547e761c34f990b8d0fe9d
Marius Kaczmarek <kaczmarek.m@gmx.de>
2023-08-23 10:38:37 +0200

    extended BackendAdapterUtils tests

--- src/Components/CoreTest/Adapters/BackendAdapter/BackendAdapterUtils.test.ts
+++ src/Components/CoreTest/Adapters/BackendAdapter/BackendAdapterUtils.test.ts
@@ -1,6 +1,40 @@
 import BackendAdapterUtils from "../../../Core/Adapters/BackendAdapter/BackendAdapterUtils";
+import { APISpace } from "../../../Core/Adapters/BackendAdapter/Types/IDSL";
+import BackendElementTO from "../../../Core/Application/DataTransferObjects/BackendElementTO";
+import { LearningElementModelTypeEnums } from "../../../Core/Domain/Types/LearningElementModelTypes";
 
 describe("BackendAdapterUtils", () => {
+  test("mapSpaces ", () => {
+    const inputSpaces: APISpace[] = [
+      {
+        spaceId: 1,
+        spaceName: "test",
+        spaceDescription: "test",
+        spaceGoals: ["test"],
+        spaceTemplate: "test",
+        spaceTemplateStyle: "test",
+        spaceSlotContents: [1],
+        requiredPointsToComplete: 1,
+        requiredSpacesToEnter: "test",
+      },
+    ];
+    const inputElements: BackendElementTO[] = [
+      {
+        id: 1,
+        name: "test",
+        description: "test",
+        goals: ["test"],
+        model: LearningElementModelTypeEnums.NoElementModelTypes.None,
+        type: "h5p",
+        value: 1,
+      },
+    ];
+
+    const result = BackendAdapterUtils["mapSpaces"](inputSpaces, inputElements);
+
+    expect(result).toHaveLength(1);
+  });
+
   test("mapElements returns empty array if given element type is not supported", () => {
     const result = BackendAdapterUtils["mapElements"]([
       {
