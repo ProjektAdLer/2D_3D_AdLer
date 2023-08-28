1a2ea683233964dde94266da6a4cc9dd7a67aaa4
DanielGlaab <daniel.glaab@th-ab.de>
2023-08-28 11:39:59 +0200

    fixed typo which caused problems in menus

--- src/Components/Core/Presentation/React/ReactRelated/ReactEntryPoint/LearningWorldMenu.tsx
+++ src/Components/Core/Presentation/React/ReactRelated/ReactEntryPoint/LearningWorldMenu.tsx
@@ -8,8 +8,8 @@ export default function LearningWorldMenu() {
     <React.Fragment>
       <main className="flex flex-col h-[100svh] bg-gradient-to-br from-adlerbggradientfrom to-adlerbggradientto overflow-hidden">
         <section className="grid order-2 min-h-full grid-cols-2 grid-rows-1 portrait:grid-rows-[1fr, 2fr] portrait:grid-cols-1 portrait:overflow-auto grow portrait:grow-0 portrait:auto-rows-min lg:rounded-lg">
-          <LearningWorldSelection className="col-start-1 portrait:col-start-1 portrait:row-start-1 p-2 m-2 rounded-lg lg:p-8 portrait:overflow-auto h-[90svh] portrait-h-[30svh]" />
-          <LearningWorldDetail className="col-start-2 portrait:col-start-1 portrait:row-start-2 p-2 lg:p-8 rounded-lg m-2 portrait:h-[75svh] h-[90svh]" />
+          <LearningWorldSelection className="col-start-1 portrait:col-start-1 portrait:row-start-1 p-2 m-2 rounded-lg lg:p-8 portrait:overflow-auto h-[90svh] portrait:h-[25svh]" />
+          <LearningWorldDetail className="col-start-2 portrait:col-start-1 portrait:row-start-2 p-2 lg:p-8 rounded-lg m-2 portrait:h-[73svh] h-[90svh]" />
         </section>
         <MenuHeaderBar
           location="world"
