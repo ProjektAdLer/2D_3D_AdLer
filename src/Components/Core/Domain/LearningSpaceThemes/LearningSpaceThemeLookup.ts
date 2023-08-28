a6cd869b51548b0c69d318b33a0ed139d9f31bb8
Marius Kaczmarek <kaczmarek.m@gmx.de>
2023-08-28 11:45:45 +0200

    implemented temporary fix for wip theme configs

--- src/Components/Core/Domain/LearningSpaceThemes/LearningSpaceThemeLookup.ts
+++ src/Components/Core/Domain/LearningSpaceThemes/LearningSpaceThemeLookup.ts
@@ -4,13 +4,14 @@ import LearningSpaceTheme_Campus from "./LearningSpaceTheme_Campus";
 export default class LearningSpaceThemeLookup {
   static getLearningSpaceTheme(type: LearningSpaceThemeType) {
     switch (type) {
+      default: // remove default when more themes are implemented
       case LearningSpaceThemeType.Campus:
         return LearningSpaceTheme_Campus;
       // TODO: add other themes here
     }
 
-    throw new Error(
-      `LearningSpaceThemeLookup.getLearningSpaceTheme: Learning space theme for ${type} not found.`
-    );
+    // throw new Error(
+    //   `LearningSpaceThemeLookup.getLearningSpaceTheme: Learning space theme for ${type} not found.`
+    // );
   }
 }
