617f357a416e95375ec9de45f0ab1d2502b27994
Felix Kleis <felixkleis@yahoo.de>
2023-09-01 13:13:07 +0200

    Added Logger to abstractPort tests (no functional change)

--- src/Components/CoreTest/Application/Ports/AbstractPort/AbstractPort.test.ts
+++ src/Components/CoreTest/Application/Ports/AbstractPort/AbstractPort.test.ts
@@ -1,5 +1,10 @@
 import { mock } from "jest-mock-extended";
 import AbstractPort from "../../../../Core/Application/Ports/AbstractPort/AbstractPort";
+import Logger from "../../../../Core/Adapters/Logger/Logger";
+import CoreDIContainer from "../../../../Core/DependencyInjection/CoreDIContainer";
+import CORE_TYPES from "../../../../Core/DependencyInjection/CoreTypes";
+
+const loggerMock = mock<Logger>();
 
 interface ITestAdapter {}
 class TestPort extends AbstractPort<ITestAdapter> {}
@@ -9,8 +14,17 @@ const testAdapter: ITestAdapter = mock<ITestAdapter>();
 describe("AbstractPort", () => {
   let systemUnderTest: TestPort;
 
+  beforeAll(() => {
+    CoreDIContainer.snapshot();
+
+    CoreDIContainer.rebind(CORE_TYPES.ILogger).toConstantValue(loggerMock);
+  });
   beforeEach(() => {
     systemUnderTest = new TestPort();
+  });
+
+  afterAll(() => {
+    CoreDIContainer.restore();
   });
 
   test("registerAdapter adds an adapter", () => {
@@ -24,7 +38,7 @@ describe("AbstractPort", () => {
     systemUnderTest.registerAdapter(testAdapter);
     systemUnderTest.registerAdapter(testAdapter);
 
-    expect(logger.warn).toBeCalledTimes(1);
+    expect(loggerMock.log).toBeCalledTimes(1);
     expect(systemUnderTest["adapters"].length).toBe(1);
   });
 
