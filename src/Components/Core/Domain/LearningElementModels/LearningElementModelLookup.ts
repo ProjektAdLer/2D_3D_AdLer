9e9688b62daa8096c14cd60bab03d3bebdf34b2a
LuckyLuke28 <37455486+LuckyLuke28@users.noreply.github.com>
2023-09-04 08:50:19 +0200

    changed loaded 3D models to campusTheme

--- src/Components/Core/Domain/LearningElementModels/LearningElementModelLookup.ts
+++ src/Components/Core/Domain/LearningElementModels/LearningElementModelLookup.ts
@@ -10,27 +10,27 @@ const LearningElementModelLookup: {
   [key in LearningElementModel]?: any;
 } = {
   [LearningElementModelTypeEnums.H5pElementModelTypes
-    .Blackboard]: require("../../../../Assets/3dModels/defaultTheme/l_h5p_blackboard_1.glb"),
+    .Blackboard]: require("../../../../Assets/3dModels/campusTheme/l_h5p_blackboard_1.glb"),
   [LearningElementModelTypeEnums.H5pElementModelTypes
-    .DeskPC]: require("../../../../Assets/3dModels/defaultTheme/l_h5p_deskpc_1.glb"),
+    .DeskPC]: require("../../../../Assets/3dModels/campusTheme/l_h5p_deskpc_1.glb"),
   [LearningElementModelTypeEnums.H5pElementModelTypes
-    .DrawingTable]: require("../../../../Assets/3dModels/defaultTheme/l_h5p_drawingtable_1.glb"),
+    .DrawingTable]: require("../../../../Assets/3dModels/campusTheme/l_h5p_drawingtable_1.glb"),
   [LearningElementModelTypeEnums.H5pElementModelTypes
-    .SlotMachine]: require("../../../../Assets/3dModels/defaultTheme/l_h5p_slotmachine_1.glb"),
+    .SlotMachine]: require("../../../../Assets/3dModels/campusTheme/l_h5p_slotmachine_1.glb"),
   [LearningElementModelTypeEnums.TextElementModelTypes
-    .Bookshelf1]: require("../../../../Assets/3dModels/defaultTheme/l_text_bookshelf_1.glb"),
+    .Bookshelf1]: require("../../../../Assets/3dModels/campusTheme/l_text_bookshelf_1.glb"),
   [LearningElementModelTypeEnums.TextElementModelTypes
-    .Bookshelf2]: require("../../../../Assets/3dModels/defaultTheme/l_text_bookshelf_2.glb"),
+    .Bookshelf2]: require("../../../../Assets/3dModels/campusTheme/l_text_bookshelf_2.glb"),
   [LearningElementModelTypeEnums.ImageElementModelTypes
-    .Painting1]: require("../../../../Assets/3dModels/defaultTheme/l_picture_painting_1.glb"),
+    .Painting1]: require("../../../../Assets/3dModels/campusTheme/l_picture_painting_1.glb"),
   [LearningElementModelTypeEnums.ImageElementModelTypes
-    .Painting2]: require("../../../../Assets/3dModels/defaultTheme/l_picture_painting_2.glb"),
+    .Painting2]: require("../../../../Assets/3dModels/campusTheme/l_picture_painting_2.glb"),
   [LearningElementModelTypeEnums.ImageElementModelTypes
-    .PaintingEasel]: require("../../../../Assets/3dModels/defaultTheme/l_picture_paintingeasel_1.glb"),
+    .PaintingEasel]: require("../../../../Assets/3dModels/campusTheme/l_picture_paintingeasel_1.glb"),
   [LearningElementModelTypeEnums.VideoElementModelTypes
-    .Television]: require("../../../../Assets/3dModels/defaultTheme/l_video_television_1.glb"),
+    .Television]: require("../../../../Assets/3dModels/campusTheme/l_video_television_1.glb"),
   [LearningElementModelTypeEnums.QuizElementModelTypes
-    .QuizNPC]: require("../../../../Assets/3dModels/defaultTheme/LukasNPC.glb"),
+    .QuizNPC]: require("../../../../Assets/3dModels/campusTheme/LukasNPC.glb"),
 };
 
 export default LearningElementModelLookup;
