dc636880c5efb97f8ee59293e844d3887565506b
Marius Kaczmarek <kaczmarek.m@gmx.de>
2023-09-22 13:57:20 +0200

    added quiz icon for AdaptivityElement

--- src/Components/Core/Presentation/Utils/GetLearningElementIcon.tsx
+++ src/Components/Core/Presentation/Utils/GetLearningElementIcon.tsx
@@ -3,7 +3,11 @@ import h5pIcon from "../../../../Assets/icons/05-h5p/h5p-icon-nobg.svg";
 import videoIcon from "../../../../Assets/icons/07-video/video-icon-nobg.svg";
 import imageIcon from "../../../../Assets/icons/04-image/image-icon.svg";
 import taskIcon from "../../../../Assets/icons/03-task/task-icon-middle-nocheck-nobg.svg";
-import { LearningElementTypeStrings } from "../../Domain/Types/LearningElementTypes";
+import quizIcon from "../../../../Assets/icons/36-adaptivity-quiz/adaptivity-quiz-icon-nobg.svg";
+import {
+  LearningElementTypeStrings,
+  LearningElementTypes,
+} from "../../Domain/Types/LearningElementTypes";
 
 export const getLearningElementIcon = (type: LearningElementTypeStrings) => {
   switch (type) {
@@ -32,6 +36,10 @@ export const getLearningElementIcon = (type: LearningElementTypeStrings) => {
     case "h5p":
       return (
         <img src={h5pIcon} alt="h5p-icon" className="h-8 mr-2 lg:h-12"></img>
+      );
+    case LearningElementTypes.quiz:
+      return (
+        <img src={quizIcon} alt="quiz-icon" className="h-8 mr-2 lg:h-12"></img>
       );
     default:
       return (
