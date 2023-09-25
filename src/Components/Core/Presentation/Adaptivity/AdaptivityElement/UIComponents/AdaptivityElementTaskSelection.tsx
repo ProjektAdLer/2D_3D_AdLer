d7d6c2e83e5bd94d3be7ca3b1cec792b0b67b663
Marius Kaczmarek <kaczmarek.m@gmx.de>
2023-09-25 17:57:37 +0200

    added completion icon to AETaskSelection

--- src/Components/Core/Presentation/Adaptivity/AdaptivityElement/UIComponents/AdaptivityElementTaskSelection.tsx
+++ src/Components/Core/Presentation/Adaptivity/AdaptivityElement/UIComponents/AdaptivityElementTaskSelection.tsx
@@ -4,11 +4,15 @@ import StyledButton from "~ReactComponents/ReactRelated/ReactBaseComponents/Styl
 
 //TODO: change this when key icon is available
 import requiredTaskIcon from "../../../../../../Assets/icons/01-badges/badge-gold-darkblue-icon-nobg.svg";
+import correctIcon from "../../../../../../Assets/icons/17-1-solution-check/check-solution-icon-nobg.svg";
+import wrongIcon from "../../../../../../Assets/icons/35-wrong/wrong-icon-nobg.svg";
 
 export default function AdaptivityElementTaskSelection({
   tasks,
+  onSelectTask,
 }: {
   tasks: AdaptivityTask[];
+  onSelectTask: (taskID: number) => void;
 }) {
   const [taskButtons, setTaskButtons] = useState<JSX.Element[]>([]);
 
@@ -20,18 +24,33 @@ export default function AdaptivityElementTaskSelection({
         );
 
         return (
-          <li key={i.toString() + ":" + task.taskID}>
+          <li key={i.toString() + "_" + task.taskID}>
             <StyledButton
               shape="freefloatleft"
-              icon={isRequired ? requiredTaskIcon : undefined}
+              onClick={() => onSelectTask(task.taskID)}
             >
+              {task.isCompleted && (
+                <img alt="" className={"h-6 lg:h-8 pr-2"} src={correctIcon} />
+              )}
+              {!task.isCompleted && (
+                <img alt="" className={"h-6 lg:h-8 pr-2"} src={wrongIcon} />
+              )}
+
               {task.taskTitle}
+
+              {isRequired && (
+                <img
+                  alt=""
+                  className={"h-6 lg:h-8 pr-2"}
+                  src={requiredTaskIcon}
+                />
+              )}
             </StyledButton>
           </li>
         );
       })
     );
-  }, [tasks]);
+  }, [tasks, onSelectTask]);
 
   return <ul>{taskButtons}</ul>;
 }
