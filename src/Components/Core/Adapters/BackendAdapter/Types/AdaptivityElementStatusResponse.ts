14e9c18ccc608fe375cc569b49e4fad4c20efdd0
Mountler <mikey-nihon@web.de>
2023-10-16 17:11:11 +0200

    moved backend responses in own files

---
+++ src/Components/Core/Adapters/BackendAdapter/Types/AdaptivityElementStatusResponse.ts
@@ -0,0 +1,20 @@
+import { ComponentID } from "src/Components/Core/Domain/Types/EntityTypes";
+
+export default class AdaptivtyElementStatusResponse {
+  element: {
+    elementID: ComponentID;
+    success: boolean;
+  };
+  questions: {
+    id: ComponentID;
+    status: string;
+    // answers?: {
+    //   checked: false;
+    //   correct: boolean;
+    // }[];
+  }[];
+  tasks: {
+    taskId: ComponentID;
+    taskStatus: string;
+  }[];
+}
