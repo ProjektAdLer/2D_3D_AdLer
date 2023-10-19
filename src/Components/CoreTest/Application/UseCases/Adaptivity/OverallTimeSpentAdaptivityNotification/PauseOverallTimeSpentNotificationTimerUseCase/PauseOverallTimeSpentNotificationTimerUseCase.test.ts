97806f2493f2a41e9003eb8271ca71ff42e0390f
Felix Kleis <felixkleis@yahoo.de>
2023-10-19 17:23:03 +0200

    Fixed PauseTimeSpentNotification UseCase Tests

--- src/Components/CoreTest/Application/UseCases/Adaptivity/OverallTimeSpentAdaptivityNotification/PauseOverallTimeSpentNotificationTimerUseCase/PauseOverallTimeSpentNotificationTimerUseCase.test.ts
+++ src/Components/CoreTest/Application/UseCases/Adaptivity/OverallTimeSpentAdaptivityNotification/PauseOverallTimeSpentNotificationTimerUseCase/PauseOverallTimeSpentNotificationTimerUseCase.test.ts
@@ -1,21 +1,17 @@
 import { OverallTimeSpentAdaptivityNotificationBreakType } from "../../../../../../Core/Domain/Entities/Adaptivity/OverallTimeSpentAdaptivityNotificationEntity";
 import OverallTimeSpentAdaptivityNotificationEntity from "../../../../../../Core/Domain/Entities/Adaptivity/OverallTimeSpentAdaptivityNotificationEntity";
-import INotificationPort from "../../../../../../Core/Application/Ports/NotificationPort/INotificationAdapter";
-import StartOverallTimeSpentNotificationTimerUseCase from "../../../../../../Core/Application/UseCases/Adaptivity/OverallTimeSpentNotification/StartOverallTimeSpentNotificationTimerUseCase/StartOverallTimeSpentNotificationTimerUseCase";
 import CoreDIContainer from "../../../../../../Core/DependencyInjection/CoreDIContainer";
-import PORT_TYPES from "../../../../../../Core/DependencyInjection/Ports/PORT_TYPES";
 import USECASE_TYPES from "../../../../../../Core/DependencyInjection/UseCases/USECASE_TYPES";
 import mock from "jest-mock-extended/lib/Mock";
 import type IEntityContainer from "../../../../../../Core/Domain/EntityContainer/IEntityContainer";
 import CORE_TYPES from "../../../../../../Core/DependencyInjection/CoreTypes";
-import IPauseOverallTimeSpentNotificationTimerUseCase from "../../../../../../Core/Application/UseCases/Adaptivity/OverallTimeSpentNotification/PauseOverallTimeSpentNotificationTimerUseCase/IPauseOverallTimeSpentNotificationTimerUseCase";
 import PauseOverallTimeSpentNotificationTimerUseCase from "../../../../../../Core/Application/UseCases/Adaptivity/OverallTimeSpentNotification/PauseOverallTimeSpentNotificationTimerUseCase/PauseOverallTimeSpentNotificationTimerUseCase";
 import IStartOverallTimeSpentNotificationTimerUseCase from "../../../../../../Core/Application/UseCases/Adaptivity/OverallTimeSpentNotification/StartOverallTimeSpentNotificationTimerUseCase/IStartOverallTimeSpentNotificationTimerUseCase";
 
 const entitycontainermock = mock<IEntityContainer>();
 const startUseCaseMock = mock<IStartOverallTimeSpentNotificationTimerUseCase>();
 
-describe("StartOverallTimeSpentNotificationTimerUseCase", () => {
+describe("PauseOverallTimeSpentNotificationTimerUseCase", () => {
   let systemUnderTest: PauseOverallTimeSpentNotificationTimerUseCase;
   let entity: OverallTimeSpentAdaptivityNotificationEntity;
 
@@ -45,18 +41,15 @@ describe("StartOverallTimeSpentNotificationTimerUseCase", () => {
 
   test("internalExecute calls StartOverallTimeSpentAdaptivityTimerUseCase on short breaktype", () => {
     jest.useFakeTimers();
-    entity.notificationType =
-      OverallTimeSpentAdaptivityNotificationBreakType.Short;
+    entity.notificationIterator = 0;
 
     entitycontainermock.getEntitiesOfType.mockReturnValue([entity]);
 
     systemUnderTest.internalExecute(startUseCaseMock);
 
-    expect(entity.notificationType).toBe(
-      OverallTimeSpentAdaptivityNotificationBreakType.Medium
+    jest.advanceTimersByTime(
+      OverallTimeSpentAdaptivityNotificationBreakType.Short * 1000 * 60 + 1
     );
-
-    jest.advanceTimersByTime(entity.notificationType * 1000 * 60 + 1);
     expect(startUseCaseMock.execute).toHaveBeenCalledTimes(1);
 
     jest.useRealTimers();
@@ -64,50 +57,33 @@ describe("StartOverallTimeSpentNotificationTimerUseCase", () => {
 
   test("internalExecute calls StartOverallTimeSpentAdaptivityTimerUseCase on medium breaktype", () => {
     jest.useFakeTimers();
-    entity.notificationType =
-      OverallTimeSpentAdaptivityNotificationBreakType.Medium;
+    entity.notificationIterator = 4;
 
     entitycontainermock.getEntitiesOfType.mockReturnValue([entity]);
 
     systemUnderTest.internalExecute(startUseCaseMock);
 
-    expect(entity.notificationType).toBe(
-      OverallTimeSpentAdaptivityNotificationBreakType.Long
+    jest.advanceTimersByTime(
+      OverallTimeSpentAdaptivityNotificationBreakType.Medium * 1000 * 60 + 1
     );
-
-    jest.advanceTimersByTime(entity.notificationType * 1000 * 60 + 1);
     expect(startUseCaseMock.execute).toHaveBeenCalledTimes(1);
 
     jest.useRealTimers();
   });
 
   test("internalExecute calls not StartOverallTimeSpentAdaptivityTimerUseCase on long breaktype", () => {
-    entity.notificationType =
-      OverallTimeSpentAdaptivityNotificationBreakType.Long;
+    jest.useFakeTimers();
+    entity.notificationIterator = 8;
 
     entitycontainermock.getEntitiesOfType.mockReturnValue([entity]);
 
     systemUnderTest.internalExecute(startUseCaseMock);
 
-    expect(entity.notificationType).toBe(
-      OverallTimeSpentAdaptivityNotificationBreakType.None
+    jest.advanceTimersByTime(
+      OverallTimeSpentAdaptivityNotificationBreakType.Long * 1000 * 60 + 1
     );
+    expect(startUseCaseMock.execute).toHaveBeenCalledTimes(1);
 
-    expect(startUseCaseMock.execute).toHaveBeenCalledTimes(0);
-  });
-
-  test("internalExecute calls nothing on none breaktype", () => {
-    entity.notificationType =
-      OverallTimeSpentAdaptivityNotificationBreakType.None;
-
-    entitycontainermock.getEntitiesOfType.mockReturnValue([entity]);
-
-    systemUnderTest.internalExecute(startUseCaseMock);
-
-    expect(entity.notificationType).toBe(
-      OverallTimeSpentAdaptivityNotificationBreakType.None
-    );
-
-    expect(startUseCaseMock.execute).toHaveBeenCalledTimes(0);
+    jest.useRealTimers();
   });
 });
