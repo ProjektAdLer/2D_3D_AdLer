a0f43955c0f7be67ab8a08b38c244dce7e8993ef
Felix Kleis <felixkleis@yahoo.de>
2023-12-11 05:31:21 +0100

    Added tests for learningworldselectionpresenter

--- src/Components/CoreTest/Presentation/React/LearningWorldMenu/LearningWorldSelection/LearningWorldSelectionPresenter.test.ts
+++ src/Components/CoreTest/Presentation/React/LearningWorldMenu/LearningWorldSelection/LearningWorldSelectionPresenter.test.ts
@@ -1,4 +1,5 @@
 import UserInitialLearningWorldsInfoTO from "../../../../../Core/Application/DataTransferObjects/UserInitialLearningWorldsInfoTO";
+import UserLearningWorldsInfoTO from "../../../../../Core/Application/DataTransferObjects/UserLearningWorldsInfoTO";
 import LearningWorldSelectionPresenter from "../../../../../Core/Presentation/React/LearningWorldMenu/LearningWorldSelection/LearningWorldSelectionPresenter";
 import LearningWorldSelectionViewModel from "../../../../../Core/Presentation/React/LearningWorldMenu/LearningWorldSelection/LearningWorldSelectionViewModel";
 
@@ -21,6 +22,19 @@ describe("LearningWorldSelectionPresenter", () => {
     expect(systemUnderTest["viewModel"].userWorlds.Value).toEqual([
       { id: 1, name: "Test World 1", isCompleted: false },
       { id: 2, name: "Test World 2", isCompleted: false },
+    ]);
+  });
+  test("onUserLearningWorldsInfoLoaded should set the correct values in the vm", () => {
+    const userWorlds: UserLearningWorldsInfoTO = {
+      worldInfo: [
+        { worldID: 1, worldName: "Test World 1", isCompleted: false },
+        { worldID: 2, worldName: "Test World 2", isCompleted: true },
+      ],
+    };
+    systemUnderTest.onUserLearningWorldsInfoLoaded(userWorlds);
+    expect(systemUnderTest["viewModel"].userWorlds.Value).toEqual([
+      { id: 1, name: "Test World 1", isCompleted: false },
+      { id: 2, name: "Test World 2", isCompleted: true },
     ]);
   });
 });
