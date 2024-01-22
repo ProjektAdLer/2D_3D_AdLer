1fc1d00adf2adc8d9da9cb25501fbaae4370a132
Mountler <mikey-nihon@web.de>
2024-01-22 14:58:09 +0100

    removed timeout from buttoncall

--- src/Components/Core/Presentation/React/GeneralComponents/LoadingScreen/LoadingScreenController.ts
+++ src/Components/Core/Presentation/React/GeneralComponents/LoadingScreen/LoadingScreenController.ts
@@ -3,7 +3,7 @@ import LoadingScreenViewModel from "./LoadingScreenViewModel";
 import CoreDIContainer from "~DependencyInjection/CoreDIContainer";
 import IBeginStoryElementCutSceneUseCase from "src/Components/Core/Application/UseCases/BeginStoryElementCutScene/IBeginStoryElementCutSceneUseCase";
 import USECASE_TYPES from "~DependencyInjection/UseCases/USECASE_TYPES";
-import EndStoryElementCutScene from "src/Components/Core/Application/UseCases/EndStoryElementCutScene/EndStoryElementCutScene";
+import { StoryElementType } from "src/Components/Core/Domain/Types/StoryElementType";
 
 export default class LoadingScreenController
   implements ILoadingScreenController
@@ -17,17 +17,9 @@ export default class LoadingScreenController
     if (this.viewModel.loadingLocation.Value.includes("spacedisplay")) {
       CoreDIContainer.get<IBeginStoryElementCutSceneUseCase>(
         USECASE_TYPES.IBeginStoryElementCutSceneUseCase
-      ).execute();
-      // fire event, that disables user input for controlling avatar
+      ).execute(StoryElementType.Intro);
 
       console.log("disable User-Inputs");
-
-      setTimeout(() => {
-        CoreDIContainer.get<EndStoryElementCutScene>(
-          USECASE_TYPES.IEndStoryElementCutScene
-        ).execute();
-        console.log("enable User-Inputs");
-      }, 10000);
     }
   }
 }
