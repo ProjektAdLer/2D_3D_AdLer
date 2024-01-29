486094ddb7dfaca0ee7ebb582ba0f5d1d1a2a737
Marius Kaczmarek <kaczmarek.m@gmx.de>
2024-01-29 13:01:04 +0100

    added tests for EndStoryCutSceneUseCase

--- src/Components/Core/Application/UseCases/EndStoryElementCutScene/EndStoryElementCutSceneUseCase.ts
+++ src/Components/Core/Application/UseCases/EndStoryElementCutScene/EndStoryElementCutSceneUseCase.ts
@@ -1,21 +1,18 @@
 import { inject, injectable } from "inversify";
 import IEndStoryElementCutSceneUseCase from "./IEndStoryElementCutSceneUseCase";
-import CORE_TYPES from "~DependencyInjection/CoreTypes";
-import type ILoggerPort from "../../Ports/Interfaces/ILoggerPort";
 import USECASE_TYPES from "~DependencyInjection/UseCases/USECASE_TYPES";
 import type IGetUserLocationUseCase from "../GetUserLocation/IGetUserLocationUseCase";
 import PORT_TYPES from "~DependencyInjection/Ports/PORT_TYPES";
 import type ILearningWorldPort from "../../Ports/Interfaces/ILearningWorldPort";
 import type IEntityContainer from "src/Components/Core/Domain/EntityContainer/IEntityContainer";
 import StoryElementEntity from "src/Components/Core/Domain/Entities/StoryElementEntity";
+import CORE_TYPES from "~DependencyInjection/CoreTypes";
 
 @injectable()
 export default class EndStoryElementCutSceneUseCase
   implements IEndStoryElementCutSceneUseCase
 {
   constructor(
-    @inject(CORE_TYPES.ILogger)
-    private logger: ILoggerPort,
     @inject(CORE_TYPES.IEntityContainer)
     private entityContainer: IEntityContainer,
     @inject(USECASE_TYPES.IGetUserLocationUseCase)
---
+++ src/Components/CoreTest/Application/UseCases/EndStoryElementCutScene/EndStoryElementCutSceneUseCase.test.ts
@@ -0,0 +1,65 @@
+import { mock } from "jest-mock-extended";
+import EndStoryElementCutSceneUseCase from "../../../../Core/Application/UseCases/EndStoryElementCutScene/EndStoryElementCutSceneUseCase";
+import IEntityContainer from "../../../../Core/Domain/EntityContainer/IEntityContainer";
+import IGetUserLocationUseCase from "../../../../Core/Application/UseCases/GetUserLocation/IGetUserLocationUseCase";
+import ILearningWorldPort from "../../../../Core/Application/Ports/Interfaces/ILearningWorldPort";
+import CoreDIContainer from "../../../../Core/DependencyInjection/CoreDIContainer";
+import CORE_TYPES from "../../../../Core/DependencyInjection/CoreTypes";
+import USECASE_TYPES from "../../../../Core/DependencyInjection/UseCases/USECASE_TYPES";
+import PORT_TYPES from "../../../../Core/DependencyInjection/Ports/PORT_TYPES";
+
+const entityContainerMock = mock<IEntityContainer>();
+const getUserLocationUseCaseMock = mock<IGetUserLocationUseCase>();
+const learnignWorldPortMock = mock<ILearningWorldPort>();
+
+describe("EndStoryElementCutSceneUseCase", () => {
+  let systemUnderTest: EndStoryElementCutSceneUseCase;
+
+  beforeAll(() => {
+    CoreDIContainer.snapshot();
+
+    CoreDIContainer.rebind(CORE_TYPES.IEntityContainer).toConstantValue(
+      entityContainerMock
+    );
+    CoreDIContainer.rebind(
+      USECASE_TYPES.IGetUserLocationUseCase
+    ).toConstantValue(getUserLocationUseCaseMock);
+    CoreDIContainer.rebind(PORT_TYPES.ILearningWorldPort).toConstantValue(
+      learnignWorldPortMock
+    );
+  });
+
+  beforeEach(() => {
+    systemUnderTest = CoreDIContainer.resolve(EndStoryElementCutSceneUseCase);
+  });
+
+  afterAll(() => {
+    CoreDIContainer.restore();
+  });
+
+  test("execute() should call onStoryElementCutSceneFinished() on the world port when story element exists", () => {
+    getUserLocationUseCaseMock.execute.mockReturnValue({
+      worldID: 1,
+      spaceID: 1,
+    });
+    entityContainerMock.filterEntitiesOfType.mockReturnValue([{}]);
+
+    systemUnderTest.execute();
+
+    expect(learnignWorldPortMock.onStoryElementCutSceneFinished).toBeCalled();
+  });
+
+  test("execute() should not call onStoryElementCutSceneFinished() on the world port when story element does not exist", () => {
+    getUserLocationUseCaseMock.execute.mockReturnValue({
+      worldID: 1,
+      spaceID: 1,
+    });
+    entityContainerMock.filterEntitiesOfType.mockReturnValue([]);
+
+    systemUnderTest.execute();
+
+    expect(
+      learnignWorldPortMock.onStoryElementCutSceneFinished
+    ).not.toBeCalled();
+  });
+});
