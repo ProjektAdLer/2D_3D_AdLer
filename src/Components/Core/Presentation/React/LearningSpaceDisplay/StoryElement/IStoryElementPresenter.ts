86d2d0d8f9752bdaa3aac62d7ec175d3ad28ceff
Marius Kaczmarek <kaczmarek.m@gmx.de>
2024-01-30 16:12:19 +0100

    removed unused import

--- src/Components/Core/Presentation/React/LearningSpaceDisplay/StoryElement/IStoryElementPresenter.ts
+++ src/Components/Core/Presentation/React/LearningSpaceDisplay/StoryElement/IStoryElementPresenter.ts
@@ -1,4 +1,3 @@
-import StoryElementTO from "src/Components/Core/Application/DataTransferObjects/StoryElementTO";
 import ILearningWorldAdapter from "src/Components/Core/Application/Ports/LearningWorldPort/ILearningWorldAdapter";
 
 export default interface IStoryElementPresenter extends ILearningWorldAdapter {
