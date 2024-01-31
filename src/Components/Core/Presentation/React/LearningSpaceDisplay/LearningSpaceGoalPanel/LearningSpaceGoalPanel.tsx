680d1766a0ddade6e2d475abee5eb956b97c9684
AdLer Lukas <lukas.schaab@th-ab.de>
2024-01-31 15:46:42 +0100

    styled LearningSpaceGoalPanel

--- src/Components/Core/Presentation/React/LearningSpaceDisplay/LearningSpaceGoalPanel/LearningSpaceGoalPanel.tsx
+++ src/Components/Core/Presentation/React/LearningSpaceDisplay/LearningSpaceGoalPanel/LearningSpaceGoalPanel.tsx
@@ -5,6 +5,11 @@ import useBuilder from "~ReactComponents/ReactRelated/CustomHooks/useBuilder";
 import BUILDER_TYPES from "~DependencyInjection/Builders/BUILDER_TYPES";
 import TextWithLineBreaks from "~ReactComponents/ReactRelated/ReactBaseComponents/TextWithLineBreaks";
 import StyledModal from "~ReactComponents/ReactRelated/ReactBaseComponents/StyledModal";
+import StyledContainer from "~ReactComponents/ReactRelated/ReactBaseComponents/StyledContainer";
+import tailwindMerge from "../../../Utils/TailwindMerge";
+import StyledButton from "~ReactComponents/ReactRelated/ReactBaseComponents/StyledButton";
+
+import GoalLogo from "../../../../../../Assets/icons/20-goal/goal-icon.svg";
 
 export default function LearningSpaceGoalPanel() {
   const [viewModel] = useBuilder<
@@ -17,30 +22,48 @@ export default function LearningSpaceGoalPanel() {
 
   if (!isOpen) return null;
   return (
-    <StyledModal
-      title="Lernziele"
-      showModal={isOpen}
-      onClose={() => {
-        viewModel.isOpen.Value = false;
-      }}
-    >
+    <StyledContainer className="z-10 fixed top-12 md:top-20 lg:right-40 max-w-2xl p-2 rounded-lg bg-buttonbgblue overflow-hidden">
       <div className="flex flex-col gap-4 m-2">
         {!goals ||
           (goals.length === 0 && (
             <div>Zu diesem Lernraum gibt es keine eingetragenen Lernziele!</div>
           ))}
         {goals && (
-          <ul className="ml-4 list-disc">
-            {goals.map((goal, index) => {
-              return (
-                <li key={index}>
-                  <TextWithLineBreaks text={goal} />
-                </li>
-              );
-            })}
-          </ul>
+          <div className="flex flex-row ">
+            <div>
+              <div className="flex flex-row justify-center">
+                <img
+                  src={GoalLogo}
+                  alt="Lernziel Logo"
+                  className="h-4 lg:h-6 pr-2"
+                />
+                <h3 className="text-sm lg:text-xl font-semibold pb-1">
+                  Lernziele
+                </h3>
+              </div>
+
+              <ul className="ml-4 list-disc text-xs lg:text-sm">
+                {goals.map((goal, index) => {
+                  return (
+                    <li key={index}>
+                      <TextWithLineBreaks text={goal} />
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            <div className="flex flex-col pl-4 justify-end">
+              <StyledButton
+                onClick={() => {
+                  viewModel.isOpen.Value = false;
+                }}
+              >
+                OK
+              </StyledButton>
+            </div>
+          </div>
         )}
       </div>
-    </StyledModal>
+    </StyledContainer>
   );
 }
