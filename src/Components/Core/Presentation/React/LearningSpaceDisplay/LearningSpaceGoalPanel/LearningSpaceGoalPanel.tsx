477b857327969a07ec6c602fca65579bb46d8fbc
Felix Kleis <felixkleis@yahoo.de>
2024-01-31 18:11:18 +0100

    Removed unused imports

--- src/Components/Core/Presentation/React/LearningSpaceDisplay/LearningSpaceGoalPanel/LearningSpaceGoalPanel.tsx
+++ src/Components/Core/Presentation/React/LearningSpaceDisplay/LearningSpaceGoalPanel/LearningSpaceGoalPanel.tsx
@@ -4,9 +4,7 @@ import useObservable from "../../ReactRelated/CustomHooks/useObservable";
 import useBuilder from "~ReactComponents/ReactRelated/CustomHooks/useBuilder";
 import BUILDER_TYPES from "~DependencyInjection/Builders/BUILDER_TYPES";
 import TextWithLineBreaks from "~ReactComponents/ReactRelated/ReactBaseComponents/TextWithLineBreaks";
-import StyledModal from "~ReactComponents/ReactRelated/ReactBaseComponents/StyledModal";
 import StyledContainer from "~ReactComponents/ReactRelated/ReactBaseComponents/StyledContainer";
-import tailwindMerge from "../../../Utils/TailwindMerge";
 import StyledButton from "~ReactComponents/ReactRelated/ReactBaseComponents/StyledButton";
 
 import GoalLogo from "../../../../../../Assets/icons/20-goal/goal-icon.svg";
@@ -22,7 +20,7 @@ export default function LearningSpaceGoalPanel() {
 
   if (!isOpen) return null;
   return (
-    <StyledContainer className="z-10 fixed top-12 md:top-20 lg:right-40 max-w-2xl p-2 rounded-lg bg-buttonbgblue overflow-hidden">
+    <StyledContainer className="fixed z-10 max-w-2xl p-2 overflow-hidden rounded-lg top-12 md:top-20 lg:right-40 bg-buttonbgblue">
       <div className="flex flex-col gap-4 m-2">
         {!goals ||
           (goals.length === 0 && (
@@ -35,14 +33,14 @@ export default function LearningSpaceGoalPanel() {
                 <img
                   src={GoalLogo}
                   alt="Lernziel Logo"
-                  className="h-4 lg:h-6 pr-2"
+                  className="h-4 pr-2 lg:h-6"
                 />
-                <h3 className="text-sm lg:text-xl font-semibold pb-1">
+                <h3 className="pb-1 text-sm font-semibold lg:text-xl">
                   Lernziele
                 </h3>
               </div>
 
-              <ul className="ml-4 list-disc text-xs lg:text-sm">
+              <ul className="ml-4 text-xs list-disc lg:text-sm">
                 {goals.map((goal, index) => {
                   return (
                     <li key={index}>
@@ -52,7 +50,7 @@ export default function LearningSpaceGoalPanel() {
                 })}
               </ul>
             </div>
-            <div className="flex flex-col pl-4 justify-end">
+            <div className="flex flex-col justify-end pl-4">
               <StyledButton
                 shape="closebutton"
                 onClick={() => {
