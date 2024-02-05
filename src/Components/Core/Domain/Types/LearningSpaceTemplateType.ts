b52f19d4f70308b782889ba755f49af3212cee0b
Marius Kaczmarek <kaczmarek.m@gmx.de>
2024-02-05 16:11:04 +0100

    fixed lowercase x in template name

--- src/Components/Core/Domain/Types/LearningSpaceTemplateType.ts
+++ src/Components/Core/Domain/Types/LearningSpaceTemplateType.ts
@@ -1,9 +1,10 @@
+// IMPORTANT: type string names have to be all-uppercase
 export enum LearningSpaceTemplateType {
   L = "L_32X31_10L",
   T = "T_38X31_13L",
   R6 = "R_20X20_6L",
   R8 = "R_20X30_8L",
-  R15 = "R_40x37_15L",
+  R15 = "R_40X37_15L",
   None = "",
 }
 
