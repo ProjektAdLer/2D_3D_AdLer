b5c4e2572578fcfeff172341d4c6e36ebab866c9
AdLer-Lukas <95357660+AdLer-Lukas@users.noreply.github.com>
2024-02-15 14:20:47 +0100

    changed icons for "next" and "back" navigation

--- src/Components/Core/Presentation/React/LearningSpaceDisplay/LearningSpaceGoalPanel/LearningSpaceGoalPanel.tsx
+++ src/Components/Core/Presentation/React/LearningSpaceDisplay/LearningSpaceGoalPanel/LearningSpaceGoalPanel.tsx
@@ -31,7 +31,7 @@ export default function LearningSpaceGoalPanel() {
       ></img>
 
       {isOpen && (
-        <StyledContainer className="fixed z-10 max-w-2xl p-2 overflow-hidden rounded-lg top-12 right-2 md:top-20 lg:right-40 bg-buttonbgblue">
+        <StyledContainer className="fixed z-10 max-w-2xl p-2 overflow-hidden rounded-lg top-12 right-0 md:top-20 lg:right-40 bg-buttonbgblue">
           <div className="flex flex-col gap-4 m-2">
             {goals && (
               <div className="flex flex-row">
