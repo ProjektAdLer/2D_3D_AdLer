0c11cd0255e8dca8b99ec60ce7095ff55716d322
Marius Kaczmarek <kaczmarek.m@gmx.de>
2024-02-23 12:53:45 +0100

    added tests for LearningSpaceThemeLookup

--- src/Components/CoreTest/Domain/LearningSpaceThemeLookup/LearningSpaceThemeLookup.test.ts
+++ src/Components/CoreTest/Domain/LearningSpaceThemeLookup/LearningSpaceThemeLookup.test.ts
@@ -1,6 +1,9 @@
 import LearningSpaceThemeLookup from "../../../Core/Domain/LearningSpaceThemes/LearningSpaceThemeLookup";
 import LearningSpaceTheme_Arcade from "../../../Core/Domain/LearningSpaceThemes/LearningSpaceTheme_Arcade";
-import LearningSpaceTheme_Campus from "../../../Core/Domain/LearningSpaceThemes/LearningSpaceTheme_Campus";
+import {
+  LearningSpaceTheme_CampusAB,
+  LearningSpaceTheme_CampusKE,
+} from "../../../Core/Domain/LearningSpaceThemes/LearningSpaceTheme_Campus";
 import LearningSpaceTheme_Suburb from "../../../Core/Domain/LearningSpaceThemes/LearningSpaceTheme_Suburb";
 import { LearningSpaceThemeType } from "../../../Core/Domain/Types/LearningSpaceThemeTypes";
 
@@ -9,7 +12,21 @@ describe("LearningSpaceThemeLookup", () => {
     const result = LearningSpaceThemeLookup.getLearningSpaceTheme(
       LearningSpaceThemeType.Campus
     );
-    expect(result).toMatchObject(LearningSpaceTheme_Campus);
+    expect(result).toMatchObject(LearningSpaceTheme_CampusAB);
+  });
+
+  test("getLearningSpaceTheme return correct theme for CampusAB", () => {
+    const result = LearningSpaceThemeLookup.getLearningSpaceTheme(
+      LearningSpaceThemeType.CampusAB
+    );
+    expect(result).toMatchObject(LearningSpaceTheme_CampusAB);
+  });
+
+  test("getLearningSpaceTheme return correct theme for CampusKE", () => {
+    const result = LearningSpaceThemeLookup.getLearningSpaceTheme(
+      LearningSpaceThemeType.CampusKE
+    );
+    expect(result).toMatchObject(LearningSpaceTheme_CampusKE);
   });
 
   test("getLearningSpaceTheme return correct theme for Arcade", () => {
