4c7403214b79681c80e6215bf7eb5695cd1fafe1
Mountler <mikey-nihon@web.de>
2024-06-04 13:35:25 +0200

    added comments to adaptivity action types for ease of understanding

--- src/Components/Core/Domain/Types/Adaptivity/AdaptivityElementActionTypes.ts
+++ src/Components/Core/Domain/Types/Adaptivity/AdaptivityElementActionTypes.ts
@@ -1,6 +1,6 @@
 // TODO: add string representations
 export enum AdaptivityElementActionTypes {
-  CommentAction = "CommentAction",
-  ReferenceAction = "AdaptivityReferenceAction",
-  ContentAction = "AdaptivityContentReferenceAction",
+  CommentAction = "CommentAction", // display text
+  ReferenceAction = "AdaptivityReferenceAction", // display learning element
+  ContentAction = "AdaptivityContentReferenceAction", // display external learning element
 }
