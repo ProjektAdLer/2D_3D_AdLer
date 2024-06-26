712601d6619ca48d1cbadd1719fecf5e92e2d41e
Mountler <mikey-nihon@web.de>
2024-06-26 12:00:30 +0200

    expanded tests for FileExporter

--- src/Components/CoreTest/Adapters/Logger/FileExporter.test.ts
+++ src/Components/CoreTest/Adapters/Logger/FileExporter.test.ts
@@ -1,14 +1,55 @@
 import FileExporter from "../../../../Components/Core/Adapters/Logger/FileExporter";
+
 describe("FileExporter", () => {
+  URL.createObjectURL = (obj: File) => {
+    return "test-url";
+  };
   let systemUnderTest: FileExporter;
 
   beforeEach(() => {
     systemUnderTest = new FileExporter();
   });
 
-  test.skip("FileExporter creates 'a' element, which gets clicked", () => {
-    let logString = "test log";
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  test("FileExporter creates 'a' element, which gets clicked", () => {
+    jest.spyOn(document, "createElement");
+    jest.spyOn(HTMLAnchorElement.prototype, "click");
+
+    let logString = "test log 1\ntest log 2\ntest log 3";
     systemUnderTest.exportLog(logString);
-    expect(document.getElementsByTagName("a").length).toBe(1);
+    expect(document.createElement).toHaveBeenCalledWith("a");
+    expect(HTMLAnchorElement.prototype.click).toHaveBeenCalled();
+  });
+
+  test("generateFile creates a file", () => {
+    jest.spyOn(URL, "createObjectURL");
+
+    let logString = "test log 1\ntest log 2\ntest log 3";
+    systemUnderTest.generateFile(logString);
+    const expectedResult = new File([logString], "AdlerLog.txt");
+    expect(URL.createObjectURL).toHaveBeenCalledWith(expectedResult);
+  });
+
+  test("generateFile sets correct url attribute", () => {
+    jest.spyOn(HTMLAnchorElement.prototype, "setAttribute");
+    let logString = "test log 1\ntest log 2\ntest log 3";
+    systemUnderTest.generateFile(logString);
+    expect(HTMLAnchorElement.prototype.setAttribute).toHaveBeenCalledWith(
+      "href",
+      "test-url"
+    );
+  });
+
+  test("generateFile sets download attribute", () => {
+    jest.spyOn(HTMLAnchorElement.prototype, "setAttribute");
+    let logString = "test log 1\ntest log 2\ntest log 3";
+    systemUnderTest.generateFile(logString);
+    expect(HTMLAnchorElement.prototype.setAttribute).toHaveBeenCalledWith(
+      "download",
+      "AdLerLog.txt"
+    );
   });
 });
