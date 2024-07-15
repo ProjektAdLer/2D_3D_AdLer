d9359f4a38b4405fb298f95a3f721ff6f0963b9f
Marius Kaczmarek <kaczmarek.m@gmx.de>
2024-07-15 15:35:14 +0200

    again

--- src/Components/Core/Presentation/Babylon/HighlightColors.ts
+++ src/Components/Core/Presentation/Babylon/HighlightColors.ts
@@ -2,7 +2,7 @@ import { Color3 } from "@babylonjs/core";
 
 const HighlightColors = {
   LearningElementUnsolved: Color3.FromHexString("#FF0000"),
-  LearningElementSolved: new Color3(0, 1, 0),
+  LearningElementSolved: Color3.FromInts(0, 255, 0),
   NonLearningElementBase: new Color3(0, 0, 1),
   LearningElementHighlighted: new Color3(1, 0, 1),
   getNonInteractableColor: (baseColor: Color3) => {
