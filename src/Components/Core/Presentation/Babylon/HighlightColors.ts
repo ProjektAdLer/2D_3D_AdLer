9c7306d62e14ef54d7142ca88b4d8b73307da0f4
Marius Kaczmarek <kaczmarek.m@gmx.de>
2024-07-15 15:31:41 +0200

    replaced highlight color shorthands with explicit values

--- src/Components/Core/Presentation/Babylon/HighlightColors.ts
+++ src/Components/Core/Presentation/Babylon/HighlightColors.ts
@@ -1,10 +1,10 @@
 import { Color3 } from "@babylonjs/core";
 
 const HighlightColors = {
-  LearningElementUnsolved: Color3.Red(),
-  LearningElementSolved: Color3.Green(),
-  NonLearningElementBase: Color3.Blue(),
-  LearningElementHighlighted: Color3.Purple(),
+  LearningElementUnsolved: new Color3(1, 0, 0),
+  LearningElementSolved: new Color3(0, 1, 0),
+  NonLearningElementBase: new Color3(0, 0, 1),
+  LearningElementHighlighted: new Color3(1, 0, 1),
   getNonInteractableColor: (baseColor: Color3) => {
     return baseColor.add(new Color3(0.45, 0.45, 0.45));
   },
