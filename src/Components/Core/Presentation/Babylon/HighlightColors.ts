57577fedc5e4056c57012b3b189a475c37db3117
splican <daniel.glaab@th-ab.de>
2024-07-25 15:35:27 +0200

    added another comment

--- src/Components/Core/Presentation/Babylon/HighlightColors.ts
+++ src/Components/Core/Presentation/Babylon/HighlightColors.ts
@@ -1,6 +1,7 @@
 import { Color3 } from "@babylonjs/core";
 
 // this will hopefully be the first line of a working commit.
+// This worked. Let's see if the second line works too.
 // possible ways to write color:
 // Color3.FromHexString("#FFFF00") -> HEX-Code standard way to use
 // Color3.FromInts(0, 255, 0) -> RGB-Code 0-256
