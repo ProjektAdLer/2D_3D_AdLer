0ce78f3c3f8eaf823ae7f41e4b323789cd7e82d1
Mountler <mikey-nihon@web.de>
2024-07-30 11:21:12 +0200

    added test to learningspacegoalpanelcontroller

--- src/Components/CoreTest/Presentation/React/LearningSpaceDisplay/LearningSpaceGoalPanel/LearningSpaceGoalPanelController.test.ts
+++ src/Components/CoreTest/Presentation/React/LearningSpaceDisplay/LearningSpaceGoalPanel/LearningSpaceGoalPanelController.test.ts
@@ -4,16 +4,28 @@ import LearningSpaceGoalPanelViewModel from "../../../../../Core/Presentation/Re
 const viewModelMock = new LearningSpaceGoalPanelViewModel();
 describe("LearningSpaceGoalPanelController", () => {
   let systemUnderTest: LearningSpaceGoalPanelController;
+
   test("closePanel sets isOpen in viewmodel to false", () => {
     viewModelMock.isOpen.Value = true;
     systemUnderTest = new LearningSpaceGoalPanelController(viewModelMock);
     systemUnderTest.closePanel();
     expect(viewModelMock.isOpen.Value).toBeFalsy();
   });
+
   test("openPanel sets isOpen in viewmodel to true", () => {
     viewModelMock.isOpen.Value = false;
     systemUnderTest = new LearningSpaceGoalPanelController(viewModelMock);
     systemUnderTest.openPanel();
+    expect(viewModelMock.isOpen.Value).toBeTruthy();
+  });
+
+  test("openOrCloseGoals inverts value of isOpen in viewmodel", () => {
+    viewModelMock.isOpen.Value = true;
+    systemUnderTest = new LearningSpaceGoalPanelController(viewModelMock);
+    systemUnderTest.openOrCloseGoals();
+    expect(viewModelMock.isOpen.Value).toBeFalsy();
+
+    systemUnderTest.openOrCloseGoals();
     expect(viewModelMock.isOpen.Value).toBeTruthy();
   });
 });
