e4effb96aea2379d6050ee9104c3667295f66252
Marius Kaczmarek <kaczmarek.m@gmx.de>
2024-09-09 15:37:19 +0200

    cleaned up ControlsExplanationModal

--- src/Components/Core/Presentation/React/GeneralComponents/ControlsExplanationModal/ControlsExplanationModal.tsx
+++ src/Components/Core/Presentation/React/GeneralComponents/ControlsExplanationModal/ControlsExplanationModal.tsx
@@ -6,64 +6,36 @@ import { useTranslation } from "react-i18next";
 import { AdLerUIComponent } from "src/Components/Core/Types/ReactTypes";
 import tailwindMerge from "../../../Utils/TailwindMerge";
 import useObservable from "~ReactComponents/ReactRelated/CustomHooks/useObservable";
-import React, { useState } from "react";
-import StyledButton from "~ReactComponents/ReactRelated/ReactBaseComponents/StyledButton";
 import ControlsExplanationContent from "./ControlsExplanationContent";
 
 export default function ControlsExplanationModal({
   className,
 }: AdLerUIComponent<{}>) {
   const [viewModel] = useBuilder<ControlsExplanationModalViewModel, undefined>(
-    BUILDER_TYPES.IControlsExplanationModalBuilder
+    BUILDER_TYPES.IControlsExplanationModalBuilder,
   );
   const { t: translate } = useTranslation("controls");
 
   const [isOpen, setIsOpen] = useObservable(viewModel?.isOpen);
-  const [showConfirmation, setShowConfirmation] = useState(false);
-  const [confirmationShownOnce, setConfirmationShownOnce] = useState(true);
 
   if (!viewModel) return null;
-  if (!isOpen && !showConfirmation) return null;
+  if (!isOpen) return null;
 
   return (
-    <React.Fragment>
-      {/* Confirmation Notice */}
-      <StyledModal
-        title={translate("title")!}
-        showModal={showConfirmation && !confirmationShownOnce}
-        canClose={false}
-      >
-        <div className="w-[90vw] max-w-6xl flex flex-row portrait:flex-col justify-between gap-12">
-          <p>{translate("confirmation")}</p>
-          <div className="flex flex-row gap-4">
-            <StyledButton
-              onClick={() => {
-                setConfirmationShownOnce(true);
-              }}
-            >
-              OK!
-            </StyledButton>
-          </div>
-        </div>
-      </StyledModal>
-
-      {/* Main Modal */}
-      <StyledModal
-        className={tailwindMerge(className, "")}
-        title={translate("title")!}
-        showModal={isOpen}
-        onClose={() => {
-          setIsOpen(false);
-          setShowConfirmation(true);
-        }}
-      >
-        <ControlsExplanationContent
-          className={tailwindMerge(
-            className,
-            "max-w-[90vw] max-h-[80vh] max-w-6xl"
-          )}
-        />
-      </StyledModal>
-    </React.Fragment>
+    <StyledModal
+      className={tailwindMerge(className, "")}
+      title={translate("title")!}
+      showModal={isOpen}
+      onClose={() => {
+        setIsOpen(false);
+      }}
+    >
+      <ControlsExplanationContent
+        className={tailwindMerge(
+          className,
+          "max-w-[90vw] max-h-[80vh] max-w-6xl",
+        )}
+      />
+    </StyledModal>
   );
 }
