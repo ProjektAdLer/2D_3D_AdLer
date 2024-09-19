b4d7c91e4db20117ca63bb2e3e4a21cb5bc23d00
Marius Kaczmarek <kaczmarek.m@gmx.de>
2024-09-19 16:22:22 +0200

    added cleanup logic to avatar focus selection

--- src/Components/Core/Presentation/Babylon/Avatar/AvatarFocusSelection/AvatarFocusSelection.ts
+++ src/Components/Core/Presentation/Babylon/Avatar/AvatarFocusSelection/AvatarFocusSelection.ts
@@ -16,33 +16,37 @@ import {
   Vector3,
 } from "@babylonjs/core";
 import { config } from "src/config";
+import IScenePresenter from "../../SceneManagement/IScenePresenter";
+import PRESENTATION_TYPES from "~DependencyInjection/Presentation/PRESENTATION_TYPES";
 
 @injectable()
 export default class AvatarFocusSelection implements IAvatarFocusSelection {
   private readonly squaredInteractionDistance: number = 1.6 ** 2; // squared distance for performance, change first value to change distance
   private readonly squaredAvatarMovementThreshold: number = 0.05 ** 2;
 
+  private scenePresenter: IScenePresenter;
+
   private focusables: IAvatarFokusable[] = [];
-  private avatarPresenter: IAvatarPresenter;
+  private avatarPresenter: IAvatarPresenter | null = null;
   private previousAvatarPosition: Vector3 | null = null;
 
   readonly CurrentFocus: Observable<IAvatarFokusable | null> =
     new Observable<IAvatarFokusable | null>(null);
 
   constructor() {
-    const scenePresenter = CoreDIContainer.get<ScenePresenterFactory>(
+    this.scenePresenter = CoreDIContainer.get<ScenePresenterFactory>(
       SCENE_TYPES.ScenePresenterFactory,
     )(LearningSpaceSceneDefinition);
 
-    scenePresenter.Scene.onBeforeRenderObservable.add(this.updateFocus);
-
-    scenePresenter.Scene.onBeforeRenderObservable.add(
+    this.scenePresenter.Scene.onBeforeRenderObservable.add(
       this.setupOnBeforeFirstFrame,
       undefined,
       false,
       undefined,
       true, // unregister after first call
     );
+    this.scenePresenter.Scene.onBeforeRenderObservable.add(this.updateFocus);
+    this.scenePresenter.Scene.onDisposeObservable.add(this.cleanup);
   }
 
   registerAvatarPresenter(avatarPresenter: IAvatarPresenter): void {
@@ -59,7 +63,7 @@ export default class AvatarFocusSelection implements IAvatarFocusSelection {
 
   @bind
   private setupOnBeforeFirstFrame(): void {
-    this.previousAvatarPosition = this.avatarPresenter.AvatarPosition;
+    this.previousAvatarPosition = this.avatarPresenter!.AvatarPosition;
 
     if (config.isDebug)
       this.focusables.forEach((focusable) => {
@@ -74,6 +78,7 @@ export default class AvatarFocusSelection implements IAvatarFocusSelection {
 
     // skip update if avatar has not moved enough
     const currentAvatarPosition = this.avatarPresenter.AvatarPosition;
+
     if (
       this.previousAvatarPosition &&
       currentAvatarPosition &&
@@ -113,19 +118,29 @@ export default class AvatarFocusSelection implements IAvatarFocusSelection {
     }
   }
 
+  @bind
+  private cleanup(): void {
+    this.scenePresenter.Scene.onBeforeRenderObservable.removeCallback(
+      this.updateFocus,
+    );
+
+    this.focusables = [];
+    this.avatarPresenter = null;
+
+    CoreDIContainer.rebind(PRESENTATION_TYPES.IAvatarFocusSelection)
+      .to(AvatarFocusSelection)
+      .inSingletonScope();
+  }
+
   private debug_drawInteractionRadius(position: Vector3) {
     if (!config.isDebug) return;
-
-    const scenePresenter = CoreDIContainer.get<ScenePresenterFactory>(
-      SCENE_TYPES.ScenePresenterFactory,
-    )(LearningSpaceSceneDefinition);
 
     const point = MeshBuilder.CreateSphere(
       "focusableCenter",
       {
         diameter: 0.1,
       },
-      scenePresenter.Scene,
+      this.scenePresenter.Scene,
     );
     const circle = MeshBuilder.CreateTorus(
       "focusableInteractionRadius",
@@ -134,12 +149,15 @@ export default class AvatarFocusSelection implements IAvatarFocusSelection {
         thickness: 0.01,
         tessellation: 64,
       },
-      scenePresenter.Scene,
+      this.scenePresenter.Scene,
     );
 
     point.position = position;
     circle.position = position;
-    const material = new StandardMaterial("material", scenePresenter.Scene);
+    const material = new StandardMaterial(
+      "material",
+      this.scenePresenter.Scene,
+    );
     material.diffuseColor = Color3.Teal();
     circle.material = material;
     point.material = material;
