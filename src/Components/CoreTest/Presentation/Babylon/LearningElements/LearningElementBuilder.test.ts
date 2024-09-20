6f5618c66e3a4edbf9f95185cb0a2c9ddb00cbd9
Marius Kaczmarek <kaczmarek.m@gmx.de>
2024-09-20 14:12:11 +0200

    updated LearningElementBuilder tests

--- src/Components/CoreTest/Presentation/Babylon/LearningElements/LearningElementBuilder.test.ts
+++ src/Components/CoreTest/Presentation/Babylon/LearningElements/LearningElementBuilder.test.ts
@@ -15,12 +15,14 @@ import {
   HistoryWrapper,
   LocationScope,
 } from "../../../../Core/Presentation/React/ReactRelated/ReactEntryPoint/HistoryWrapper";
+import IAvatarFocusSelection from "../../../../Core/Presentation/Babylon/Avatar/AvatarFocusSelection/IAvatarFokusSelection";
 
 jest.mock(
-  "../../../../Core/Presentation/Babylon/LearningElements/LearningElementView"
+  "../../../../Core/Presentation/Babylon/LearningElements/LearningElementView",
 );
 
 const worldPortMock = mock<ILearningWorldPort>();
+const avatarFocusSelectionMock = mock<IAvatarFocusSelection>();
 
 const mockElementData: LearningElementTO = {
   id: 0,
@@ -43,11 +45,14 @@ describe("LearningElementBuilder", () => {
   beforeAll(() => {
     CoreDIContainer.snapshot();
     CoreDIContainer.rebind(PORT_TYPES.ILearningWorldPort).toConstantValue(
-      worldPortMock
+      worldPortMock,
     );
     CoreDIContainer.bind(
-      PRESENTATION_TYPES.IBottomTooltipPresenter
+      PRESENTATION_TYPES.IBottomTooltipPresenter,
     ).toConstantValue(mock());
+    CoreDIContainer.rebind(
+      PRESENTATION_TYPES.IAvatarFocusSelection,
+    ).toConstantValue(avatarFocusSelectionMock);
   });
 
   beforeEach(() => {
@@ -66,7 +71,7 @@ describe("LearningElementBuilder", () => {
     systemUnderTest.elementPosition = mockElementPosition;
     expect(() => {
       systemUnderTest.buildViewModel();
-    }).toThrowError("elementData is undefined");
+    }).toThrow("elementData is undefined");
   });
 
   test("buildViewModel throws if elementPosition is not set", () => {
@@ -86,12 +91,12 @@ describe("LearningElementBuilder", () => {
     expect(systemUnderTest["viewModel"]!.id).toBe(mockElementData.id);
     expect(systemUnderTest["viewModel"]!.name).toBe(mockElementData.name);
     expect(systemUnderTest["viewModel"]!.description).toBe(
-      mockElementData.description
+      mockElementData.description,
     );
     expect(systemUnderTest["viewModel"]!.goals).toBe(mockElementData.goals);
     expect(systemUnderTest["viewModel"]!.type).toBe(mockElementData.type);
     expect(systemUnderTest["viewModel"]!.hasScored.Value).toBe(
-      mockElementData.hasScored
+      mockElementData.hasScored,
     );
     expect(systemUnderTest["viewModel"]!.modelType).toBe(mockElementData.model);
     expect(systemUnderTest["viewModel"]!.value).toBe(mockElementData.value);
@@ -110,14 +115,27 @@ describe("LearningElementBuilder", () => {
     expect(systemUnderTest["presenter"]).toBeDefined();
     expect(systemUnderTest.getPresenter()).toBeDefined();
     expect(systemUnderTest.getPresenter()).toBeInstanceOf(
-      LearningElementPresenter
+      LearningElementPresenter,
     );
 
     // call to the element port
     expect(worldPortMock.registerAdapter).toHaveBeenCalledTimes(1);
     expect(worldPortMock.registerAdapter).toHaveBeenCalledWith(
       systemUnderTest["presenter"],
-      LocationScope._sceneRendering
+      LocationScope._sceneRendering,
+    );
+  });
+
+  test("buildPresenter registers presenter with the avatarFocusSelection", () => {
+    systemUnderTest.elementData = mockElementData;
+    systemUnderTest.elementPosition = mockElementPosition;
+
+    systemUnderTest.buildViewModel();
+    systemUnderTest.buildPresenter();
+
+    expect(avatarFocusSelectionMock.registerFocusable).toHaveBeenCalledTimes(1);
+    expect(avatarFocusSelectionMock.registerFocusable).toHaveBeenCalledWith(
+      systemUnderTest["presenter"],
     );
   });
 
@@ -135,7 +153,7 @@ describe("LearningElementBuilder", () => {
 
     expect(systemUnderTest["view"]).toBeDefined();
     expect(systemUnderTest["view"]!.setupLearningElement).toHaveBeenCalledTimes(
-      1
+      1,
     );
     await expect(systemUnderTest.isCompleted).resolves.toBeUndefined();
   });
