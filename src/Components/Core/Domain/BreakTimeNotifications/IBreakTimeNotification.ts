3315a04d4374e9dc7145ea10f523927b0b64b99e
Marius Kaczmarek <kaczmarek.m@gmx.de>
2024-10-16 13:32:22 +0200

    refactored BreakTimeNotification contents structure

--- src/Components/Core/Domain/BreakTimeNotifications/IBreakTimeNotification.ts
+++ src/Components/Core/Domain/BreakTimeNotifications/IBreakTimeNotification.ts
@@ -1,9 +1,6 @@
 export default interface IBreakTimeNotification {
   titleKey: string; // key reference string inside localize language package
   titleMessageKeys: string[]; // keys reference strings inside localize language package
-  image1: string;
-  image2: string;
-  image3: string;
-  image4: string;
+  images: string[];
   seenBefore: boolean;
 }
