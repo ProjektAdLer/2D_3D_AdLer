5cad5750ef167571d67c496040fbb3167e93b3b7
Felix Kleis <felixkleis@yahoo.de>
2024-11-15 10:59:44 +0100

    Added sliders to 5th page

--- src/Components/Core/Presentation/AvatarEditor/AvatarEditorCategories/AvatarEditorCategoryContents/AvatarEditorBodyCategory.tsx
+++ src/Components/Core/Presentation/AvatarEditor/AvatarEditorCategories/AvatarEditorCategoryContents/AvatarEditorBodyCategory.tsx
@@ -2,6 +2,7 @@ import AvatarEditorCategoryContentProps from "./AvatarEditorCategoryContentProps
 import { useTranslation } from "react-i18next";
 import ColorPicker from "~ReactComponents/GeneralComponents/ColorPicker/ColorPicker";
 import { defaultColorPickerSchema } from "~ReactComponents/GeneralComponents/ColorPicker/ColorPickerColors";
+import RangeSlider from "~ReactComponents/ReactRelated/ReactBaseComponents/RangeSlider";
 
 export default function AvatarEditorBodyCategory(
   props: AvatarEditorCategoryContentProps,
@@ -24,13 +25,16 @@ export default function AvatarEditorBodyCategory(
       <div className="pb-2 border-b border-gray-500">
         <h1 className="text-2xl font-bold">{translate("upperBodyTitle")}</h1>
       </div>
-      <div>PLACEHOLDER</div>
+      <div>
+        <RangeSlider min={0} max={100} step={1} callback={console.log} />
+      </div>
 
       <div className="pb-2 border-b border-gray-500">
         <h1 className="text-2xl font-bold">{translate("lowerBodyTitle")}</h1>
       </div>
-
-      <div>PLACEHOLDER</div>
+      <div>
+        <RangeSlider min={0} max={42} step={1} callback={console.log} />
+      </div>
     </div>
   );
 }
