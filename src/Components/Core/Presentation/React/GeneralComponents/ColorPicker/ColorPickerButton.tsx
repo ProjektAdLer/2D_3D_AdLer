1a9d19df16f688f914e5d3e2a6416f9a44a23c5e
Felix Kleis <felixkleis@yahoo.de>
2024-11-29 13:36:40 +0100

    Added colorpreview to the colorpickerbutton

--- src/Components/Core/Presentation/React/GeneralComponents/ColorPicker/ColorPickerButton.tsx
+++ src/Components/Core/Presentation/React/GeneralComponents/ColorPicker/ColorPickerButton.tsx
@@ -2,10 +2,12 @@ import { AdLerUIComponent } from "src/Components/Core/Types/ReactTypes";
 import tailwindMerge from "../../../Utils/TailwindMerge";
 import StyledButton from "~ReactComponents/ReactRelated/ReactBaseComponents/StyledButton";
 import { AvatarColor } from "src/Components/Core/Domain/AvatarModels/AvatarColorPalette";
+import { useEffect, useState } from "react";
 
 interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   currentColor: AvatarColor;
 }
+
 export default function ColorPickerButton({
   onClick,
   className,
@@ -15,6 +17,10 @@ export default function ColorPickerButton({
     <div className={tailwindMerge(className, "")}>
       <StyledButton onClick={onClick} shape="freeFloatCenter">
         {/* <img className="w-10 xl:w-12" src={helpIcon} alt="Help Icon" /> */}
+        <div
+          className={`border-2 border-black rounded m-2 h-5 w-5`}
+          style={{ backgroundColor: currentColor.hexColor }}
+        />
         {currentColor.nameKey}
       </StyledButton>
     </div>
