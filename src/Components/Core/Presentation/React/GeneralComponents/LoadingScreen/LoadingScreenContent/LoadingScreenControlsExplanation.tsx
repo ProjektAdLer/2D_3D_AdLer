9e2e5a392ddd9e5737036fc0fa2bcbff9c4f9092
splican <daniel.glaab@th-ab.de>
2024-12-17 15:39:08 +0100

    changed faulty icons

--- src/Components/Core/Presentation/React/GeneralComponents/LoadingScreen/LoadingScreenContent/LoadingScreenControlsExplanation.tsx
+++ src/Components/Core/Presentation/React/GeneralComponents/LoadingScreen/LoadingScreenContent/LoadingScreenControlsExplanation.tsx
@@ -5,12 +5,12 @@ export default function LoadingScreenControlsExplanation() {
   const { t: translate } = useTranslation("learningSpace");
 
   return (
-    <div className="row-span-4 w-[90vw] max-w-[80vw] portrait:h-[50vh] max-h-[45vh] bg-buttonbgblue p-4 rounded-xl overflow-y-auto">
+    <div className="row-span-4 w-[90vw] relative max-w-[80vw] portrait:h-[50vh] max-h-[45vh] bg-buttonbgblue p-4 rounded-xl overflow-y-auto">
       <h1 className="font-bold portrait:pb-6 text-adlerdarkblue">
         {translate("sidebar_controls")}
       </h1>
       <ControlsExplanationContent />
-      <p className="w-full text-xs text-center">
+      <p className="w-full text-xs text-center absolute bottom-5">
         {translate("hint_controlsExplanationModal")}
       </p>
     </div>
