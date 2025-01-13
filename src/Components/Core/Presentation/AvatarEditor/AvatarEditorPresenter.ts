6f36a72842086e631eedd500fdd58e0e7e5a8a69
Felix Kleis <felixkleis@yahoo.de>
2025-01-13 15:32:40 +0100

    added missing onAvatarConfigChanged portcall in Avatareditor presenter

--- src/Components/Core/Presentation/AvatarEditor/AvatarEditorPresenter.ts
+++ src/Components/Core/Presentation/AvatarEditor/AvatarEditorPresenter.ts
@@ -16,4 +16,21 @@ export default class AvatarEditorPresenter implements IAvatarEditorPresenter {
     this.viewModel.mouth.Value =
       avatarConfig.mouth ?? this.viewModel.mouth.Value;
   }
+
+  onAvatarConfigChanged(
+    newAvatarConfig: AvatarConfigTO,
+    avatarConfigDiff: Partial<AvatarConfigTO>,
+  ): void {
+    this.viewModel.hair.Value =
+      newAvatarConfig.hair ?? this.viewModel.hair.Value;
+    this.viewModel.beard.Value =
+      newAvatarConfig.beard ?? this.viewModel.beard.Value;
+    this.viewModel.hairColor.Value =
+      newAvatarConfig.hairColor ?? this.viewModel.hairColor.Value;
+
+    this.viewModel.eyes.Value =
+      newAvatarConfig.eyes ?? this.viewModel.eyes.Value;
+    this.viewModel.mouth.Value =
+      newAvatarConfig.mouth ?? this.viewModel.mouth.Value;
+  }
 }
