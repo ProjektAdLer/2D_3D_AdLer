ea05ba4673b2fe10059480c1e377fa614d04a5a9
Felix Kleis <felixkleis@yahoo.de>
2025-01-14 11:23:39 +0100

    ixed existing tests

--- src/Components/CoreTest/Presentation/React/GeneralComponents/TileLayout/Tile.test.tsx
+++ src/Components/CoreTest/Presentation/React/GeneralComponents/TileLayout/Tile.test.tsx
@@ -2,7 +2,7 @@ import { render } from "@testing-library/react";
 import React from "react";
 import Tile from "../../../../../Core/Presentation/React/GeneralComponents/TileLayout/Tile";
 
-const img = require("../../../../../../Assets/avatarEditorThumbnails/hair/hairstyles/Hair_Backhead.png");
+const img = require("../../../../../../Assets/avatarEditorThumbnails/hair/hairstyles/aa-hair-backhead.png");
 
 describe("Tile", () => {
   test("should render", () => {
