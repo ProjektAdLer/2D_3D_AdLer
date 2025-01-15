b621cda81527a74368819445a21bc6d4c42a092f
Felix Kleis <felixkleis@yahoo.de>
2025-01-15 16:16:10 +0100

    Reordered AvatarEditorPresenter for better readability

--- src/Components/Core/Presentation/AvatarEditor/AvatarEditorPresenter.ts
+++ src/Components/Core/Presentation/AvatarEditor/AvatarEditorPresenter.ts
@@ -16,20 +16,24 @@ export default class AvatarEditorPresenter implements IAvatarEditorPresenter {
     this.updateViewModel(newAvatarConfig);
   }
   updateViewModel(avatarConfig: AvatarConfigTO) {
+    // Face
+    this.viewModel.mouth.Value =
+      avatarConfig.mouth ?? this.viewModel.mouth.Value;
+    this.viewModel.eyes.Value = avatarConfig.eyes ?? this.viewModel.eyes.Value;
+    // Hair
     this.viewModel.hair.Value = avatarConfig.hair ?? this.viewModel.hair.Value;
     this.viewModel.beard.Value =
       avatarConfig.beard ?? this.viewModel.beard.Value;
     this.viewModel.hairColor.Value =
       avatarConfig.hairColor ?? this.viewModel.hairColor.Value;
-
-    this.viewModel.eyes.Value = avatarConfig.eyes ?? this.viewModel.eyes.Value;
-    this.viewModel.mouth.Value =
-      avatarConfig.mouth ?? this.viewModel.mouth.Value;
-    this.viewModel.skinColor.Value =
-      avatarConfig.skinColor ?? this.viewModel.skinColor.Value;
+    // Accessories
     this.viewModel.headgear.Value =
       avatarConfig.headgear ?? this.viewModel.headgear.Value;
     this.viewModel.glasses.Value =
       avatarConfig.glasses ?? this.viewModel.glasses.Value;
+    //clothes
+    //Body
+    this.viewModel.skinColor.Value =
+      avatarConfig.skinColor ?? this.viewModel.skinColor.Value;
   }
 }
