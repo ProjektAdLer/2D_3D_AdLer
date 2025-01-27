1385f76b4d17754fd90f59a7e9b1c6e518c3d22a
Mountler <mikey-nihon@web.de>
2025-01-27 14:58:01 +0100

    refactored calculation of uv coordiantes of avatar editor

--- src/Components/Core/Domain/AvatarModels/AvatarFaceUVTexture.ts
+++ src/Components/Core/Domain/AvatarModels/AvatarFaceUVTexture.ts
@@ -5,116 +5,70 @@ interface AvatarUVOffset {
   vOffset: number;
 }
 
-export const AvatarEyeBrowTexture: AvatarUVOffset[] = [
-  { id: 0, name: "Brows_1", uOffset: 0, vOffset: 0 },
-  { id: 1, name: "Brows_2", uOffset: 0.2497, vOffset: 0 },
-  { id: 2, name: "Brows_3", uOffset: 0.4994, vOffset: 0 },
-  { id: 3, name: "Brows_4", uOffset: 0.7491, vOffset: 0 },
-  { id: 4, name: "Brows_5", uOffset: 0, vOffset: 0.249 },
-  { id: 5, name: "Brows_6", uOffset: 0.2497, vOffset: 0.249 },
-  { id: 6, name: "Brows_7", uOffset: 0.4994, vOffset: 0.249 },
-  { id: 7, name: "Brows_8", uOffset: 0.7491, vOffset: 0.249 },
-  { id: 8, name: "Brows_9", uOffset: 0, vOffset: 0.498 },
-  { id: 9, name: "Brows_10", uOffset: 0.2497, vOffset: 0.498 },
-  { id: 10, name: "Brows_11", uOffset: 0.4994, vOffset: 0.498 },
-  { id: 11, name: "Brows_12", uOffset: 0.7491, vOffset: 0.498 },
-];
+const AvatarEyeBrowTexture: AvatarUVOffset[] = [];
+for (let y = 0; y < 3; y++) {
+  for (let x = 0; x < 4; x++) {
+    AvatarEyeBrowTexture.push({
+      id: x + y * 4,
+      name: "Brows_" + (1 + x + y * 4).toString(),
+      uOffset: x * 0.2497,
+      vOffset: y * 0.249,
+    });
+  }
+}
+export { AvatarEyeBrowTexture };
 
-export const AvatarEyeTexture: AvatarUVOffset[] = [
-  { id: 0, name: "Neural_Eyes_1", uOffset: 0, vOffset: 0 },
-  { id: 1, name: "Neural_Eyes_2", uOffset: 0.2498, vOffset: 0 },
-  { id: 2, name: "Neural_Eyes_3", uOffset: 0.4996, vOffset: 0 },
-  { id: 3, name: "Neural_Eyes_4", uOffset: 0.7494, vOffset: 0 },
-  { id: 4, name: "Female_Eyes_1", uOffset: 0, vOffset: 0.09975 },
-  { id: 5, name: "Female_Eyes_2", uOffset: 0.2498, vOffset: 0.09975 },
-  { id: 6, name: "Female_Eyes_3", uOffset: 0.4996, vOffset: 0.09975 },
-  { id: 7, name: "Female_Eyes_4", uOffset: 0.7494, vOffset: 0.09975 },
-  { id: 8, name: "Angry_Eyes_1", uOffset: 0, vOffset: 0.1995 },
-  { id: 9, name: "Angry_Eyes_2", uOffset: 0.2498, vOffset: 0.1995 },
-  { id: 10, name: "Angry_Eyes_3", uOffset: 0.4996, vOffset: 0.1995 },
-  { id: 11, name: "Angry_Eyes_4", uOffset: 0.7494, vOffset: 0.1995 },
-  { id: 12, name: "Specular_Eyes_1", uOffset: 0, vOffset: 0.29925 },
-  { id: 13, name: "Specular_Eyes_2", uOffset: 0.2498, vOffset: 0.29925 },
-  { id: 14, name: "Specular_Eyes_3", uOffset: 0.4996, vOffset: 0.29925 },
-  { id: 15, name: "Specular_Eyes_4", uOffset: 0.7494, vOffset: 0.29925 },
-  { id: 16, name: "Tired_Eyes_1", uOffset: 0, vOffset: 0.399 },
-  { id: 17, name: "Tired_Eyes_2", uOffset: 0.2498, vOffset: 0.399 },
-  { id: 18, name: "Tired_Eyes_3", uOffset: 0.4996, vOffset: 0.399 },
-  { id: 19, name: "Tired_Eyes_4", uOffset: 0.7494, vOffset: 0.399 },
-  { id: 20, name: "Makeup_Eyes_1", uOffset: 0, vOffset: 0.49875 },
-  { id: 21, name: "Makeup_Eyes_2", uOffset: 0.2498, vOffset: 0.49875 },
-  { id: 22, name: "Makeup_Eyes_3", uOffset: 0.4996, vOffset: 0.49875 },
-  { id: 23, name: "Makeup_Eyes_4", uOffset: 0.7494, vOffset: 0.49875 },
-  { id: 24, name: "Old_Eyes_1", uOffset: 0, vOffset: 0.5985 },
-  { id: 25, name: "Old_Eyes_2", uOffset: 0.2498, vOffset: 0.5985 },
-  { id: 26, name: "Old_Eyes_3", uOffset: 0.4996, vOffset: 0.5985 },
-  { id: 27, name: "Old_Eyes_4", uOffset: 0.7494, vOffset: 0.5985 },
-  { id: 28, name: "Crooked_Eyes_1", uOffset: 0, vOffset: 0.69825 },
-  { id: 29, name: "Crooked_Eyes_2", uOffset: 0.2498, vOffset: 0.69825 },
-  { id: 30, name: "Crooked_Eyes_3", uOffset: 0.4996, vOffset: 0.69825 },
-  { id: 31, name: "Crooked_Eyes_4", uOffset: 0.7494, vOffset: 0.69825 },
-  { id: 32, name: "Small_Eyes_1", uOffset: 0, vOffset: 0.798 },
-  { id: 33, name: "Small_Eyes_2", uOffset: 0.2498, vOffset: 0.798 },
-  { id: 34, name: "Small_Eyes_3", uOffset: 0.4996, vOffset: 0.798 },
-  { id: 35, name: "Small_Eyes_4", uOffset: 0.7494, vOffset: 0.798 },
-];
+const AvatarEyeTexture: AvatarUVOffset[] = [];
+for (let y = 0; y < 9; y++) {
+  for (let x = 0; x < 4; x++) {
+    AvatarEyeTexture.push({
+      id: x + y * 4,
+      name:
+        [
+          "Neural_Eyes_",
+          "Female_Eyes_",
+          "Angry_Eyes_",
+          "Specular_Eyes_",
+          "Tired_Eyes_",
+          "Makeup_Eyes_",
+          "Old_Eyes_",
+          "Crooked_Eyes_",
+          "Small_Eyes_",
+        ][y] + (1 + x).toString(),
+      uOffset: x * 0.2498,
+      vOffset: y * 0.09975,
+    });
+  }
+  console.log("hello");
+}
+export { AvatarEyeTexture };
 
-export const AvatarNoseTexture: AvatarUVOffset[] = [
-  { id: 0, name: "Nose_1", uOffset: 0, vOffset: 0 },
-  { id: 1, name: "Nose_2", uOffset: 0.1665, vOffset: 0 },
-  { id: 2, name: "Nose_3", uOffset: 0.333, vOffset: 0 },
-  { id: 3, name: "Nose_4", uOffset: 0.4995, vOffset: 0 },
-  { id: 4, name: "Nose_5", uOffset: 0.666, vOffset: 0 },
-  { id: 5, name: "Nose_6", uOffset: 0.8325, vOffset: 0 },
+const AvatarNoseTexture: AvatarUVOffset[] = [];
+for (let y = 0; y < 3; y++) {
+  for (let x = 0; x < 6; x++) {
+    AvatarNoseTexture.push({
+      id: x + y * 6,
+      name: "Nose_" + (1 + x + y * 6).toString(),
+      uOffset: x * 0.1665,
+      vOffset: y * 0.249,
+    });
+  }
+}
+export { AvatarNoseTexture };
 
-  { id: 6, name: "Nose_7", uOffset: 0, vOffset: 0.249 },
-  { id: 7, name: "Nose_8", uOffset: 0.1665, vOffset: 0.249 },
-  { id: 8, name: "Nose_9", uOffset: 0.333, vOffset: 0.249 },
-  { id: 9, name: "Nose_10", uOffset: 0.4995, vOffset: 0.249 },
-  { id: 10, name: "Nose_11", uOffset: 0.666, vOffset: 0.249 },
-  { id: 11, name: "Nose_12", uOffset: 0.8325, vOffset: 0.249 },
-
-  { id: 12, name: "Nose_13", uOffset: 0, vOffset: 0.498 },
-  { id: 13, name: "Nose_14", uOffset: 0.1665, vOffset: 0.498 },
-  { id: 14, name: "Nose_15", uOffset: 0.333, vOffset: 0.498 },
-  { id: 15, name: "Nose_16", uOffset: 0.4995, vOffset: 0.498 },
-  { id: 16, name: "Nose_17", uOffset: 0.666, vOffset: 0.498 },
-  { id: 17, name: "Nose_18", uOffset: 0.8325, vOffset: 0.498 },
-];
-
-export const AvatarMouthTexture: AvatarUVOffset[] = [
-  { id: 0, name: "Mouth_1", uOffset: -0.09, vOffset: -0.047 },
-  { id: 1, name: "Mouth_2", uOffset: 0, vOffset: -0.047 },
-  { id: 2, name: "Mouth_3", uOffset: 0.09, vOffset: -0.047 },
-  { id: 3, name: "Mouth_4", uOffset: 0.18, vOffset: -0.047 },
-  { id: 4, name: "Mouth_5", uOffset: 0.27, vOffset: -0.047 },
-  { id: 5, name: "Mouth_6", uOffset: 0.36, vOffset: -0.047 },
-
-  { id: 6, name: "Mouth_7", uOffset: -0.09, vOffset: 0 },
-  { id: 7, name: "Mouth_8", uOffset: 0, vOffset: 0 },
-  { id: 8, name: "Mouth_9", uOffset: 0.09, vOffset: 0 },
-  { id: 9, name: "Mouth_10", uOffset: 0.18, vOffset: 0 },
-  { id: 10, name: "Mouth_11", uOffset: 0.27, vOffset: 0 },
-  { id: 11, name: "Mouth_12", uOffset: 0.36, vOffset: 0 },
-
-  { id: 12, name: "Mouth_13", uOffset: -0.09, vOffset: 0.046 },
-  { id: 13, name: "Mouth_14", uOffset: 0, vOffset: 0.046 },
-  { id: 14, name: "Mouth_15", uOffset: 0.09, vOffset: 0.046 },
-  { id: 15, name: "Mouth_16", uOffset: 0.18, vOffset: 0.046 },
-  { id: 16, name: "Mouth_17", uOffset: 0.27, vOffset: 0.046 },
-  { id: 17, name: "Mouth_18", uOffset: 0.36, vOffset: 0.046 },
-
-  { id: 18, name: "Mouth_19", uOffset: -0.09, vOffset: 0.093 },
-  { id: 19, name: "Mouth_20", uOffset: 0, vOffset: 0.093 },
-  { id: 20, name: "Mouth_21", uOffset: 0.09, vOffset: 0.093 },
-  { id: 21, name: "Mouth_22", uOffset: 0.18, vOffset: 0.093 },
-  { id: 22, name: "Mouth_23", uOffset: 0.27, vOffset: 0.093 },
-  { id: 23, name: "Mouth_24", uOffset: 0.36, vOffset: 0.093 },
-
-  { id: 24, name: "Mouth_25", uOffset: -0.09, vOffset: 0.14 },
-  { id: 25, name: "Mouth_26", uOffset: 0, vOffset: 0.14 },
-  { id: 26, name: "Mouth_27", uOffset: 0.09, vOffset: 0.14 },
-  { id: 27, name: "Mouth_28", uOffset: 0.18, vOffset: 0.14 },
-  { id: 28, name: "Mouth_29", uOffset: 0.27, vOffset: 0.14 },
-  { id: 29, name: "Mouth_30", uOffset: 0.36, vOffset: 0.14 },
-];
+const AvatarMouthTexture: AvatarUVOffset[] = [];
+for (let y = 0; y < 5; y++) {
+  for (let x = 0; x < 6; x++) {
+    AvatarMouthTexture.push({
+      id: x + y * 6,
+      name: "Mouth_" + (1 + x + y * 6).toString(),
+      uOffset: x * 0.1666,
+      vOffset: y * 0.166,
+    });
+    AvatarMouthTexture[x + y * 6].id = x + y * 6;
+    AvatarMouthTexture[x + y * 6].name = "Mouth_" + (1 + x + y * 6).toString();
+    AvatarMouthTexture[x + y * 6].uOffset = x * 0.1666;
+    AvatarMouthTexture[x + y * 6].vOffset = y * 0.166;
+  }
+}
+export { AvatarMouthTexture };
