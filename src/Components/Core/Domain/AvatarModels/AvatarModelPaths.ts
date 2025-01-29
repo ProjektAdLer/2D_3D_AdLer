2c89d6a65e90e8ee6b20f71e1256b26114e4c2d4
Mountler <mikey-nihon@web.de>
2025-01-29 11:49:14 +0100

    exported common avatar editor data into seperate modules

---
+++ src/Components/Core/Domain/AvatarModels/AvatarModelPaths.ts
@@ -0,0 +1,13 @@
+const AvatarModelAssetPaths = {
+  hairPath: "hair/hairstyle",
+  beardPath: "hair/beards",
+  headGearPath: "accessoires/headgear",
+  glassesPath: "accessoires/glasses",
+  backpackPath: "accessoires/backpack",
+  otherPath: "accessoires/other",
+  shirtPath: "clothing/shirts",
+  pantsPath: "clothing/pants",
+  shoesPath: "clothing/shoes",
+} as const;
+
+export default AvatarModelAssetPaths;
