e3602ae6ed4c897afae7943d7001576b52bd9662
Felix Kleis <felixkleis@yahoo.de>
2025-02-21 15:41:52 +0100

    Added tests to AvatarEditorClothingCategory

---
+++ src/Components/CoreTest/Presentation/AvatarEditor/AvatarEditorCategories/AvatarEditorCategoryContents/AvatarEditorClothingCategory.test.tsx
@@ -0,0 +1,107 @@
+import { mock } from "jest-mock-extended";
+import AvatarEditorViewModel, {
+  AvatarEditorUI,
+} from "../../../../../Core/Presentation/AvatarEditor/AvatarEditorViewModel";
+import IAvatarEditorController from "../../../../../Core/Presentation/AvatarEditor/IAvatarEditorController";
+import Observable from "../../../../../../Lib/Observable";
+import {
+  AvatarPantsModels,
+  AvatarShirtModels,
+  AvatarShoesModels,
+} from "../../../../../Core/Domain/AvatarModels/AvatarModelTypes";
+import { AvatarColor } from "../../../../../Core/Domain/AvatarModels/AvatarColorPalette";
+import { fireEvent, render } from "@testing-library/react";
+import React from "react";
+import AvatarEditorClothingCategory from "../../../../../Core/Presentation/AvatarEditor/AvatarEditorCategories/AvatarEditorCategoryContents/AvatarEditorClothingCategory";
+
+const avatarEditorMock = mock<AvatarEditorViewModel>();
+const avatarEditorControllerMock = mock<IAvatarEditorController>();
+
+describe("AvatarEditorClothingCategory", () => {
+  test("should render", () => {
+    avatarEditorMock.uiVisiblity = {
+      clothingMenu: {
+        shirts: new Observable<boolean>(true),
+        pants: new Observable<boolean>(true),
+        shoes: new Observable<boolean>(true),
+      },
+    } as AvatarEditorUI;
+
+    avatarEditorMock.shirt = new Observable<AvatarShirtModels>("none");
+    avatarEditorMock.pants = new Observable<AvatarPantsModels>("none");
+    avatarEditorMock.shoes = new Observable<AvatarShoesModels>("none");
+    avatarEditorMock.shirtColor = new Observable<AvatarColor>({
+      id: 33,
+      nameKey: "Brown 2",
+      hexColor: "#4b2a1a",
+    });
+    avatarEditorMock.pantsColor = new Observable<AvatarColor>({
+      id: 33,
+      nameKey: "Brown 2",
+      hexColor: "#4b2a1a",
+    });
+    avatarEditorMock.shoesColor = new Observable<AvatarColor>({
+      id: 33,
+      nameKey: "Brown 2",
+      hexColor: "#4b2a1a",
+    });
+
+    const { container } = render(
+      <AvatarEditorClothingCategory
+        viewModel={avatarEditorMock}
+        controller={avatarEditorControllerMock}
+      />,
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  test.skip.each([
+    ["shirt", true, false, false],
+    ["pants", false, true, false],
+    ["shoes", false, false, true],
+  ])(
+    "click on tile in category %s calls controller",
+    (category, shirtUI, pantsUI, shoesUi) => {
+      avatarEditorMock.uiVisiblity = {
+        clothingMenu: {
+          shirts: new Observable<boolean>(shirtUI),
+          pants: new Observable<boolean>(pantsUI),
+          shoes: new Observable<boolean>(shoesUi),
+        },
+      } as AvatarEditorUI;
+
+      avatarEditorMock.shirt = new Observable<AvatarShirtModels>("none");
+      avatarEditorMock.pants = new Observable<AvatarPantsModels>("none");
+      avatarEditorMock.shoes = new Observable<AvatarShoesModels>("none");
+      avatarEditorMock.shirtColor = new Observable<AvatarColor>({
+        id: 33,
+        nameKey: "Brown 2",
+        hexColor: "#4b2a1a",
+      });
+      avatarEditorMock.pantsColor = new Observable<AvatarColor>({
+        id: 33,
+        nameKey: "Brown 2",
+        hexColor: "#4b2a1a",
+      });
+      avatarEditorMock.shoesColor = new Observable<AvatarColor>({
+        id: 33,
+        nameKey: "Brown 2",
+        hexColor: "#4b2a1a",
+      });
+
+      const container = render(
+        <AvatarEditorClothingCategory
+          viewModel={avatarEditorMock}
+          controller={avatarEditorControllerMock}
+        />,
+      );
+
+      const tile = container.getByAltText("noneThumbnail");
+      fireEvent.click(tile);
+
+      expect(
+        avatarEditorControllerMock.onAvatarConfigChanged,
+      ).toHaveBeenCalled();
+    },
+  );
+});
