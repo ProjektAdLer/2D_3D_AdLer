1b498231e0956a8e52adf2f95b28a87c26e9edcf
Lukas Schaab <lukas.schaab@lukaio.de>
2025-03-03 14:57:02 +0100

    added new glbs for subtheme library and elevator doors

--- src/Components/Core/Adapters/BackendAdapter/MockBackendData/SmallWorldAWT.ts
+++ src/Components/Core/Adapters/BackendAdapter/MockBackendData/SmallWorldAWT.ts
@@ -47,7 +47,7 @@ const SmallWorldAWT: AWT = {
           "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit",
           "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit",
         ],
-        spaceTemplate: "T_40X32_13L",
+        spaceTemplate: "D_40X37_15L",
         spaceTemplateStyle: "CAMPUSKEMPTEN",
         spaceStory: {
           introStory: {
