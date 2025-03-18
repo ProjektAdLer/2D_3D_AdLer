f62a75f86b55421eb611e8b061d962174d69600f
splican <daniel2105@gmx.net>
2025-03-18 11:42:27 +0100

    initial branch commit with newly styled narrativeFramework intro

--- src/Components/Core/Presentation/React/GeneralComponents/NarrativeFramework/NarrativeFrameworkViewModel.ts
+++ src/Components/Core/Presentation/React/GeneralComponents/NarrativeFramework/NarrativeFrameworkViewModel.ts
@@ -4,7 +4,9 @@ import Observable from "src/Lib/Observable";
 export default class NarrativeFrameworkViewModel {
   isOpen: Observable<boolean> = new Observable<boolean>(true);
 
-  introText?: string = "Beispieltext";
-  outroText?: string = "Beispieltext";
+  introText?: string =
+    "Die Jedi sind die Friedenswächter der Galaxis. Sie nutzen ihre Kraft zur Verteidigung und zum Schutz anderer, nie jedoch zum Angriff. Die Jedi achten alles Leben, in jeder Form. Die Jedi dienen, anstatt zu herrschen, zum Wohle der Galaxis. Die Jedi streben nach Vervollkommnung durch Wissen und Ausbildung. Die Jedi leben in der Macht. Die Macht ist die Energie, die alles Leben verbindet, die Macht umgibt uns, sie durchdringt uns, sie hält die Galaxis zusammen. Die Jedi vertrauen auf die Macht. Möge die Macht mit dir sein. Immer. Ich war dein Bruder Anakin. Ich liebte dich. Ich hasse dich! Meis.";
+  outroText?: string =
+    "Und wenn sie nicht gestorben sind, dann leben sie noch heute.";
   theme: LearningSpaceThemeType;
 }
