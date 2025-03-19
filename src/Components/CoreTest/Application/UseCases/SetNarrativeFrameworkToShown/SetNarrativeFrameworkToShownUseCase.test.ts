e4e4cc34eddb33c71ba37202a107cce085247ae6
Felix Kleis <felixkleis@yahoo.de>
2025-03-19 10:28:32 +0100

    Added tests for setNarrativeFrameworkToShown Usecase

--- src/Components/CoreTest/Application/UseCases/SetNarrativeFrameworkToShown/SetNarrativeFrameworkToShownUseCase.test.ts
+++ src/Components/CoreTest/Application/UseCases/SetNarrativeFrameworkToShown/SetNarrativeFrameworkToShownUseCase.test.ts
@@ -1,27 +1,33 @@
-import { mockDeep } from "jest-mock-extended";
-import SetWorldCompletionModalToShownUseCase from "../../../../Core/Application/UseCases/SetWorldCompletionModalToShown/SetWorldCompletionModalToShownUseCase";
+import { mock, mockDeep } from "jest-mock-extended";
 import IEntityContainer from "../../../../Core/Domain/EntityContainer/IEntityContainer";
+import SetNarrativeFrameworkToShownUseCase from "../../../../Core/Application/UseCases/SetNarrativeFrameworkToShown/SetNarrativeFrameworkToShownUseCase";
 import CoreDIContainer from "../../../../Core/DependencyInjection/CoreDIContainer";
 import CORE_TYPES from "../../../../Core/DependencyInjection/CoreTypes";
+import GetUserLocationUseCase from "../../../../Core/Application/UseCases/GetUserLocation/GetUserLocationUseCase";
+import USECASE_TYPES from "../../../../Core/DependencyInjection/UseCases/USECASE_TYPES";
 import LearningWorldEntity from "../../../../Core/Domain/Entities/LearningWorldEntity";
 import { ConstructorReference } from "../../../../Core/Types/EntityManagerTypes";
 
 const entityContainerMock = mockDeep<IEntityContainer>();
+const getUserLocationUseCaseMock = mock<GetUserLocationUseCase>();
 
-describe("SetWorldCompletionModalToShownUseCase", () => {
-  let systemUnderTest: SetWorldCompletionModalToShownUseCase;
+describe("SetNarrativeFrameworkToShownUseCase", () => {
+  let systemUnderTest: SetNarrativeFrameworkToShownUseCase;
 
   beforeAll(() => {
     CoreDIContainer.snapshot();
 
-    CoreDIContainer.rebind<IEntityContainer>(
-      CORE_TYPES.IEntityContainer,
-    ).toConstantValue(entityContainerMock);
+    CoreDIContainer.rebind(CORE_TYPES.IEntityContainer).toConstantValue(
+      entityContainerMock,
+    );
+    CoreDIContainer.rebind(
+      USECASE_TYPES.IGetUserLocationUseCase,
+    ).toConstantValue(getUserLocationUseCaseMock);
   });
 
   beforeEach(() => {
     systemUnderTest = CoreDIContainer.resolve(
-      SetWorldCompletionModalToShownUseCase,
+      SetNarrativeFrameworkToShownUseCase,
     );
   });
 
@@ -29,28 +35,36 @@ describe("SetWorldCompletionModalToShownUseCase", () => {
     CoreDIContainer.restore();
   });
 
-  test("execute sets completionModalShown in worldEntity", () => {
+  test("execute sets narrativeFramework.shownBefore in worldEntity", () => {
     const worldEntity = {
       name: "worldEntity",
       id: 1,
-      completionModalShown: false,
+      narrativeFramework: {
+        shownBefore: false,
+      },
     } as Partial<LearningWorldEntity>;
+    getUserLocationUseCaseMock.execute.mockReturnValue({ worldID: 1 } as any);
     entityContainerMock.filterEntitiesOfType.mockReturnValueOnce([worldEntity]);
-    systemUnderTest.execute({ worldID: 1 });
-    expect(worldEntity.completionModalShown).toBe(true);
+    systemUnderTest.execute();
+    expect(worldEntity.narrativeFramework?.shownBefore).toBe(true);
   });
 
   test("Filter function filters the right world", () => {
     const worldEntity1 = {
       name: "worldEntity1",
       id: 1,
-      completionModalShown: false,
+      narrativeFramework: {
+        shownBefore: false,
+      },
     } as Partial<LearningWorldEntity>;
     const worldEntity2 = {
       name: "worldEntity2",
       id: 2,
-      completionModalShown: false,
+      narrativeFramework: {
+        shownBefore: false,
+      },
     } as Partial<LearningWorldEntity>;
+    getUserLocationUseCaseMock.execute.mockReturnValue({ worldID: 1 } as any);
 
     let testFilter;
     entityContainerMock.filterEntitiesOfType.mockImplementation(
@@ -62,7 +76,7 @@ describe("SetWorldCompletionModalToShownUseCase", () => {
         return [worldEntity1];
       },
     );
-    systemUnderTest.execute({ worldID: 1 });
+    systemUnderTest.execute();
 
     expect(testFilter(worldEntity1)).toBe(true);
     expect(testFilter(worldEntity2)).toBe(false);
