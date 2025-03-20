eec04e73d6b78c3cf24939b980eca048186c7c62
Felix Kleis <felixkleis@yahoo.de>
2025-03-20 14:30:17 +0100

    removed unused import

--- src/Components/Core/Application/UseCases/GetNarrativeFrameworkInfo/IGetNarrativeFrameworkInfoUseCase.ts
+++ src/Components/Core/Application/UseCases/GetNarrativeFrameworkInfo/IGetNarrativeFrameworkInfoUseCase.ts
@@ -1,5 +1,4 @@
 import { ISynchronousUsecase } from "../../Abstract/ISynchronousUsecase";
-import UserLocationTO from "../../DataTransferObjects/UserLocationTO";
 
 export default interface IGetNarrativeFrameworkInfoUseCase
   extends ISynchronousUsecase<void, void> {}
