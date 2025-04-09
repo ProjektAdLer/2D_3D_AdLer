e71c352f2edaa1cbe44fec12a1fa0bf6d40c9ab9
AdLer Lukas <lukas.schaab@th-ab.de>
2025-04-09 11:56:51 +0200

    reduced offset of avtar to ground

--- src/Components/Core/Presentation/Babylon/Ambience/AmbienceView.ts
+++ src/Components/Core/Presentation/Babylon/Ambience/AmbienceView.ts
@@ -7,6 +7,7 @@ import SCENE_TYPES, {
 import LearningSpaceSceneDefinition from "../SceneManagement/Scenes/LearningSpaceSceneDefinition";
 import { Mesh, Vector3 } from "@babylonjs/core";
 import LearningSpaceThemeLookup from "src/Components/Core/Domain/LearningSpaceThemes/LearningSpaceThemeLookup";
+import { LearningSpaceThemeType } from "src/Components/Core/Domain/Types/LearningSpaceThemeTypes";
 
 export default class AmbienceView {
   private scenePresenter: IScenePresenter;
@@ -25,7 +26,15 @@ export default class AmbienceView {
 
     this.viewModel.meshes.Value = results as Mesh[];
     this.viewModel.meshes.Value.forEach((mesh) => {
-      mesh.translate(new Vector3(0, -0.05, 0), 1);
+      if (
+        this.viewModel.theme === LearningSpaceThemeType.Campus ||
+        this.viewModel.theme === LearningSpaceThemeType.CampusAB ||
+        this.viewModel.theme === LearningSpaceThemeType.CampusKE ||
+        this.viewModel.theme === LearningSpaceThemeType.Arcade ||
+        this.viewModel.theme === LearningSpaceThemeType.Suburb
+      ) {
+        mesh.translate(new Vector3(0, -0.05, 0), 1);
+      }
       mesh.alwaysSelectAsActiveMesh = true; //Fixes Background Animations being Culled, but may cause Performance Issues, Fix with Changing Camera Rotation Frustum
       mesh.isPickable = false;
     });
