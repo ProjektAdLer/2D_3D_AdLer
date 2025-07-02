87235d4f5b0e60d871bbc1788772200d255fa748
AdLer Lukas <lukas.schaab@th-ab.de>
2025-07-02 12:08:28 +0200

    fixed error in MenuHeaderBarViewModel

--- src/Components/Core/Presentation/React/GeneralComponents/MenuHeaderBar/MenuHeaderBarViewModel.ts
+++ src/Components/Core/Presentation/React/GeneralComponents/MenuHeaderBar/MenuHeaderBarViewModel.ts
@@ -2,7 +2,8 @@ import Observable from "src/Lib/Observable";
 import { GradingStyle } from "src/Components/Core/Domain/Types/GradingStyle";
 
 export default class MenuHeaderBarViewModel {
-  public currentWorldName: Observable<string> = new Observable<string>();
-  public gradingStyle: Observable<GradingStyle> =
-    new Observable<GradingStyle>();
+  currentWorldName: Observable<string> = new Observable<string>("");
+  gradingStyle: Observable<GradingStyle | undefined> = new Observable<
+    GradingStyle | undefined
+  >(undefined);
 }
