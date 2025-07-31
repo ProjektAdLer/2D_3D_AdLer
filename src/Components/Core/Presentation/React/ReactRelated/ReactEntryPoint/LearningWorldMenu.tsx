5c8846a987416c208c384737650a44cb53a6224d
splican <daniel2105@gmx.net>
2025-07-31 14:03:21 +0200

    overhauled learningWorldMenu for all supported platforms

--- src/Components/Core/Presentation/React/ReactRelated/ReactEntryPoint/LearningWorldMenu.tsx
+++ src/Components/Core/Presentation/React/ReactRelated/ReactEntryPoint/LearningWorldMenu.tsx
@@ -6,16 +6,16 @@ import ReturnHomeModal from "~ReactComponents/LearningWorldMenu/ReturnHomeModal/
 
 export default function LearningWorldMenu() {
   return (
-    <div className="flex h-[100svh] flex-col overflow-hidden bg-gradient-to-br from-adlerbggradientfrom to-adlerbggradientto mobile-landscape:h-[100dvh] mobile-landscape:w-[100dvw] portrait:max-h-[100dvh]">
-      <div className="order-2 grid grow grid-cols-2 grid-rows-1 lg:rounded-lg portrait:grid-cols-1 portrait:grid-rows-6 portrait:gap-4">
-        <ReturnHomeModal />
-        <WorldSelection className="col-start-1 m-2 rounded-lg p-2 lg:p-8 portrait:col-span-2 portrait:col-start-1 portrait:row-start-1 portrait:h-full portrait:overflow-auto" />
-        <LearningWorldDetail className="col-start-2 flex p-2 lg:p-8 portrait:col-start-1 portrait:row-span-4 portrait:row-start-2" />
-      </div>
+    <div className="flex h-[100dvh] flex-col overflow-hidden bg-gradient-to-br from-adlerbggradientfrom to-adlerbggradientto mobile-landscape:h-[100dvh] mobile-landscape:w-[100dvw] portrait:h-[100dvh]">
       <MenuHeaderBar
         location="world"
-        className="order-1 w-full self-center p-2 font-semibold"
+        className="w-full flex-shrink-0 self-center p-2 font-semibold"
       />
+      <div className="grid min-h-0 flex-1 grid-cols-[1fr_1fr] grid-rows-1 lg:rounded-lg mobile-landscape:overflow-hidden portrait:grid-cols-1 portrait:grid-rows-[1fr_2fr] portrait:gap-4">
+        <ReturnHomeModal />
+        <WorldSelection className="col-start-1 m-2 rounded-lg p-2 lg:p-8 mobile-landscape:max-h-full mobile-landscape:overflow-auto portrait:col-span-1 portrait:col-start-1 portrait:row-start-1 portrait:overflow-auto" />
+        <LearningWorldDetail className="col-start-2 flex p-2 lg:p-8 mobile-landscape:min-h-0 mobile-landscape:overflow-hidden portrait:col-start-1 portrait:row-start-2 portrait:min-h-0 portrait:overflow-hidden" />
+      </div>
     </div>
   );
 }
