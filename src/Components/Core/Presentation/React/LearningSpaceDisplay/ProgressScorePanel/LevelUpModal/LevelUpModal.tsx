8f4f2a5647df2b7c0e440a6d82061c5549dc9794
Felix Kleis <felixkleis@yahoo.de>
2025-08-12 10:19:09 +0200

    Added an array to levelUpModal in preperation for badges

--- src/Components/Core/Presentation/React/LearningSpaceDisplay/ProgressScorePanel/LevelUpModal/LevelUpModal.tsx
+++ src/Components/Core/Presentation/React/LearningSpaceDisplay/ProgressScorePanel/LevelUpModal/LevelUpModal.tsx
@@ -1,11 +1,23 @@
 import StyledModal from "~ReactComponents/ReactRelated/ReactBaseComponents/StyledModal";
-import levelUpGraphic from "../../../../../../../../src/Assets/graphics/level-up.png";
 import useObservable from "~ReactComponents/ReactRelated/CustomHooks/useObservable";
 import useBuilder from "~ReactComponents/ReactRelated/CustomHooks/useBuilder";
 import BUILDER_TYPES from "~DependencyInjection/Builders/BUILDER_TYPES";
 import LevelUpModalViewModel from "./LevelUpModalViewModel";
 import { useEffect } from "react";
 import LevelUpModalController from "./LevelUpModalController";
+
+const badgePictures: { [key: number]: string } = {
+  1: require("../../../../../../../../src/Assets/graphics/level-up.png"),
+  2: require("../../../../../../../../src/Assets/graphics/level-up.png"),
+  3: require("../../../../../../../../src/Assets/graphics/level-up.png"),
+  4: require("../../../../../../../../src/Assets/graphics/level-up.png"),
+  5: require("../../../../../../../../src/Assets/graphics/level-up.png"),
+  6: require("../../../../../../../../src/Assets/graphics/level-up.png"),
+  7: require("../../../../../../../../src/Assets/graphics/level-up.png"),
+  8: require("../../../../../../../../src/Assets/graphics/level-up.png"),
+  9: require("../../../../../../../../src/Assets/graphics/level-up.png"),
+  10: require("../../../../../../../../src/Assets/graphics/level-up.png"),
+};
 
 export default function LevelUpModal() {
   const [viewModel, controller] = useBuilder<
@@ -27,11 +39,11 @@ export default function LevelUpModal() {
 
   return (
     <StyledModal showModal={isOpen} onClose={() => controller.close()}>
-      <div className="flex justify-center max-h-[75vh] lg:max-h-[85vh] xl:max-h-[85vh] w-fit h-fit max-w-[99vw]">
+      <div className="flex h-fit max-h-[75vh] w-fit max-w-[99vw] justify-center lg:max-h-[85vh] xl:max-h-[85vh]">
         <img
-          className="object-scale-down max-h-[75vh] max-w-[90vw]"
+          className="max-h-[75vh] max-w-[90vw] object-scale-down"
           alt="LearningImage!"
-          src={levelUpGraphic}
+          src={badgePictures[viewModel.level]}
         />
       </div>
     </StyledModal>
--- src/Components/Core/Presentation/React/LearningSpaceDisplay/ProgressScorePanel/LevelUpModal/LevelUpModalViewModel.ts
+++ src/Components/Core/Presentation/React/LearningSpaceDisplay/ProgressScorePanel/LevelUpModal/LevelUpModalViewModel.ts
@@ -2,5 +2,6 @@ import Observable from "src/Lib/Observable";
 
 export default class LevelUpModalViewModel {
   isOpen: Observable<boolean> = new Observable<boolean>(false);
+  level: number = 0;
   timeToClose: number = 2500;
 }
