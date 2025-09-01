28c3b688531a5b5ad4725705cc17792e9a3bbf92
Lukas Schaab <lukas.schaab@lukaio.de>
2025-09-01 11:16:10 +0200

    Add showcase mode with auto-login and simplified UI to WelcomePage

--- src/Components/Core/Presentation/React/ReactRelated/ReactEntryPoint/WelcomePage.tsx
+++ src/Components/Core/Presentation/React/ReactRelated/ReactEntryPoint/WelcomePage.tsx
@@ -14,9 +14,26 @@ import WelcomePageButton from "~ReactComponents/WelcomePage/WelcomePageButton/We
 import LMSButton from "~ReactComponents/WelcomePage/LMSButton/LMSButtonView";
 import learningWorldButtonBackgroundVideo from "../../../../../../Assets/misc/WelcomeScreenButtonBackgrounds/BackgroundVideoLernweltButton.mp4";
 import avatarEditorButtonBackgroundVideo from "../../../../../../Assets/misc/WelcomeScreenButtonBackgrounds/BackgroundVideoAvatarEditorButton.mp4";
+import { useEffect } from "react";
+import { useInjection } from "inversify-react";
+import ILoginUseCase from "src/Components/Core/Application/UseCases/Login/ILoginUseCase";
+import USECASE_TYPES from "~DependencyInjection/UseCases/USECASE_TYPES";
 
 export default function WelcomePage() {
   const { t: translate } = useTranslation("start");
+  const isShowcase = process.env.REACT_APP_IS_SHOWCASE === "true";
+  const loginUseCase = useInjection<ILoginUseCase>(USECASE_TYPES.ILoginUseCase);
+
+  // Automatischer Login im Showcase-Modus
+  useEffect(() => {
+    if (isShowcase) {
+      loginUseCase.executeAsync({
+        username: "showcase",
+        password: "showcase",
+      });
+    }
+  }, [isShowcase, loginUseCase]);
+
   return (
     <div className="relative grid h-[100svh] grid-cols-8 grid-rows-6 bg-gradient-to-br from-adlerbggradientfrom to-adlerbggradientto p-2 mobile-landscape:h-[100dvh] mobile-landscape:w-[100dvw] mobile-landscape:pb-1">
       <img
@@ -42,12 +59,18 @@ export default function WelcomePage() {
             default
           />
         </video>
-        <img
-          className="mobile-landscape:w-88 w-64 lg:hidden mobile-landscape:block tablet-portrait:w-full"
-          src={welcomePicture}
-          alt="3D Welcome Text"
-        />
-        <LoginComponent className="col-span-6 col-start-2 flex flex-col items-center justify-around lg:pt-8 xl:pt-3 mobile-landscape:pt-0 mobile-portrait:row-start-2 mobile-portrait:justify-start tablet-portrait:col-span-8 tablet-portrait:col-start-1" />
+        {!isShowcase && (
+          <img
+            className="mobile-landscape:w-88 w-64 lg:hidden mobile-landscape:block tablet-portrait:w-full"
+            src={welcomePicture}
+            alt="3D Welcome Text"
+          />
+        )}
+        {!isShowcase && (
+          <>
+            <LoginComponent className="col-span-6 col-start-2 flex flex-col items-center justify-around lg:pt-8 xl:pt-3 mobile-landscape:pt-0 mobile-portrait:row-start-2 mobile-portrait:justify-start tablet-portrait:col-span-8 tablet-portrait:col-start-1" />
+          </>
+        )}
       </section>
 
       <section className="portrait-col-span-6 col-span-6 col-start-2 row-span-3 row-start-3 mb-4 flex items-center justify-center gap-4 md:mb-8 md:pt-4 mobile-landscape:mb-2 mobile-landscape:h-[75%] mobile-landscape:self-end mobile-portrait:mt-16 portrait:col-start-2 portrait:row-span-3 portrait:row-start-3 portrait:h-1/2 portrait:gap-6 portrait:self-center">
@@ -67,10 +90,14 @@ export default function WelcomePage() {
         />
       </section>
 
-      <LogoutComponent className="relative z-0 col-span-2 col-start-7 row-start-6 flex flex-col gap-2 self-end justify-self-end mobile-landscape:mb-10 mobile-portrait:mb-10" />
+      {!isShowcase && (
+        <LogoutComponent className="relative z-0 col-span-2 col-start-7 row-start-6 flex flex-col gap-2 self-end justify-self-end mobile-landscape:mb-10 mobile-portrait:mb-10" />
+      )}
 
       {/* <CookieModal /> */}
-      <LMSButton className="col-span-1 col-start-1 row-span-1 row-start-1 w-32 2xl:w-44 mobile-portrait:w-24" />
+      {!isShowcase && (
+        <LMSButton className="col-span-1 col-start-1 row-span-1 row-start-1 w-32 2xl:w-44 mobile-portrait:w-24" />
+      )}
 
       <p className="lg:text-md row-start-6 self-end justify-self-center rounded-lg p-2 text-center text-2xs text-adlerdarkblue md:text-xs lg:col-span-4 lg:col-start-3 lg:font-semibold mobile-landscape:col-span-6 mobile-landscape:col-start-2 mobile-portrait:col-start-1 portrait:col-span-6 portrait:col-start-2 portrait:row-start-6 portrait:text-2xs">
         {translate("copyright")}
