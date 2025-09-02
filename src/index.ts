e2e2e504e65c15f0c6af3e09d0baff36648663ff
Felix Kleis <felixkleis@yahoo.de>
2025-09-02 09:47:37 +0200

    cleaned up draco file locations for them to be in the same place

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import CoreDIContainer from "./Components/Core/DependencyInjection/CoreDIContain
 import CORE_TYPES from "./Components/Core/DependencyInjection/CoreTypes";
 import IReactEntry from "./Components/Core/Presentation/React/ReactRelated/ReactEntryPoint/IReactEntry";
 import { config } from "./config";
-import { configureDracoForOfflineUse } from "./Components/Core/Presentation/Babylon/SceneManagement/DracoConfiguration";
+import { configureDracoForOfflineUse } from "../public/draco/draco-configuration";
 
 console.log("Current Config:", config);
 
