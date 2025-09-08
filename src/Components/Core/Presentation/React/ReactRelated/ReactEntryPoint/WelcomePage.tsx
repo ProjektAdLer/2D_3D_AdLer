4c6e767f35a6a0041d0d407dca22eccbd5cfe514
Mountler <mikey-nihon@web.de>
2025-09-08 09:52:52 +0200

    added test-id for welcome screen

--- src/Components/Core/Presentation/React/ReactRelated/ReactEntryPoint/WelcomePage.tsx
+++ src/Components/Core/Presentation/React/ReactRelated/ReactEntryPoint/WelcomePage.tsx
@@ -49,7 +49,10 @@ export default function WelcomePage() {
   }, [isShowcase, loginUseCase, loadAvatarConfigUseCase]);
 
   return (
-    <div className="relative grid h-[100svh] grid-cols-8 grid-rows-6 bg-gradient-to-br from-adlerbggradientfrom to-adlerbggradientto p-2 mobile-landscape:h-[100dvh] mobile-landscape:w-[100dvw] mobile-landscape:pb-1">
+    <div
+      className="relative grid h-[100svh] grid-cols-8 grid-rows-6 bg-gradient-to-br from-adlerbggradientfrom to-adlerbggradientto p-2 mobile-landscape:h-[100dvh] mobile-landscape:w-[100dvw] mobile-landscape:pb-1"
+      data-testid="welcomepage"
+    >
       <img
         className="absolute -bottom-3 -right-3 m-4 w-32 opacity-20"
         src={logo}
