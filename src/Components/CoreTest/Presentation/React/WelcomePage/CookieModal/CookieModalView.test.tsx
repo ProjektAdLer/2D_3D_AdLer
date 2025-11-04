e06db4d135b453a6423123e71d989c8a3c4fcc92
Lukas Schaab <lukas.schaab@th-ab.de>
2025-11-04 12:51:55 +0100

    added unit tests for CookieModal

--- src/Components/CoreTest/Presentation/React/WelcomePage/CookieModal/CookieModalView.test.tsx
+++ src/Components/CoreTest/Presentation/React/WelcomePage/CookieModal/CookieModalView.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render } from "@testing-library/react";
+import { fireEvent, render, waitFor } from "@testing-library/react";
 import React from "react";
 import CookieModal from "../../../../../Core/Presentation/React/WelcomePage/CookieModal/CookieModal";
 import ICookieModalController from "../../../../../Core/Presentation/React/WelcomePage/CookieModal/ICookieModalController";
@@ -7,17 +7,25 @@ import CoreDIContainer from "../../../../../Core/DependencyInjection/CoreDIConta
 import useBuilderMock from "../../ReactRelated/CustomHooks/useBuilder/useBuilderMock";
 import { mock } from "jest-mock-extended";
 import CookieModalViewModel from "../../../../../Core/Presentation/React/WelcomePage/CookieModal/CookieModalViewModel";
+import CookieModalController from "../../../../../Core/Presentation/React/WelcomePage/CookieModal/CookieModalController";
 
 describe("CookieModal", () => {
+  let getConsentSpy: jest.SpyInstance;
+
+  beforeEach(() => {
+    getConsentSpy = jest.spyOn(CookieModalController, "getConsent");
+  });
+
   afterEach(() => {
     jest.clearAllMocks();
+    jest.restoreAllMocks();
   });
 
   test("should render", () => {
     const result = render(
       <Provider container={CoreDIContainer}>
         <CookieModal />
-      </Provider>
+      </Provider>,
     );
 
     expect(result.getByTestId("cookieModal")).not.toBeEmptyDOMElement();
@@ -29,7 +37,7 @@ describe("CookieModal", () => {
     const result = render(
       <Provider container={CoreDIContainer}>
         <CookieModal />
-      </Provider>
+      </Provider>,
     );
 
     expect(result.container.firstChild).toBeNull();
@@ -42,7 +50,7 @@ describe("CookieModal", () => {
     const result = render(
       <Provider container={CoreDIContainer}>
         <CookieModal />
-      </Provider>
+      </Provider>,
     );
 
     fireEvent.click(result.getByTestId("cookieDecline"));
@@ -57,11 +65,108 @@ describe("CookieModal", () => {
     const result = render(
       <Provider container={CoreDIContainer}>
         <CookieModal />
-      </Provider>
+      </Provider>,
     );
 
     fireEvent.click(result.getByTestId("cookieAcceptAll"));
 
     expect(mockedController.accept).toBeCalledTimes(1);
+  });
+
+  test("setShowModal to false after accept button click", async () => {
+    const mockedController = mock<ICookieModalController>();
+    useBuilderMock([new CookieModalViewModel(), mockedController]);
+
+    const result = render(
+      <Provider container={CoreDIContainer}>
+        <CookieModal />
+      </Provider>,
+    );
+
+    const acceptButton = result.getByTestId("cookieAcceptAll");
+    fireEvent.click(acceptButton);
+
+    // Controller should be called
+    expect(mockedController.accept).toBeCalledTimes(1);
+  });
+
+  test("setShowModal to false after decline button click", async () => {
+    const mockedController = mock<ICookieModalController>();
+    useBuilderMock([new CookieModalViewModel(), mockedController]);
+
+    const result = render(
+      <Provider container={CoreDIContainer}>
+        <CookieModal />
+      </Provider>,
+    );
+
+    const declineButton = result.getByTestId("cookieDecline");
+    fireEvent.click(declineButton);
+
+    // Controller should be called
+    expect(mockedController.decline).toBeCalledTimes(1);
+  });
+
+  test("modal doesn't show when consent already given (accepted)", async () => {
+    getConsentSpy.mockReturnValue("accepted");
+
+    const mockedController = mock<ICookieModalController>();
+    useBuilderMock([new CookieModalViewModel(), mockedController]);
+
+    const result = render(
+      <Provider container={CoreDIContainer}>
+        <CookieModal />
+      </Provider>,
+    );
+
+    await waitFor(() => {
+      expect(getConsentSpy).toHaveBeenCalled();
+    });
+
+    // Modal should not be visible when consent already given
+    const modal = result.queryByTestId("cookieModal");
+    expect(modal).not.toBeInTheDocument();
+  });
+
+  test("modal doesn't show when consent already declined", async () => {
+    getConsentSpy.mockReturnValue("declined");
+
+    const mockedController = mock<ICookieModalController>();
+    useBuilderMock([new CookieModalViewModel(), mockedController]);
+
+    const result = render(
+      <Provider container={CoreDIContainer}>
+        <CookieModal />
+      </Provider>,
+    );
+
+    await waitFor(() => {
+      expect(getConsentSpy).toHaveBeenCalled();
+    });
+
+    // Modal should not be visible when consent already declined
+    const modal = result.queryByTestId("cookieModal");
+    expect(modal).not.toBeInTheDocument();
+  });
+
+  test("modal shows when no consent is stored", async () => {
+    getConsentSpy.mockReturnValue(null);
+
+    const mockedController = mock<ICookieModalController>();
+    useBuilderMock([new CookieModalViewModel(), mockedController]);
+
+    const result = render(
+      <Provider container={CoreDIContainer}>
+        <CookieModal />
+      </Provider>,
+    );
+
+    await waitFor(() => {
+      expect(getConsentSpy).toHaveBeenCalled();
+    });
+
+    // Modal should be rendered when no consent is stored
+    const modal = result.getByTestId("cookieModal");
+    expect(modal).toBeInTheDocument();
   });
 });
