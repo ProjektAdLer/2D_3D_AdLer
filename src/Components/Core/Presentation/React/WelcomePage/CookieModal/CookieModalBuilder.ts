6f90caa52e75d0ae2e659ec0c85bc2482b203709
Lukas Schaab <lukas.schaab@th-ab.de>
2025-11-10 11:39:17 +0100

    connected CookieModalController to SettingsUseCase

--- src/Components/Core/Presentation/React/WelcomePage/CookieModal/CookieModalBuilder.ts
+++ src/Components/Core/Presentation/React/WelcomePage/CookieModal/CookieModalBuilder.ts
@@ -1,8 +1,11 @@
-import { injectable } from "inversify";
+import { injectable, inject } from "inversify";
 import CookieModalController from "./CookieModalController";
 import ICookieModalController from "./ICookieModalController";
 import CookieModalViewModel from "./CookieModalViewModel";
 import PresentationBuilder from "../../../PresentationBuilder/PresentationBuilder";
+import type ISetSettingsConfigUseCase from "src/Components/Core/Application/UseCases/SetSettingsConfig/ISetSettingsConfigUseCase";
+import type IGetSettingsConfigUseCase from "src/Components/Core/Application/UseCases/GetSettingsConfig/IGetSettingsConfigUseCase";
+import USECASE_TYPES from "~DependencyInjection/UseCases/USECASE_TYPES";
 
 @injectable()
 export default class CookieModalBuilder extends PresentationBuilder<
@@ -11,7 +14,24 @@ export default class CookieModalBuilder extends PresentationBuilder<
   undefined,
   undefined
 > {
-  constructor() {
-    super(CookieModalViewModel, CookieModalController, undefined, undefined);
+  private setSettingsConfigUseCase: ISetSettingsConfigUseCase;
+  private getSettingsConfigUseCase: IGetSettingsConfigUseCase;
+
+  constructor(
+    @inject(USECASE_TYPES.ISetSettingsConfigUseCase)
+    setSettingsConfigUseCase: ISetSettingsConfigUseCase,
+    @inject(USECASE_TYPES.IGetSettingsConfigUseCase)
+    getSettingsConfigUseCase: IGetSettingsConfigUseCase,
+  ) {
+    super(CookieModalViewModel, undefined, undefined, undefined);
+    this.setSettingsConfigUseCase = setSettingsConfigUseCase;
+    this.getSettingsConfigUseCase = getSettingsConfigUseCase;
+  }
+
+  override buildController(): void {
+    this.controller = new CookieModalController(
+      this.setSettingsConfigUseCase,
+      this.getSettingsConfigUseCase,
+    );
   }
 }
--- src/Components/Core/Presentation/React/WelcomePage/CookieModal/CookieModalController.ts
+++ src/Components/Core/Presentation/React/WelcomePage/CookieModal/CookieModalController.ts
@@ -1,26 +1,33 @@
+import { inject } from "inversify";
 import ICookieModalController from "./ICookieModalController";
+import type ISetSettingsConfigUseCase from "src/Components/Core/Application/UseCases/SetSettingsConfig/ISetSettingsConfigUseCase";
+import type IGetSettingsConfigUseCase from "src/Components/Core/Application/UseCases/GetSettingsConfig/IGetSettingsConfigUseCase";
+import USECASE_TYPES from "~DependencyInjection/UseCases/USECASE_TYPES";
 
 export default class CookieModalController implements ICookieModalController {
   private static readonly CONSENT_KEY = "adler_cookie_consent";
   private static readonly CONSENT_TIMESTAMP_KEY =
     "adler_cookie_consent_timestamp";
 
+  private setSettingsConfigUseCase: ISetSettingsConfigUseCase;
+  private getSettingsConfigUseCase: IGetSettingsConfigUseCase;
+
+  constructor(
+    @inject(USECASE_TYPES.ISetSettingsConfigUseCase)
+    setSettingsConfigUseCase: ISetSettingsConfigUseCase,
+    @inject(USECASE_TYPES.IGetSettingsConfigUseCase)
+    getSettingsConfigUseCase: IGetSettingsConfigUseCase,
+  ) {
+    this.setSettingsConfigUseCase = setSettingsConfigUseCase;
+    this.getSettingsConfigUseCase = getSettingsConfigUseCase;
+  }
+
   accept(): void {
-    localStorage.setItem(CookieModalController.CONSENT_KEY, "accepted");
-    localStorage.setItem(
-      CookieModalController.CONSENT_TIMESTAMP_KEY,
-      Date.now().toString(),
-    );
-    console.log("Cookies accepted and saved to localStorage.");
+    this.setSettingsConfigUseCase.execute({ cookieConsent: "accepted" });
   }
 
   decline(): void {
-    localStorage.setItem(CookieModalController.CONSENT_KEY, "declined");
-    localStorage.setItem(
-      CookieModalController.CONSENT_TIMESTAMP_KEY,
-      Date.now().toString(),
-    );
-    console.log("Cookies declined and saved to localStorage.");
+    this.setSettingsConfigUseCase.execute({ cookieConsent: "declined" });
   }
 
   public static getConsent(): string | null {
