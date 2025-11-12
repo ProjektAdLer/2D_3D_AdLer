14a7bf269fc8ca73d11520b7f484ea5578a776f9
Felix Kleis <felixkleis@yahoo.de>
2025-11-12 15:18:49 +0100

    belatedly increased version number

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ import { parseBool } from "./Lib/ParseBool";
 // We can not use the logger as its loglevel gets defined here.
 // We have to use console.log and console.error here.
 
-const version = "2.10.0";
+const version = "2.11.0";
 
 // Get the Server URL from config file in Public folder if we are in Production.
 export const getMoodleURL = () => {
